48b2830787975cdfb94bb5003eef7cdecf68c36a
jinsilei <jinsilei@corp.netease.com>
2022-07-21 16:47:09 +0800

    add test

--- spark/v3.1/spark/src/test/java/com/netease/arctic/spark/TestCreateKeyedTableAsSelect.java
+++ spark/v3.1/spark/src/test/java/com/netease/arctic/spark/TestCreateKeyedTableAsSelect.java
@@ -23,8 +23,6 @@ import com.netease.arctic.table.TableIdentifier;
 import org.apache.commons.lang.StringUtils;
 import org.apache.iceberg.PartitionSpec;
 import org.apache.iceberg.Schema;
-import org.apache.iceberg.Table;
-import org.apache.iceberg.relocated.com.google.common.collect.Iterables;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
 import org.apache.iceberg.types.Types;
 import org.junit.After;
@@ -33,16 +31,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.Map;
-
-import static org.apache.spark.sql.functions.*;
 
 public class TestCreateKeyedTableAsSelect extends SparkTestBase {
 
   private final String database = "db_def";
   private final String table = "testA";
-
   private final String sourceTable = "test_table";
+  private final TableIdentifier identifier = TableIdentifier.of(catalogName, database, table);
 
   @Before
   public void prepare() {
@@ -67,18 +62,77 @@ public class TestCreateKeyedTableAsSelect extends SparkTestBase {
   @After
   public void removeTables() {
     sql("DROP TABLE IF EXISTS {0}.{1}", database, sourceTable);
+    sql("DROP TABLE IF EXISTS {0}.{1}", database, table);
   }
 
   @Test
-  public void testCTAS() {
-    TableIdentifier identifier = TableIdentifier.of(catalogName, database, table);
+  public void testPrimaryKeyCTAS() {
     sql("create table {0}.{1} using arctic primary key(id) AS SELECT * from {2}.{3}.{4}",
         database, table, catalogName, database, sourceTable);
     assertTableExist(identifier);
     sql("desc table {0}.{1}", database, table);
     assertDescResult(rows, Lists.newArrayList("id"));
-    rows = sql("select * from {0}.{1}", database, table);
-    Assert.assertEquals(8, rows.size());
+    Schema expectedSchema = new Schema(
+        Types.NestedField.optional(1, "id", Types.IntegerType.get()),
+        Types.NestedField.optional(2, "data", Types.StringType.get()),
+        Types.NestedField.optional(3, "pt", Types.StringType.get())
+    );
+    Assert.assertEquals("Should have expected nullable schema",
+        expectedSchema.asStruct(), loadTable(identifier).schema().asStruct());
+    Assert.assertEquals("Should be an unpartitioned table",
+        0, loadTable(identifier).spec().fields().size());
+    assertEquals("Should have rows matching the source table",
+        sql("SELECT * FROM {0}.{1} ORDER BY id", database, table),
+        sql("SELECT * FROM {0}.{1} ORDER BY id", database, sourceTable));
+  }
+
+  @Test
+  public void testPartitionedCTAS() {
+    sql("CREATE TABLE {0}.{1} USING arctic PARTITIONED BY (id) AS SELECT * FROM {2}.{3} ORDER BY id",
+        database, table, database, sourceTable);
+
+    Schema expectedSchema = new Schema(
+        Types.NestedField.optional(1, "id", Types.IntegerType.get()),
+        Types.NestedField.optional(2, "data", Types.StringType.get()),
+        Types.NestedField.optional(3, "pt", Types.StringType.get())
+    );
+
+    PartitionSpec expectedSpec = PartitionSpec.builderFor(expectedSchema)
+        .identity("id")
+        .build();
+
+
+    Assert.assertEquals("Should have expected nullable schema",
+        expectedSchema.asStruct(), loadTable(identifier).schema().asStruct());
+    Assert.assertEquals("Should be partitioned by id",
+        expectedSpec, loadTable(identifier).spec());
+    assertEquals("Should have rows matching the source table",
+        sql("SELECT * FROM {0}.{1} ORDER BY id", database, table),
+        sql("SELECT * FROM {0}.{1} ORDER BY id", database, sourceTable));
+  }
+
+  @Test
+  public void testRTAS() {
+    sql("CREATE TABLE {0}.{1} USING arctic TBLPROPERTIES (''prop1''=''val1'', ''prop2''=''val2'')" +
+        "AS SELECT * FROM {2}.{3}", database, table, database, sourceTable);
+
+    assertEquals("Should have rows matching the source table",
+        sql("SELECT * FROM {0}.{1} ORDER BY id", database, table),
+        sql("SELECT * FROM {0}.{1} ORDER BY id", database, sourceTable));
+
+    Schema expectedSchema = new Schema(
+        Types.NestedField.optional(1, "id", Types.IntegerType.get()),
+        Types.NestedField.optional(2, "data", Types.StringType.get()),
+        Types.NestedField.optional(3, "pt", Types.StringType.get())
+    );
+
+    Assert.assertEquals("Should have expected nullable schema",
+        expectedSchema.asStruct(), loadTable(identifier).schema().asStruct());
+
+    Assert.assertEquals("Should have updated table property",
+        "val1", loadTable(identifier).properties().get("prop1"));
+    Assert.assertEquals("Should have preserved table property",
+        "val2", loadTable(identifier).properties().get("prop2"));
   }
 
   private void assertDescResult(List<Object[]> rows, List<String> primaryKeys) {
