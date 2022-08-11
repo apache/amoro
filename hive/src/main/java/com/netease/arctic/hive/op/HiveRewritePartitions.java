6cf98336cae58c435753e69a0f8c0f4034513ae9
hzzhangyongxiang <hzzhangyongxiang@corp..netease.com>
2022-08-11 18:41:21 +0800

    [hive] rewrite partition operation, test case

--- hive/src/main/java/com/netease/arctic/hive/op/HiveRewritePartitions.java
+++ hive/src/main/java/com/netease/arctic/hive/op/HiveRewritePartitions.java
@@ -1,27 +1,28 @@
 package com.netease.arctic.hive.op;
 
-import com.google.common.base.Joiner;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import com.netease.arctic.hive.CachedHiveClientPool;
 import com.netease.arctic.op.KeyedPartitionRewrite;
 import com.netease.arctic.table.KeyedTable;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.metastore.api.Partition;
+import org.apache.hadoop.hive.metastore.api.PrincipalPrivilegeSet;
+import org.apache.hadoop.hive.metastore.api.StorageDescriptor;
 import org.apache.hadoop.hive.metastore.api.Table;
+import org.apache.iceberg.ContentFile;
 import org.apache.iceberg.DataFile;
-import org.apache.iceberg.PartitionKey;
 import org.apache.iceberg.StructLike;
 import org.apache.iceberg.Transaction;
+import org.apache.iceberg.relocated.com.google.common.base.Joiner;
+import org.apache.iceberg.relocated.com.google.common.collect.Lists;
+import org.apache.iceberg.relocated.com.google.common.collect.Maps;
 import org.apache.iceberg.types.Type;
 import org.apache.iceberg.types.Types;
 import org.apache.iceberg.util.StructLikeMap;
 import org.apache.thrift.TException;
-import org.mortbay.util.StringUtil;
+
+import java.util.List;
+import java.util.Map;
 
 public class HiveRewritePartitions extends KeyedPartitionRewrite {
   private CachedHiveClientPool client;
@@ -30,7 +31,8 @@ public class HiveRewritePartitions extends KeyedPartitionRewrite {
   private final KeyedTable keyedTable;
 
   private List<Partition> rewritePartitions;
-  private String hiveLocation ;
+  private List<Partition> newPartitions;
+  private String hiveLocation;
 
   public HiveRewritePartitions(KeyedTable keyedTable, CachedHiveClientPool client) {
     super(keyedTable);
@@ -40,19 +42,17 @@ public class HiveRewritePartitions extends KeyedPartitionRewrite {
     this.keyedTable = keyedTable;
   }
 
-
-
   @Override
   protected StructLikeMap<Long> apply(Transaction transaction, StructLikeMap<Long> partitionMaxTxId) {
     StructLikeMap<Long> partitionMaxId = super.apply(transaction, partitionMaxTxId);
-    if (!addFiles.isEmpty()){
-      try{
-        if (keyedTable.spec().isUnpartitioned()){
+    if (!addFiles.isEmpty()) {
+      try {
+        if (keyedTable.spec().isUnpartitioned()) {
           hiveLocation = dataFileDirPath(addFiles.get(0));
-        }else {
-          rewritePartitions = rewritePartitions();
+        } else {
+          rewritePartitions();
         }
-      }catch (TException|InterruptedException e){
+      } catch (TException | InterruptedException e) {
         throw new IllegalStateException("failed to load partition information from hive metastore");
       }
     }
@@ -64,60 +64,120 @@ public class HiveRewritePartitions extends KeyedPartitionRewrite {
     super.commit();
 
     try {
-      if (keyedTable.spec().isUnpartitioned() && !StringUtils.isEmpty(hiveLocation)){
-        client.run( c -> {
+      if (keyedTable.spec().isUnpartitioned() && !StringUtils.isEmpty(hiveLocation)) {
+        client.run(c -> {
           Table tbl = c.getTable(db, tableName);
           tbl.getSd().setLocation(hiveLocation);
           c.alter_table(db, tableName, tbl);
           return 0;
         });
-      } else if (!keyedTable.spec().isUnpartitioned() && !rewritePartitions.isEmpty()){
+      } else if (!keyedTable.spec().isUnpartitioned() &&
+          (rewritePartitions != null || newPartitions != null)) {
         client.run(
             c -> {
-              c.alter_partitions(db, tableName, rewritePartitions);
+              if (newPartitions != null) {
+                c.add_partitions(newPartitions);
+              }
+              if (rewritePartitions != null) {
+                c.alter_partitions(db, tableName, rewritePartitions);
+              }
               return 0;
             }
         );
       }
-    }catch (TException|InterruptedException e){
+    } catch (TException | InterruptedException e) {
       throw new IllegalStateException("failed to update data location in hive metastore");
     }
-
   }
 
-  private String dataFileDirPath(DataFile d){
+  private String dataFileDirPath(DataFile d) {
     Path path = new Path(d.path().toString());
     return path.getParent().toString();
   }
 
-  private List<Partition> rewritePartitions() throws TException, InterruptedException {
+  private void rewritePartitions() throws TException, InterruptedException {
     Types.StructType partitionSchema = keyedTable.spec().partitionType();
 
     // partitionValue -> partitionLocation.
     Map<String, String> partitionLocationMap = Maps.newHashMap();
-    for (DataFile d: addFiles){
+    Map<String, List<DataFile>> partitionDataFileMap = Maps.newHashMap();
+    Map<String, String[]> partitionValueMap = Maps.newHashMap();
+    for (DataFile d : addFiles) {
       String[] partitionValues = partitionValues(d.partition(), partitionSchema);
       String value = Joiner.on("/").join(partitionValues);
       String location = dataFileDirPath(d);
       partitionLocationMap.put(value, location);
+      if (!partitionDataFileMap.containsKey(value)) {
+        partitionDataFileMap.put(value, Lists.newArrayList());
+      }
+      partitionDataFileMap.get(value).add(d);
+      partitionValueMap.put(value, partitionValues);
     }
-
 
     List<Partition> tablePartitions = client.run(
         c -> c.listPartitions(db, tableName, (short) -1)
     );
 
     List<Partition> rewritePartition = Lists.newArrayList();
-    for(Partition p: tablePartitions){
+    List<String> matchedPartition = Lists.newArrayList();
+    for (Partition p : tablePartitions) {
       String values = Joiner.on("/").join(p.getValues());
-      if (partitionLocationMap.containsKey(values)){
+      if (partitionLocationMap.containsKey(values)) {
         String newLocation = partitionLocationMap.get(values);
-
         p.getSd().setLocation(newLocation);
+        int lastAccessTime = (int) (System.currentTimeMillis() / 1000);
+        p.setLastAccessTime(lastAccessTime);
+
+        List<DataFile> partitionDataFiles = partitionDataFileMap.get(values);
+        int files = partitionDataFiles.size();
+        long totalSize = partitionDataFiles.stream().map(ContentFile::fileSizeInBytes).reduce(0L, Long::sum);
+        p.putToParameters("transient_lastDdlTime", lastAccessTime + "");
+        p.putToParameters("totalSize", totalSize + "");
+        p.putToParameters("numFiles", files + "");
+
         rewritePartition.add(p);
+        matchedPartition.add(values);
       }
     }
-    return rewritePartition;
+    if (!rewritePartition.isEmpty()) {
+      this.rewritePartitions = rewritePartition;
+    }
+
+    // remove rewrite partition, left partitions are new partitions.
+    matchedPartition.forEach(partitionLocationMap::remove);
+
+    if (!partitionLocationMap.isEmpty()) {
+      this.newPartitions = Lists.newArrayList();
+
+      Table tbl = client.run(c -> c.getTable(db, tableName));
+      StorageDescriptor tableSd = tbl.getSd();
+      PrincipalPrivilegeSet privilegeSet = tbl.getPrivileges();
+      for (String val : partitionLocationMap.keySet()) {
+        String location = partitionLocationMap.get(val);
+        String[] values = partitionValueMap.get(val);
+        int lastAccessTime = (int) (System.currentTimeMillis() / 1000);
+
+        Partition p = new Partition();
+        p.setValues(Lists.newArrayList(values));
+        p.setDbName(db);
+        p.setTableName(tableName);
+        p.setCreateTime(lastAccessTime);
+        p.setLastAccessTime(lastAccessTime);
+        StorageDescriptor sd = tableSd.deepCopy();
+        sd.setLocation(location);
+        p.setSd(sd);
+        List<DataFile> partitionDataFiles = partitionDataFileMap.get(val);
+        int files = partitionDataFiles.size();
+        long totalSize = partitionDataFiles.stream().map(ContentFile::fileSizeInBytes).reduce(0L, Long::sum);
+        p.putToParameters("transient_lastDdlTime", lastAccessTime + "");
+        p.putToParameters("totalSize", totalSize + "");
+        p.putToParameters("numFiles", files + "");
+        if (privilegeSet != null) {
+          p.setPrivileges(privilegeSet.deepCopy());
+        }
+        newPartitions.add(p);
+      }
+    }
   }
 
   private static String[] partitionValues(StructLike partitionData, Types.StructType partitionSchema) {
@@ -130,5 +190,4 @@ public class HiveRewritePartitions extends KeyedPartitionRewrite {
     }
     return values;
   }
-
 }
