9bb3176c09e8f40dc7b6f4930e7c8b96d7d6cd2a
jinsilei <jinsilei@corp.netease.com>
2022-08-11 10:56:17 +0800

    add filter

--- spark/v2.3/spark/src/main/scala/com/netease/arctic/spark/sql/execution/CreateArcticTableAsSelectCommand.scala
+++ spark/v2.3/spark/src/main/scala/com/netease/arctic/spark/sql/execution/CreateArcticTableAsSelectCommand.scala
@@ -1,14 +1,12 @@
 package com.netease.arctic.spark.sql.execution
 
-import com.netease.arctic.catalog.CatalogLoader
 import com.netease.arctic.spark.source.{ArcticSource, SupportsDynamicOverwrite}
-import com.netease.arctic.spark.util.ArcticSparkUtil
-import com.netease.arctic.table.TableIdentifier
 import org.apache.spark.sql.arctic.AnalysisException
 import org.apache.spark.sql.catalyst.catalog.{CatalogTable, CatalogTableType}
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.execution.command.RunnableCommand
 import org.apache.spark.sql.execution.datasources.v2.WriteToDataSourceV2
+import org.apache.spark.sql.internal.StaticSQLConf
 import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 
 case class CreateArcticTableAsSelectCommand(arctic: ArcticSource,
@@ -20,6 +18,11 @@ case class CreateArcticTableAsSelectCommand(arctic: ArcticSource,
   override def run(sparkSession: SparkSession): Seq[Row] = {
     assert(table.tableType != CatalogTableType.VIEW)
     assert(table.provider.isDefined)
+    val spark = SparkSession.getActiveSession.get
+    val sparkCatalogImpl = spark.conf.get(StaticSQLConf.CATALOG_IMPLEMENTATION.key)
+    if (!"hive".equalsIgnoreCase(sparkCatalogImpl)) {
+      throw AnalysisException.message(s"failed to create table ${table.identifier} not use hive catalog")
+    }
     // create table
     val arcticTable = arctic.createTable(table.identifier, table.schema,
       scala.collection.JavaConversions.seqAsJavaList(table.partitionColumnNames),
--- spark/v2.3/spark/src/main/scala/com/netease/arctic/spark/sql/execution/CreateArcticTableCommand.scala
+++ spark/v2.3/spark/src/main/scala/com/netease/arctic/spark/sql/execution/CreateArcticTableCommand.scala
@@ -4,6 +4,7 @@ import com.netease.arctic.spark.source.ArcticSource
 import org.apache.spark.sql.arctic.AnalysisException
 import org.apache.spark.sql.catalyst.catalog.{CatalogTable, CatalogTableType}
 import org.apache.spark.sql.execution.command.RunnableCommand
+import org.apache.spark.sql.internal.StaticSQLConf
 import org.apache.spark.sql.{Row, SparkSession}
 
 case class CreateArcticTableCommand(arctic: ArcticSource, catalogTable: CatalogTable, ignoreIfExists: Boolean)
@@ -11,6 +12,11 @@ case class CreateArcticTableCommand(arctic: ArcticSource, catalogTable: CatalogT
   override def run(sparkSession: SparkSession): Seq[Row] = {
     assert(catalogTable.tableType != CatalogTableType.VIEW)
     assert(catalogTable.provider.isDefined)
+    val spark = SparkSession.getActiveSession.get
+    val sparkCatalogImpl = spark.conf.get(StaticSQLConf.CATALOG_IMPLEMENTATION.key)
+    if (!"hive".equalsIgnoreCase(sparkCatalogImpl)) {
+      throw AnalysisException.message(s"failed to create table ${catalogTable.identifier} not use hive catalog")
+    }
     arctic.createTable(catalogTable.identifier, catalogTable.schema,
       scala.collection.JavaConversions.seqAsJavaList(catalogTable.partitionColumnNames),
       scala.collection.JavaConversions.mapAsJavaMap(catalogTable.properties))
