3afe14a67905f7f217522cfa6b46faab73c3034e
ZhouJinsong <zhoujinsong0505@163.com>
2022-08-24 17:34:52 +0800

    [Arctic-43][Hive]Add partition properties support (#211)
    * add partition peroperties method for unkeyed table
    
    * add a empty struct
    
    * add partition hive location properties
    
    * expose all properties to base store
    
    * fix validate errors
    
    * fix ut error

--- hive/src/test/java/com/netease/arctic/hive/op/TestOverwriteFiles.java
+++ hive/src/test/java/com/netease/arctic/hive/op/TestOverwriteFiles.java
@@ -7,6 +7,7 @@ import com.netease.arctic.hive.table.UnkeyedHiveTable;
 import com.netease.arctic.op.OverwriteBaseFiles;
 import com.netease.arctic.table.KeyedTable;
 import com.netease.arctic.table.TableIdentifier;
+import com.netease.arctic.table.TableProperties;
 import com.netease.arctic.table.UnkeyedTable;
 import com.netease.arctic.utils.FileUtil;
 import java.util.List;
@@ -14,6 +15,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
+import com.netease.arctic.utils.TablePropertyUtil;
 import org.apache.hadoop.hive.metastore.api.Table;
 import org.apache.iceberg.DataFile;
 import org.apache.iceberg.OverwriteFiles;
@@ -167,6 +170,10 @@ public class TestOverwriteFiles extends HiveTableTestBase {
     Assert.assertTrue(
         "table location to new path",
         hiveTable.getSd().getLocation().endsWith("/test_path/hive_data_location"));
+    Assert.assertEquals("table partition properties is error",
+        hiveTable.getSd().getLocation(),
+        table.partitionProperty().get(TablePropertyUtil.EMPTY_STRUCT)
+            .get(TableProperties.PARTITION_PROPERTIES_KEY_HIVE_LOCATION));
 
     // =================== test delete all files and add no file to un-partitioned table ===================
     overwriteFiles = table.newOverwrite();
@@ -183,6 +190,10 @@ public class TestOverwriteFiles extends HiveTableTestBase {
     Assert.assertTrue(
         "table location change to hive location",
         hiveLocation.startsWith(table.hiveLocation()));
+    Assert.assertEquals("table partition properties is error",
+        hiveTable.getSd().getLocation(),
+        table.partitionProperty().get(TablePropertyUtil.EMPTY_STRUCT)
+            .get(TableProperties.PARTITION_PROPERTIES_KEY_HIVE_LOCATION));
     System.out.println(hiveLocation);
 
     hiveCatalog.dropTable(identifier, true);
