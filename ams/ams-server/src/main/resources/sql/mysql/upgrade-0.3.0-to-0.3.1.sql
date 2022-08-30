6f8f9bab7e347cf06d4d8a2f6c4c11e2014d8c52
wangzeyu <hameizi369@gmail.com>
2022-08-30 15:53:19 +0800

    [ARCTIC-142][AMS]add identify optimize snapshot logic (#215)
    * 1.add identify optimize snapshot logic
    2.add upgrade sql file and add derby and mysql sql dir
    
    * fix
    
    * add derby sql
    
    * update doc
    
    * fix sql
    
    * fix comment
    
    * fix
    
    * change logic getTxExcludeOptimize and change table snapshot_info_cache schema
    
    * fix
    
    * fix comment
    
    * fix unit test

---
+++ ams/ams-server/src/main/resources/sql/mysql/upgrade-0.3.0-to-0.3.1.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `api_tokens`
+(
+    `id`         int(11) NOT NULL AUTO_INCREMENT,
+    `apikey`     varchar(256) NOT NULL COMMENT 'openapi client public key',
+    `secret`     varchar(256) NOT NULL COMMENT 'The key used by the client to generate the request signature',
+    `apply_time` datetime DEFAULT NULL COMMENT 'apply time',
+    PRIMARY KEY (`id`) USING BTREE,
+    UNIQUE KEY `account_unique` (`apikey`) USING BTREE COMMENT 'account unique'
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='Openapi  secret';
+
+CREATE TABLE `ddl_record`
+(
+    `table_identifier` varchar(256) NOT NULL,
+    `ddl`              mediumtext,
+    `ddl_type`         varchar(256) NOT NULL,
+    `commit_time`      timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `snapshot_info_cache` ADD COLUMN `producer` varchar(64) NOT NULL DEFAULT 'INGESTION';
+ALTER TABLE `snapshot_info_cache` ADD COLUMN `file_size` bigint(20) NOT NULL DEFAULT 0;
+ALTER TABLE `snapshot_info_cache` ADD COLUMN `file_count` int(11) NOT NULL DEFAULT 0;
+ALTER TABLE `file_info_cache` ADD COLUMN `producer` varchar(64) NOT NULL DEFAULT 'INGESTION';
+ALTER TABLE `table_metadata` ADD COLUMN `cur_schema_id` int(11) DEFAULT NULL;
