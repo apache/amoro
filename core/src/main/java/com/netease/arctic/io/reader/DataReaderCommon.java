9859478dbd54260b12a38858b32cc8a84ab325a0
shidayang <530847445@qq.com>
2022-11-01 15:04:27 +0800

    Make BaseIcebergReader can read "transaction_id, offset,action"

--- core/src/main/java/com/netease/arctic/io/reader/DataReaderCommon.java
+++ core/src/main/java/com/netease/arctic/io/reader/DataReaderCommon.java
@@ -17,7 +17,7 @@ import java.util.function.BiFunction;
 
 public class DataReaderCommon {
 
-  protected static Map<Integer, ?> getIdToConstant(ArcticFileScanTask task, Schema projectedSchema,
+  protected static Map<Integer, ?> getIdToConstant(FileScanTask task, Schema projectedSchema,
       BiFunction<Type, Object, Object> convertConstant) {
     Schema partitionSchema = TypeUtil.select(projectedSchema, task.spec().identitySourceIds());
     Map<Integer, Object> idToConstant = new HashMap<>();
@@ -26,38 +26,28 @@ public class DataReaderCommon {
     }
     idToConstant.put(org.apache.iceberg.MetadataColumns.FILE_PATH.fieldId(),
         convertConstant.apply(Types.StringType.get(), task.file().path().toString()));
-    idToConstant.put(
-        MetadataColumns.TRANSACTION_ID_FILED_ID,
-        convertConstant.apply(Types.LongType.get(), task.file().transactionId()));
 
-    if (task.fileType() == DataFileType.BASE_FILE) {
+    if (task instanceof ArcticFileScanTask) {
+      ArcticFileScanTask arcticFileScanTask = (ArcticFileScanTask)task;
       idToConstant.put(
-          MetadataColumns.FILE_OFFSET_FILED_ID,
-          convertConstant.apply(Types.LongType.get(), Long.MAX_VALUE));
-    }
-    if (task.fileType() == DataFileType.EQ_DELETE_FILE) {
-      idToConstant.put(MetadataColumns.CHANGE_ACTION_ID, convertConstant.apply(
-          Types.StringType.get(),
-          ChangeAction.DELETE.toString()));
-    } else if (task.fileType() == DataFileType.INSERT_FILE) {
-      idToConstant.put(MetadataColumns.CHANGE_ACTION_ID, convertConstant.apply(
-          Types.StringType.get(),
-          ChangeAction.INSERT.toString()));
+          MetadataColumns.TRANSACTION_ID_FILED_ID,
+          convertConstant.apply(Types.LongType.get(), arcticFileScanTask.file().transactionId()));
+
+      if (arcticFileScanTask.fileType() == DataFileType.BASE_FILE) {
+        idToConstant.put(
+            MetadataColumns.FILE_OFFSET_FILED_ID,
+            convertConstant.apply(Types.LongType.get(), Long.MAX_VALUE));
+      }
+      if (arcticFileScanTask.fileType() == DataFileType.EQ_DELETE_FILE) {
+        idToConstant.put(MetadataColumns.CHANGE_ACTION_ID, convertConstant.apply(
+            Types.StringType.get(),
+            ChangeAction.DELETE.toString()));
+      } else if (arcticFileScanTask.fileType() == DataFileType.INSERT_FILE) {
+        idToConstant.put(MetadataColumns.CHANGE_ACTION_ID, convertConstant.apply(
+            Types.StringType.get(),
+            ChangeAction.INSERT.toString()));
+      }
     }
     return idToConstant;
   }
-
-  protected static Map<Integer, ?> getIdToConstant(FileScanTask task, Schema projectedSchema,
-                                                   BiFunction<Type, Object, Object> convertConstant) {
-    Schema partitionSchema = TypeUtil.select(projectedSchema, task.spec().identitySourceIds());
-    Map<Integer, Object> idToConstant = new HashMap<>();
-    if (!partitionSchema.columns().isEmpty()) {
-      idToConstant.putAll(PartitionUtil.constantsMap(task, convertConstant));
-    }
-    idToConstant.put(org.apache.iceberg.MetadataColumns.FILE_PATH.fieldId(),
-        convertConstant.apply(Types.StringType.get(), task.file().path().toString()));
-
-    return idToConstant;
-  }
-
 }
