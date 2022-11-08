294197a4f91d22d11fa7898d723164d79036d19f
jinsilei <jinsilei@corp.netease.com>
2022-11-08 16:02:13 +0800

    add test

--- core/src/test/java/com/netease/arctic/utils/ArcticDataFilesTest.java
+++ core/src/test/java/com/netease/arctic/utils/ArcticDataFilesTest.java
@@ -6,8 +6,6 @@ import org.apache.iceberg.types.Types;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Optional;
-
 public class ArcticDataFilesTest {
 
   private static final Schema SCHEMA = new Schema(
@@ -21,10 +19,5 @@ public class ArcticDataFilesTest {
   @Test
   public void testReadData() {
     Assert.assertEquals(MONTH, ArcticDataFiles.data(SPEC, "dt_month=2022-01").get(0));
-  }
-
-  @Test
-  public void testReadMonthData() {
-    Assert.assertEquals(MONTH, ArcticDataFiles.readMonthData("2022-01"));
   }
 }
