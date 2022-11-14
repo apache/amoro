f667608ad493b7f651b61d4345ec0cf932055c3e
zstraw <zstraw@163.com>
2022-11-14 16:32:05 +0800

    [ARCTIC-561] When flink job fails over, flink job will commit a wrong transaction id (#568)
    (cherry picked from commit 3ccffa4fa2c3693bf5dacaf071581b20bd43287e)
    Signed-off-by: zhoujinsong <463763777@qq.com>

--- flink/v1.14/flink/src/main/java/com/netease/arctic/flink/write/ArcticFileWriter.java
+++ flink/v1.14/flink/src/main/java/com/netease/arctic/flink/write/ArcticFileWriter.java
@@ -26,6 +26,9 @@ import com.netease.arctic.flink.util.ArcticUtils;
 import com.netease.arctic.table.ArcticTable;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.flink.annotation.VisibleForTesting;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.typeutils.base.LongSerializer;
+import org.apache.flink.runtime.state.StateInitializationContext;
 import org.apache.flink.streaming.api.operators.AbstractStreamOperator;
 import org.apache.flink.streaming.api.operators.BoundedOneInput;
 import org.apache.flink.streaming.api.operators.OneInputStreamOperator;
@@ -94,7 +97,6 @@ public class ArcticFileWriter extends AbstractStreamOperator<WriteResult>
 
   @Override
   public void open() {
-    this.subTaskId = getRuntimeContext().getIndexOfThisSubtask();
     this.attemptId = getRuntimeContext().getAttemptNumber();
     this.jobId = getContainingTask().getEnvironment().getJobID().toString();
     table = ArcticUtils.loadArcticTable(tableLoader);
@@ -103,6 +105,19 @@ public class ArcticFileWriter extends AbstractStreamOperator<WriteResult>
     initTaskWriterFactory(mask);
 
     this.writer = table.io().doAs(taskWriterFactory::create);
+  }
+
+  @Override
+  public void initializeState(StateInitializationContext context) throws Exception {
+    super.initializeState(context);
+
+    this.subTaskId = getRuntimeContext().getIndexOfThisSubtask();
+
+    if (context.isRestored()) {
+      checkpointId = context.getRestoredCheckpointId().getAsLong();
+      // prepare for the writer init in open(). It is used for next ckpId.
+      checkpointId++;
+    }
   }
 
   private void initTaskWriterFactory(Long mask) {
@@ -120,11 +135,17 @@ public class ArcticFileWriter extends AbstractStreamOperator<WriteResult>
     if (table.isKeyedTable()) {
       String signature = BaseEncoding.base16().encode((jobId + checkpointId).getBytes());
       transaction = table.asKeyedTable().beginTransaction(signature);
-      LOG.info("table:{}, signature:{}, transactionId:{}", table.name(), signature, transaction);
+      LOG.info("table:{}, signature:{}, transactionId:{}. From jobId:{}, ckpId:{}", table.name(), signature,
+          transaction, jobId, checkpointId);
     } else {
       transaction = null;
     }
     return transaction;
+  }
+
+  @VisibleForTesting
+  public long getCheckpointId() {
+    return checkpointId;
   }
 
   private long getMask(int subTaskId) {
