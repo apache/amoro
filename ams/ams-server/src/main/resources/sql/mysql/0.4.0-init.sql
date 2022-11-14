8b4bc5f6ea347a8985bf9dcb7b0dc4d44725cb8e
wangzeyu <hameizi369@gmail.com>
2022-11-14 17:31:48 +0800

    [AMS] change 0.4.0-init sql file (#600)
    change sql

--- ams/ams-server/src/main/resources/sql/mysql/0.4.0-init.sql
+++ ams/ams-server/src/main/resources/sql/mysql/0.4.0-init.sql
@@ -35,6 +35,7 @@ CREATE TABLE `file_info_cache`
     `add_snapshot_id`    bigint(20) NOT NULL COMMENT 'the snapshot id who add this file',
     `parent_snapshot_id` bigint(20) NOT NULL COMMENT 'parent snapshot of add_snapshot_id',
     `delete_snapshot_id` bigint(20) DEFAULT NULL COMMENT 'the snapshot id who delete this file',
+    `add_snapshot_sequence` bigint(20) NOT NULL DEFAULT -1 COMMENT 'the snapshot sequence who add this file',
     `inner_table`        varchar(64)          DEFAULT NULL COMMENT 'table type like change/base',
     `file_path`          varchar(400)         NOT NULL COMMENT 'table type like change/base',
     `file_type`          varchar(64)          DEFAULT NULL COMMENT 'absolute file path',
@@ -105,25 +106,25 @@ CREATE TABLE `optimize_history`
     KEY                             `table_name_record` (`catalog_name`,`db_name`,`table_name`,`history_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT 'History of optimizing after each commit';
 
-CREATE TABLE `optimize_job`
+CREATE TABLE `optimizer`
 (
-    `job_id`               bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `job_name`             varchar(1024) DEFAULT NULL COMMENT 'job name',
+    `optimizer_id`               bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `optimizer_name`             varchar(1024) DEFAULT NULL COMMENT 'optimizer name',
     `queue_id`             int(11) DEFAULT NULL COMMENT 'queue id',
     `queue_name`           varchar(1024) DEFAULT NULL COMMENT 'queue name',
-    `job_start_time`       varchar(1024) DEFAULT NULL COMMENT 'job start time',
-    `job_fail_time`        varchar(1024) DEFAULT NULL COMMENT 'job fail time',
-    `job_status`           varchar(16)   DEFAULT NULL COMMENT 'job status',
+    `optimizer_start_time`       varchar(1024) DEFAULT NULL COMMENT 'optimizer start time',
+    `optimizer_fail_time`        varchar(1024) DEFAULT NULL COMMENT 'optimizer fail time',
+    `optimizer_status`           varchar(16)   DEFAULT NULL COMMENT 'optimizer status',
     `core_number`          int(11) DEFAULT NULL COMMENT 'total number of all CPU resources',
-    `memory`               bigint(30) DEFAULT NULL COMMENT 'job use memory size',
-    `parallelism`          int(11) DEFAULT NULL COMMENT 'job parallelism',
+    `memory`               bigint(30) DEFAULT NULL COMMENT 'optimizer use memory size',
+    `parallelism`          int(11) DEFAULT NULL COMMENT 'optimizer parallelism',
     `jobmanager_url`       varchar(1024) DEFAULT NULL COMMENT 'jobmanager url',
     `optimizer_instance`   blob COMMENT 'optimizer instance bytes, use to deserialize optimizer instance',
     `optimizer_state_info` mediumtext COMMENT 'optimizer state info, contains like yarn application id and flink job id',
-    `container`            varchar(50)   DEFAULT '' COMMENT 'name of container which this job belongs to',
+    `container`            varchar(50)   DEFAULT '' COMMENT 'name of container which this optimizer belongs to',
     `update_time` timestamp not null default CURRENT_TIMESTAMP COMMENT 'update time',
-    PRIMARY KEY (`job_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT 'Job info of optimize';
+    PRIMARY KEY (`optimizer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT 'Optimizer info';
 
 CREATE TABLE `optimize_group`
 (
@@ -138,12 +139,12 @@ CREATE TABLE `optimize_group`
 CREATE TABLE `optimize_task`
 (
     `trace_id`                  varchar(40) NOT NULL COMMENT 'Optimize task uuid',
-    `optimize_type`                 varchar(10) NOT NULL COMMENT 'Optimize type: Major, Minor',
+    `optimize_type`             varchar(10) NOT NULL COMMENT 'Optimize type: Major, Minor',
     `catalog_name`              varchar(64) NOT NULL COMMENT 'Catalog name',
     `db_name`                   varchar(64) NOT NULL COMMENT 'Database name',
     `table_name`                varchar(64) NOT NULL COMMENT 'Table name',
     `partition`                 varchar(128)  DEFAULT NULL COMMENT 'Partition',
-    `task_group`                varchar(40)   DEFAULT NULL COMMENT 'Group of task, task of one group should commit together',
+    `task_commit_group`         varchar(40)   DEFAULT NULL COMMENT 'UUID. Commit group of task, task of one commit group should commit together',
     `max_change_transaction_id` bigint(20) NOT NULL DEFAULT '-1' COMMENT 'Max change transaction id',
     `create_time`               datetime(3) DEFAULT NULL COMMENT 'Task create time',
     `properties`                text COMMENT 'Task properties',
@@ -157,7 +158,7 @@ CREATE TABLE `optimize_task`
     `base_file_size`            bigint(20) DEFAULT NULL COMMENT 'Base file size in bytes',
     `pos_delete_file_size`      bigint(20) DEFAULT NULL COMMENT 'Pos-Delete file size in bytes',
     `source_nodes`              varchar(2048) DEFAULT NULL COMMENT 'Source nodes of task',
-    `task_history_id`           varchar(40)   DEFAULT NULL COMMENT 'Task history id',
+    `task_plan_group`           varchar(40)   DEFAULT NULL COMMENT 'UUID. Plan group of task, task of one plan group are planned together',
     `status`        varchar(16)   DEFAULT NULL  COMMENT 'Optimize Status: Init, Pending, Executing, Failed, Prepared, Committed',
     `pending_time`  datetime(3) DEFAULT NULL COMMENT 'Time when task start waiting to execute',
     `execute_time`  datetime(3) DEFAULT NULL COMMENT 'Time when task start executing',
@@ -180,6 +181,7 @@ CREATE TABLE `snapshot_info_cache`
 (
     `table_identifier`   varchar(384) NOT NULL COMMENT 'table full name with catalog.db.table',
     `snapshot_id`        bigint(20) NOT NULL COMMENT 'snapshot id',
+    `snapshot_sequence`  bigint(20) NOT NULL DEFAULT -1 COMMENT 'snapshot sequence'，
     `parent_snapshot_id` bigint(20) NOT NULL COMMENT 'parent snapshot id',
     `action`             varchar(64)          DEFAULT NULL COMMENT 'snapshot type',
     `inner_table`        varchar(64)          NOT NULL COMMENT 'table type like change/base',
@@ -223,7 +225,7 @@ CREATE TABLE `optimize_table_runtime`
     `current_snapshot_id`        bigint(20) NOT NULL DEFAULT '-1' COMMENT 'Base table current snapshot id',
     `latest_major_optimize_time` mediumtext COMMENT 'Latest Major Optimize time for all partitions',
     `latest_minor_optimize_time` mediumtext COMMENT 'Latest Minor Optimize time for all partitions',
-    `latest_task_history_id`     varchar(40) DEFAULT NULL COMMENT 'Latest task history id',
+    `latest_task_plan_group`     varchar(40) DEFAULT NULL COMMENT 'Latest task plan group',
     `optimize_status`            varchar(20) DEFAULT 'Idle' COMMENT 'Table optimize status: MajorOptimizing, MinorOptimizing, Pending, Idle',
     `optimize_status_start_time` datetime(3) DEFAULT NULL COMMENT 'Table optimize status start time',
     `current_change_snapshotId`  bigint(20) DEFAULT NULL COMMENT 'Change table current snapshot id',
@@ -233,18 +235,20 @@ CREATE TABLE `optimize_table_runtime`
 
 CREATE TABLE `optimize_task_history`
 (
-    `task_history_id` varchar(40) NOT NULL COMMENT 'Task history id',
-    `task_group_id`   varchar(40) NOT NULL COMMENT 'Task group id',
-    `catalog_name`    varchar(64) NOT NULL COMMENT 'Catalog name',
-    `db_name`         varchar(64) NOT NULL COMMENT 'Database name',
-    `table_name`      varchar(64) NOT NULL COMMENT 'Table name',
-    `start_time`      datetime(3) DEFAULT NULL COMMENT 'Task start time',
-    `end_time`        datetime(3) DEFAULT NULL COMMENT 'Task end time',
-    `cost_time`       bigint(20) DEFAULT NULL COMMENT 'Task cost time',
-    `queue_id`        int(11) DEFAULT NULL COMMENT 'Task queue id',
-    PRIMARY KEY (`task_history_id`,`task_group_id`),
-    KEY               `task_group_id_index` (`task_history_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT 'History of each optimize task';
+    `task_trace_id`     varchar(50) NOT NULL COMMENT 'Optimize task uuid',
+    `retry`             int(11) NOT NULL COMMENT 'Retry times for the same task_trace_id',
+    `task_plan_group`   varchar(40) NOT NULL COMMENT 'Plan group of task, task of one plan group are planned together',
+    `catalog_name`      varchar(64) NOT NULL COMMENT 'Catalog name',
+    `db_name`           varchar(64) NOT NULL COMMENT 'Database name',
+    `table_name`        varchar(64) NOT NULL COMMENT 'Table name',
+    `start_time`        datetime(3) DEFAULT NULL COMMENT 'Task start time',
+    `end_time`          datetime(3) DEFAULT NULL COMMENT 'Task end time',
+    `cost_time`         bigint(20) DEFAULT NULL COMMENT 'Task cost time',
+    `queue_id`          int(11) DEFAULT NULL COMMENT 'Queue id which execute task',
+    PRIMARY KEY (`task_trace_id`, `retry`),
+    KEY `table_end_time_plan_group_index` (`catalog_name`, `db_name`, `table_name`, `end_time`, `task_plan_group`),
+    KEY `table_plan_group_index` (`catalog_name`, `db_name`, `table_name`, `task_plan_group`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT 'History of each optimize task execute';
 
 CREATE TABLE `table_transaction_meta`
 (
--- ams/ams-server/src/main/resources/sql/mysql/upgrade-0.3.2-to-0.4.sql
+++
@@ -1,5 +0,0 @@
-TRUNCATE optimize_task;
-TRUNCATE optimize_file;
-ALTER TABLE file_info_cache ADD COLUMN `add_snapshot_sequence` bigint(20) NOT NULL DEFAULT -1 COMMENT 'the snapshot sequence who add this file' 
-after `delete_snapshot_id`;
-ALTER TABLE snapshot_info_cache ADD COLUMN `snapshot_sequence` bigint(20) NOT NULL DEFAULT -1 COMMENT 'snapshot sequence' after `snapshot_id`;
