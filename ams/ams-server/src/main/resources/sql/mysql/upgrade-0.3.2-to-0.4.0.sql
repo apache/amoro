99d9a825e166b2a8671a881444d1d6e72541ed2e
wangzeyu <hameizi369@gmail.com>
2022-11-14 17:31:48 +0800

    [AMS] change 0.4.0-init sql file (#600)
    change sql

--- ams/ams-server/src/main/resources/sql/mysql/upgrade-0.3.2-to-0.4.0.sql
+++ ams/ams-server/src/main/resources/sql/mysql/upgrade-0.3.2-to-0.4.0.sql
@@ -1,3 +1,8 @@
 update table_metadata set current_tx_id=0 where current_tx_id is null;
 ALTER TABLE `table_metadata` modify COLUMN `current_tx_id` bigint(20) NOT NULL DEFAULT 0 COMMENT 'current transaction id';
 ALTER TABLE `table_metadata` modify COLUMN `properties` mediumtext COMMENT 'Table properties';
+TRUNCATE optimize_task;
+TRUNCATE optimize_file;
+ALTER TABLE file_info_cache ADD COLUMN `add_snapshot_sequence` bigint(20) NOT NULL DEFAULT -1 COMMENT 'the snapshot sequence who add this file'
+after `delete_snapshot_id`;
+ALTER TABLE snapshot_info_cache ADD COLUMN `snapshot_sequence` bigint(20) NOT NULL DEFAULT -1 COMMENT 'snapshot sequence' after `snapshot_id`;
