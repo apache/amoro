29ae62c027f94aa2be6d20934630e263e3749ef0
Qishang Zhong <zhongqishang@gmail.com>
2022-11-21 14:42:25 +0800

    [ARCTIC-660][AMS] Derby does not support the `limit` syntax

--- ams/ams-server/src/main/java/com/netease/arctic/ams/server/mapper/derby/DerbyPlatformFileInfoMapper.java
+++ ams/ams-server/src/main/java/com/netease/arctic/ams/server/mapper/derby/DerbyPlatformFileInfoMapper.java
@@ -4,6 +4,7 @@ import com.netease.arctic.ams.server.mapper.PlatformFileInfoMapper;
 import com.netease.arctic.ams.server.model.PlatformFileInfo;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @Auth: hzwangtao6
@@ -18,4 +19,8 @@ public interface DerbyPlatformFileInfoMapper extends PlatformFileInfoMapper {
   @Insert("insert into " + TABLE_NAME + "(file_name,file_content_b64)" +
           "values(#{fileInfo.fileName},#{fileInfo.fileContent})")
   void addFile(@Param("fileInfo") PlatformFileInfo platformFileInfo);
+
+  // get fileId by content which is encoded with base64. ** caution: for derby only
+  @Select("select id from " + TABLE_NAME + " where file_content_b64=#{content} fetch first 1 rows only")
+  Integer getFileId(@Param("content") String content);
 }
