53105a6426bc2888f9b55f2529fb3be3e263fb98
zhoujinsong <463763777@qq.com>
2022-12-01 20:58:58 +0800

    fix uinit test error

--- core/src/test/java/com/netease/arctic/utils/CatalogUtilTest.java
+++ core/src/test/java/com/netease/arctic/utils/CatalogUtilTest.java
@@ -1,6 +1,5 @@
 package com.netease.arctic.utils;
 
-import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,18 +13,18 @@ public class CatalogUtilTest {
     @Test
     public void testMergeCatalogPropertiesToTable() {
         Map<String, String> expected = new HashMap<>();
-        expected.put("log-store.enable","true");
+        expected.put("log-store.enabled","true");
         expected.put("log-store.address","168.0.0.1:9092");
         expected.put("log-store.type","kafka");
         expected.put("other.prop","10");
         expected.put("log-store.consistency.guarantee.enable","true");
 
         Map<String, String> userDefined = new HashMap<>();
-        userDefined.put("log-store.enable","true");
+        userDefined.put("log-store.enabled","true");
         userDefined.put("other.prop","10");
 
         Map<String, String> catalogProperties = new HashMap<>();
-        catalogProperties.put("table.log-store.enable","false");
+        catalogProperties.put("table.log-store.enabled","false");
         catalogProperties.put("table.log-store.address","168.0.0.1:9092");
         catalogProperties.put("table.log-store.type","kafka");
         catalogProperties.put("table.log-store.consistency.guarantee.enable","true");
@@ -41,13 +40,13 @@ public class CatalogUtilTest {
     @Test
     public void testMergeCatalogPropertiesToTable1() {
         Map<String, String> expected = new HashMap<>();
-        expected.put("log-store.enable","false");
+        expected.put("log-store.enabled","false");
 
         Map<String, String>  userDefined = new HashMap<>();
-        userDefined.put("log-store.enable","false");
+        userDefined.put("log-store.enabled","false");
 
         Map<String, String> catalogProperties = new HashMap<>();
-        catalogProperties.put("table.log-store.enable","false");
+        catalogProperties.put("table.log-store.enabled","false");
         catalogProperties.put("table.log-store.address","168.0.0.1:9092");
         catalogProperties.put("table.log-store.type","kafka");
         catalogProperties.put("table.log-store.consistency.guarantee.enable","true");
@@ -63,17 +62,17 @@ public class CatalogUtilTest {
     @Test
     public void testMergeCatalogPropertiesToTable2() {
         Map<String, String> expected = new HashMap<>();
-        expected.put("log-store.enable","true");
+        expected.put("log-store.enabled","true");
         expected.put("log-store.address","168.0.0.1:9092");
         expected.put("log-store.type","pulsar");
         expected.put("log-store.consistency.guarantee.enable","true");
 
         Map<String, String> userDefined = new HashMap<>();
-        userDefined.put("log-store.enable","true");
+        userDefined.put("log-store.enabled","true");
         userDefined.put("log-store.type","pulsar");
 
         Map<String, String> catalogProperties = new HashMap<>();
-        catalogProperties.put("table.log-store.enable","false");
+        catalogProperties.put("table.log-store.enabled","false");
         catalogProperties.put("table.log-store.address","168.0.0.1:9092");
         catalogProperties.put("table.log-store.type","kafka");
         catalogProperties.put("table.log-store.consistency.guarantee.enable","true");
@@ -89,19 +88,19 @@ public class CatalogUtilTest {
     @Test
     public void testMergeCatalogPropertiesToTable3() {
         Map<String, String> expected = new HashMap<>();
-        expected.put("log-store.enable","true");
+        expected.put("log-store.enabled","true");
         expected.put("log-store.address","168.0.0.1:9092");
         expected.put("log-store.type","kafka");
         expected.put("log-store.consistency.guarantee.enable","true");
         expected.put("table.other-props","foo");
 
         Map<String, String>  userDefined = new HashMap<>();
-        userDefined.put("log-store.enable","true");
+        userDefined.put("log-store.enabled","true");
         userDefined.put("log-store.type","kafka");
         userDefined.put("table.other-props","foo");
 
         Map<String, String> catalogProperties = new HashMap<>();
-        catalogProperties.put("table.log-store.enable","false");
+        catalogProperties.put("table.log-store.enabled","false");
         catalogProperties.put("table.log-store.address","168.0.0.1:9092");
         catalogProperties.put("table.log-store.type","kafka");
         catalogProperties.put("table.log-store.consistency.guarantee.enable","true");
@@ -112,36 +111,36 @@ public class CatalogUtilTest {
     }
 
     /**
-     * user-defined and default catalog 'optimize.enable' are both switched on,
+     * user-defined and default catalog 'self-optimizing.enabled' are both switched on,
      * keep all related props
      */
     @Test
     public void testMergeCatalogPropertiesToTable4() {
         Map<String, String> expected = new HashMap<>();
-        expected.put("log-store.enable","true");
+        expected.put("log-store.enabled","true");
         expected.put("log-store.address","168.0.0.1:9092");
         expected.put("log-store.type","kafka");
         expected.put("log-store.consistency.guarantee.enable","true");
-        expected.put("optimize.enable","true");
-        expected.put("optimize.quota","0.2"); // should not overwritten by default
-        expected.put("optimize.group","mygroup"); // inherit from default prop
+        expected.put("self-optimizing.enabled","true");
+        expected.put("self-optimizing.quota","0.2"); // should not overwritten by default
+        expected.put("self-optimizing.group","mygroup"); // inherit from default prop
         expected.put("table.other-props","foo");
 
 
         Map<String, String> userDefined = new HashMap<>();
-        userDefined.put("log-store.enable","true");
-        userDefined.put("optimize.enable","true");
-        userDefined.put("optimize.quota","0.2");
+        userDefined.put("log-store.enabled","true");
+        userDefined.put("self-optimizing.enabled","true");
+        userDefined.put("self-optimizing.quota","0.2");
         userDefined.put("table.other-props","foo");
 
         Map<String, String> catalogProperties = new HashMap<>();
-        catalogProperties.put("table.log-store.enable","false");
+        catalogProperties.put("table.log-store.enabled","false");
         catalogProperties.put("table.log-store.address","168.0.0.1:9092");
         catalogProperties.put("table.log-store.type","kafka");
         catalogProperties.put("table.log-store.consistency.guarantee.enable","true");
-        catalogProperties.put("table.optimize.enable","false");
-        catalogProperties.put("table.optimize.quota","0.1");
-        catalogProperties.put("table.optimize.group","mygroup");
+        catalogProperties.put("table.self-optimizing.enabled","false");
+        catalogProperties.put("table.self-optimizing.quota","0.1");
+        catalogProperties.put("table.self-optimizing.group","mygroup");
         catalogProperties.put("ams.address","127.0.0.1");
 
         Map<String, String> result = CatalogUtil.mergeCatalogPropertiesToTable(userDefined, catalogProperties);
@@ -149,36 +148,36 @@ public class CatalogUtilTest {
     }
 
     /**
-     * user-defined and default catalog prop 'optimize.enable' are both switched off,
+     * user-defined and default catalog prop 'self-optimizing.enabled' are both switched off,
      * remove optimizer related props from default catalog
-     * while keep user-defined related prop and 'optimize.enable' itself.
+     * while keep user-defined related prop and 'self-optimizing.enabled' itself.
      */
     @Test
     public void testMergeCatalogPropertiesToTable5() {
         Map<String, String> expected = new HashMap<>();
-        expected.put("log-store.enable","true");
+        expected.put("log-store.enabled","true");
         expected.put("log-store.address","168.0.0.1:9092");
         expected.put("log-store.type","kafka");
         expected.put("log-store.consistency.guarantee.enable","true");
-        expected.put("optimize.enable","false");
+        expected.put("self-optimizing.enabled","false");
         // user-defined related prop should be kept
-        expected.put("optimize.quota","0.2");
+        expected.put("self-optimizing.quota","0.2");
         expected.put("table.other-props","foo");
 
         Map<String, String> userDefined = new HashMap<>();
-        userDefined.put("log-store.enable","true");
-        userDefined.put("optimize.enable","false");
-        userDefined.put("optimize.quota","0.2");
+        userDefined.put("log-store.enabled","true");
+        userDefined.put("self-optimizing.enabled","false");
+        userDefined.put("self-optimizing.quota","0.2");
         userDefined.put("table.other-props","foo");
 
         Map<String, String> catalogProperties = new HashMap<>();
-        catalogProperties.put("table.log-store.enable","false");
+        catalogProperties.put("table.log-store.enabled","false");
         catalogProperties.put("table.log-store.address","168.0.0.1:9092");
         catalogProperties.put("table.log-store.type","kafka");
         catalogProperties.put("table.log-store.consistency.guarantee.enable","true");
-        catalogProperties.put("table.optimize.enable","false");
-        catalogProperties.put("table.optimize.quota","0.1");
-        catalogProperties.put("table.optimize.group","mygroup");
+        catalogProperties.put("table.self-optimizing.enabled","false");
+        catalogProperties.put("table.self-optimizing.quota","0.1");
+        catalogProperties.put("table.self-optimizing.group","mygroup");
         catalogProperties.put("ams.address","127.0.0.1");
 
         Map<String, String> result = CatalogUtil.mergeCatalogPropertiesToTable(userDefined, catalogProperties);
@@ -187,30 +186,30 @@ public class CatalogUtilTest {
 
     /**
      * when optimized flag is off in catalog props and no user-defined value,
-     * remove optimizer related props but 'optimize.enable' itself.
+     * remove optimizer related props but 'self-optimizing.enabled' itself.
      */
     @Test
     public void testMergeCatalogPropertiesToTable6() {
         Map<String, String> expected = new HashMap<>();
-        expected.put("log-store.enable","true");
+        expected.put("log-store.enabled","true");
         expected.put("log-store.address","168.0.0.1:9092");
         expected.put("log-store.type","kafka");
         expected.put("log-store.consistency.guarantee.enable","true");
-        expected.put("optimize.enable","false");
+        expected.put("self-optimizing.enabled","false");
         expected.put("table.other-props","foo");
 
         Map<String, String> userDefined = new HashMap<>();
-        userDefined.put("log-store.enable","true");
+        userDefined.put("log-store.enabled","true");
         userDefined.put("table.other-props","foo");
 
         Map<String, String> catalogProperties = new HashMap<>();
-        catalogProperties.put("table.log-store.enable","false");
+        catalogProperties.put("table.log-store.enabled","false");
         catalogProperties.put("table.log-store.address","168.0.0.1:9092");
         catalogProperties.put("table.log-store.type","kafka");
         catalogProperties.put("table.log-store.consistency.guarantee.enable","true");
-        catalogProperties.put("table.optimize.enable","false");
-        catalogProperties.put("table.optimize.quota","0.1");
-        catalogProperties.put("table.optimize.group","mygroup");
+        catalogProperties.put("table.self-optimizing.enabled","false");
+        catalogProperties.put("table.self-optimizing.quota","0.1");
+        catalogProperties.put("table.self-optimizing.group","mygroup");
         catalogProperties.put("ams.address","127.0.0.1");
 
         Map<String, String> result = CatalogUtil.mergeCatalogPropertiesToTable(userDefined, catalogProperties);
@@ -218,29 +217,29 @@ public class CatalogUtilTest {
     }
 
     /**
-     * user-defined 'optimize.enable' is switched on,
+     * user-defined 'self-optimizing.enabled' is switched on,
      * overwrite behavior of default catalog props
      */
     @Test
     public void testMergeCatalogPropertiesToTable7() {
         Map<String, String> expected = new HashMap<>();
-        expected.put("optimize.enable","true");
-        expected.put("optimize.quota","0.1");
-        expected.put("optimize.group","mygroup");
+        expected.put("self-optimizing.enabled","true");
+        expected.put("self-optimizing.quota","0.1");
+        expected.put("self-optimizing.group","mygroup");
         expected.put("table.other-props","foo");
 
         Map<String, String> userDefined = new HashMap<>();
-        userDefined.put("optimize.enable","true");
+        userDefined.put("self-optimizing.enabled","true");
         userDefined.put("table.other-props","foo");
 
         Map<String, String> catalogProperties = new HashMap<>();
-        catalogProperties.put("table.log-store.enable","false");
+        catalogProperties.put("table.log-store.enabled","false");
         catalogProperties.put("table.log-store.address","168.0.0.1:9092");
         catalogProperties.put("table.log-store.type","kafka");
         catalogProperties.put("table.log-store.consistency.guarantee.enable","true");
-        catalogProperties.put("table.optimize.enable","false");
-        catalogProperties.put("table.optimize.quota","0.1");
-        catalogProperties.put("table.optimize.group","mygroup");
+        catalogProperties.put("table.self-optimizing.enabled","false");
+        catalogProperties.put("table.self-optimizing.quota","0.1");
+        catalogProperties.put("table.self-optimizing.group","mygroup");
         catalogProperties.put("ams.address","127.0.0.1");
 
         Map<String, String> result = CatalogUtil.mergeCatalogPropertiesToTable(userDefined, catalogProperties);
