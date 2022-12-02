50653fd0aa35284b755f7b1a62e127d1e388fb77
ZhouJinsong <zhoujinsong0505@163.com>
2022-12-02 14:11:48 +0800

    [ARCTIC-797]Validate catalog properties when creating (#802)
    * validate catalog properties when creating
    
    * add unit test case
    
    * remove derby files
    
    * change local catalog insert sql
    
    * change table table format for local catalog

--- ams/ams-server/src/main/resources/sql/derby/ams-init.sql
+++ ams/ams-server/src/main/resources/sql/derby/ams-init.sql
@@ -266,4 +266,4 @@ CREATE TABLE platform_file_info (
   PRIMARY KEY (id)
 );
 
-INSERT INTO catalog_metadata(catalog_name,catalog_type,storage_configs,auth_configs, catalog_properties) VALUES ('local_catalog','ams','{"storage.type":"hdfs","hive.site":"PGNvbmZpZ3VyYXRpb24+PC9jb25maWd1cmF0aW9uPg==","hadoop.core.site":"PGNvbmZpZ3VyYXRpb24+PC9jb25maWd1cmF0aW9uPg==","hadoop.hdfs.site":"PGNvbmZpZ3VyYXRpb24+PC9jb25maWd1cmF0aW9uPg=="}','{"auth.type":"simple","auth.simple.hadoop_username":"root"}','{"warehouse.dir":"/tmp/arctic/warehouse","table-formats":"ICEBERG"}');
+INSERT INTO catalog_metadata(catalog_name,catalog_type,storage_configs,auth_configs, catalog_properties) VALUES ('local_catalog','ams','{"storage.type":"hdfs","hive.site":"PGNvbmZpZ3VyYXRpb24+PC9jb25maWd1cmF0aW9uPg==","hadoop.core.site":"PGNvbmZpZ3VyYXRpb24+PC9jb25maWd1cmF0aW9uPg==","hadoop.hdfs.site":"PGNvbmZpZ3VyYXRpb24+PC9jb25maWd1cmF0aW9uPg=="}','{"auth.type":"simple","auth.simple.hadoop_username":"root"}','{"warehouse":"/tmp/arctic/warehouse","table-formats":"MIXED_ICEBERG"}');
