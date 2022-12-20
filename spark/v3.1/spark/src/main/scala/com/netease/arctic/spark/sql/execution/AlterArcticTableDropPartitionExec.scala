f2a4223961f6fc4ba70575efa16186f9519d6f99
jinsilei <jinsilei@corp.netease.com>
2022-12-20 10:29:46 +0800

    add license

--- spark/v3.1/spark/src/main/scala/com/netease/arctic/spark/sql/execution/AlterArcticTableDropPartitionExec.scala
+++ spark/v3.1/spark/src/main/scala/com/netease/arctic/spark/sql/execution/AlterArcticTableDropPartitionExec.scala
@@ -1,18 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.netease.arctic.spark.sql.execution
 
-import com.netease.arctic.hive.table.{SupportHive, UnkeyedHiveTable}
-import com.netease.arctic.spark.ArcticSparkCatalog
-import com.netease.arctic.spark.table.{ArcticIcebergSparkTable, ArcticSparkTable, SupportDropPartitions}
-import com.netease.arctic.table.{ArcticTable, BaseUnkeyedTable, UnkeyedTable}
-import org.apache.commons.collections.CollectionUtils
+import com.netease.arctic.spark.table.SupportDropPartitions
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.analysis.{PartitionSpec, UnresolvedPartitionSpec}
 import org.apache.spark.sql.catalyst.expressions.Attribute
-import org.apache.spark.sql.connector.catalog.{SupportsAtomicPartitionManagement, Table, TableCatalog}
+import org.apache.spark.sql.connector.catalog.Table
 import org.apache.spark.sql.execution.datasources.v2.V2CommandExec
 
-import java.util.stream.Collectors
-import scala.collection.JavaConverters.{asJavaIterableConverter, seqAsJavaListConverter}
+import scala.collection.JavaConverters.asJavaIterableConverter
 
 case class AlterArcticTableDropPartitionExec(table: Table,
                                              parts: Seq[PartitionSpec],
