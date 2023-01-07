0fa476222c1f3b0950dcb17604987d2870c499a9
zy <zstraw@163.com>
2023-01-07 21:52:14 +0800

    sync to 1.15

---
+++ flink/v1.15/flink/src/main/java/com/netease/arctic/flink/read/source/log/LogKafkaPartitionSplit.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netease.arctic.flink.read.source.log;
+
+import org.apache.flink.connector.kafka.source.split.KafkaPartitionSplit;
+
+import java.util.NavigableMap;
+
+public class LogKafkaPartitionSplit extends KafkaPartitionSplit {
+
+  /**
+   * Denote reader is in retracting read mode.
+   * In this mode, data would be read in reverse order and opposite RowKind.
+   */
+  private final boolean retracting;
+  private final Long retractStopOffset;
+  private final Long revertStartOffset;
+  private final Long retractingEpicNo;
+  private final String retractingUpstreamId;
+  /**
+   * Key: upstream job id + "_" + epicNo
+   * Value: epic start offset
+   */
+  private final NavigableMap<String, Long> upStreamEpicStartOffsets;
+
+  public boolean isRetracting() {
+    return retracting;
+  }
+
+  public Long getRetractStopOffset() {
+    return retractStopOffset;
+  }
+
+  public Long getRevertStartOffset() {
+    return revertStartOffset;
+  }
+
+  public NavigableMap<String, Long> getUpStreamEpicStartOffsets() {
+    return upStreamEpicStartOffsets;
+  }
+
+  public Long getRetractingEpicNo() {
+    return retractingEpicNo;
+  }
+
+  public String getRetractingUpstreamId() {
+    return retractingUpstreamId;
+  }
+
+  public LogKafkaPartitionSplit(LogKafkaPartitionSplitState splitState) {
+    super(splitState.getTopicPartition(), splitState.getStartingOffset(),
+        splitState.getStoppingOffset().orElse(NO_STOPPING_OFFSET));
+    retracting = splitState.isRetracting();
+    retractStopOffset = splitState.getRetractStopOffset();
+    revertStartOffset = splitState.getRevertStartOffset();
+    upStreamEpicStartOffsets = splitState.getUpstreamEpicStartOffsets();
+    retractingEpicNo = splitState.getRetractingEpicNo();
+    retractingUpstreamId = splitState.getRetractingUpstreamId();
+  }
+}
