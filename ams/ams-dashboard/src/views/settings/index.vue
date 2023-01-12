f6a2ccb153a1bc8c07c23f81609064b479121c21
huiyuan_ <819747197@qq.com>
2023-01-12 16:00:33 +0800

    [ARCTIC-1005][AMS]: Optimize settings page and terminal page (#1006)
    * feat: update introduce content
    
    * feat: revert
    
    * feat: settings optimizations
    
    Co-authored-by: zhubeibei <zhubeibei@corp.netease.com>

--- ams/ams-dashboard/src/views/settings/index.vue
+++ ams/ams-dashboard/src/views/settings/index.vue
@@ -1,58 +1,64 @@
 <template>
   <div class="setting-wrap">
-    <div class="system-setting">
-      <h1 class="g-mb-12">{{$t('systemSetting')}}</h1>
-      <a-table
-        rowKey="key"
-        :columns="basicColumns"
-        v-if="systemSettingArray.length"
-        :data-source="systemSettingArray"
-        :pagination="false"
-      />
-    </div>
-    <div class="container-setting">
-      <h1 class="g-mb-12">{{$t('containerSetting')}}</h1>
-      <a-collapse v-model:activeKey="activeKey">
-        <a-collapse-panel v-for="container in containerSetting" :key="container.name" :header="container.name">
-          <ul class="content">
-            <li class="item">
-              <h3 class="left">{{$t('name')}}</h3>
-              <span class="right">{{container.name}}</span>
-            </li>
-            <li class="item">
-              <h3 class="left">{{$t('type')}}</h3>
-              <span class="right">{{container.type}}</span>
-            </li>
-          </ul>
-          <h3 class="g-mb-12 g-mt-12">{{$t('properties')}}</h3>
+    <a-tabs v-model:activeKey="activeKeyTab" @change="onChangeTab">
+      <a-tab-pane :key="tabMap['system'].key" :tab="tabMap['system'].title">
+        <div class="system-setting">
           <a-table
             rowKey="key"
-            v-if="container.propertiesArray.length"
             :columns="basicColumns"
-            :data-source="container.propertiesArray"
+            v-if="systemSettingArray.length"
+            :data-source="systemSettingArray"
             :pagination="false"
           />
-          <h3 class="g-mb-12 g-mt-12">{{$t('optimzeGroup')}}</h3>
-          <a-table
-            rowKey="name"
-            :columns="optimzeGroupColumns"
-            :data-source="container.optimizeGroup"
-            :pagination="false"
-          />
-        </a-collapse-panel>
-      </a-collapse>
-    </div>
+        </div>
+      </a-tab-pane>
+      <a-tab-pane :key="tabMap['container'].key" :tab="tabMap['container'].title">
+        <div class="container-setting">
+          <a-collapse v-model:activeKey="activeKey">
+            <a-collapse-panel v-for="container in containerSetting" :key="container.name" :header="container.name">
+              <ul class="content">
+                <li class="item">
+                  <h3 class="left">{{$t('name')}}</h3>
+                  <span class="right">{{container.name}}</span>
+                </li>
+                <li class="item">
+                  <h3 class="left">{{$t('type')}}</h3>
+                  <span class="right">{{container.type}}</span>
+                </li>
+              </ul>
+              <h3 class="g-mb-12 g-mt-12">{{$t('properties')}}</h3>
+              <a-table
+                rowKey="key"
+                v-if="container.propertiesArray.length"
+                :columns="basicColumns"
+                :data-source="container.propertiesArray"
+                :pagination="false"
+              />
+              <h3 class="g-mb-12 g-mt-12">{{$t('optimzeGroup')}}</h3>
+              <a-table
+                rowKey="name"
+                :columns="optimzeGroupColumns"
+                :data-source="container.optimizeGroup"
+                :pagination="false"
+              />
+            </a-collapse-panel>
+          </a-collapse>
+        </div>
+      </a-tab-pane>
+    </a-tabs>
   </div>
   <u-loading v-if="loading" />
 </template>
 
 <script lang="ts" setup>
-import { onMounted, reactive, ref } from 'vue'
+import { reactive, ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 import { IColumns, IKeyAndValue, IContainerSetting } from '@/types/common.type'
 import { useI18n } from 'vue-i18n'
 import { getSystemSetting, getContainersSetting } from '@/services/setting.services'
-
 const { t } = useI18n()
+const router = useRouter()
+const route = useRoute()
 const loading = ref<boolean>(false)
 const systemSettingArray = reactive<IKeyAndValue>([])
 const containerSetting = reactive<IContainerSetting[]>([])
@@ -61,47 +67,74 @@ const optimzeGroupColumns: IColumns[] = reactive([
   { title: t('propertiesMemory', { type: 'taskmanager' }), dataIndex: 'tmMemory', width: '50%', ellipsis: true },
   { title: t('propertiesMemory', { type: 'jobmanager' }), dataIndex: 'jmMemory', width: '50%', ellipsis: true }
 ])
-
 const basicColumns: IColumns[] = reactive([
   { title: t('key'), dataIndex: 'key', width: 340, ellipsis: true },
   { title: t('value'), dataIndex: 'value' }
 ])
 const activeKey = ref<string[]>([])
-
+const tabMap = {
+  system: { title: t('systemSetting'), key: 'system' },
+  container: { title: t('containerSetting'), key: 'container' }
+}
+const activeKeyTab = ref<string>(tabMap.system.key)
+watch(
+  () => route,
+  (val) => {
+    activeKeyTab.value = val.query?.tab as string || tabMap.system.key
+    onChangeTab(activeKeyTab.value)
+  }, {
+    immediate: true
+  }
+)
 async function getSystemSettingInfo() {
-  const res = await getSystemSetting()
-  if (!res) { return }
-  Object.keys(res).forEach(key => {
-    systemSettingArray.push({
-      key: key,
-      value: res[key]
-    })
-  })
-}
-async function getContainersSettingInfo() {
-  const res = await getContainersSetting()
-  activeKey.value = [];
-  (res || []).forEach((ele, index) => {
-    ele.propertiesArray = []
-    activeKey.value.push(ele.name)
-    containerSetting.push(ele)
-    Object.keys(ele.properties || {}).forEach(key => {
-      containerSetting[index].propertiesArray.push({
-        key: key,
-        value: ele.properties[key]
-      })
-    })
-  })
-}
-onMounted(async() => {
   try {
     loading.value = true
-    await Promise.all([getSystemSettingInfo(), getContainersSettingInfo()])
+    const res = await getSystemSetting()
+    if (!res) { return }
+    systemSettingArray.length = 0
+    Object.keys(res).forEach(key => {
+      systemSettingArray.push({
+        key: key,
+        value: res[key]
+      })
+    })
   } finally {
     loading.value = false
   }
-})
-
+}
+async function getContainersSettingInfo() {
+  try {
+    loading.value = true
+    const res = await getContainersSetting()
+    activeKey.value = [];
+    (res || []).forEach((ele, index) => {
+      ele.propertiesArray = []
+      activeKey.value.push(ele.name)
+      containerSetting.push(ele)
+      Object.keys(ele.properties || {}).forEach(key => {
+        containerSetting[index].propertiesArray.push({
+          key: key,
+          value: ele.properties[key]
+        })
+      })
+    })
+  } finally {
+    loading.value = false
+  }
+}
+function onChangeTab(key: string) {
+  const query = { ...route.query }
+  query.tab = key
+  router.replace({ query: { ...query } })
+  getSettingInfo()
+}
+function getSettingInfo() {
+  if (activeKeyTab.value === tabMap.system.key) {
+    getSystemSettingInfo()
+  } else {
+    getContainersSettingInfo()
+  }
+}
 </script>
 
 <style lang="less" scoped>
@@ -119,10 +152,8 @@ onMounted(async() => {
     font-size: 14px;
   }
   .container-setting {
-    padding-top: 16px;
     :deep(.ant-collapse) {
       > .ant-collapse-item > .ant-collapse-header {
-        font-size: 16px;
         font-weight: 500;
         .ant-collapse-arrow {
           vertical-align: 1px;
