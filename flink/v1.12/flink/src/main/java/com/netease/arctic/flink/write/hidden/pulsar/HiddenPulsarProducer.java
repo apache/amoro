948f7a254af3f3fe875de6e3465d73dab937b315
zstraw <zstraw@163.com>
2023-01-29 19:09:25 +0800

    shade

--- flink/v1.12/flink/src/main/java/com/netease/arctic/flink/write/hidden/pulsar/HiddenPulsarProducer.java
+++ flink/v1.12/flink/src/main/java/com/netease/arctic/flink/write/hidden/pulsar/HiddenPulsarProducer.java
@@ -39,7 +39,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
