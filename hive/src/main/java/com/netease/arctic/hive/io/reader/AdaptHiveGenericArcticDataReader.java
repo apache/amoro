18ad69b48798419815a46b88943a9c47f375551b
huangjianmin <531493269@qq.com>
2023-01-31 15:28:47 +0800

    Fix if timestamp field type as a key will cause ClassCastException. Add some UT case to test.

--- hive/src/main/java/com/netease/arctic/hive/io/reader/AdaptHiveGenericArcticDataReader.java
+++ hive/src/main/java/com/netease/arctic/hive/io/reader/AdaptHiveGenericArcticDataReader.java
@@ -19,23 +19,32 @@
 package com.netease.arctic.hive.io.reader;
 
 import com.netease.arctic.data.DataTreeNode;
+import com.netease.arctic.data.PrimaryKeyedFile;
 import com.netease.arctic.iceberg.optimize.InternalRecordWrapper;
 import com.netease.arctic.io.ArcticFileIO;
 import com.netease.arctic.io.reader.BaseArcticDataReader;
+import com.netease.arctic.io.reader.DataReaderCommon;
+import com.netease.arctic.scan.ArcticFileScanTask;
+import com.netease.arctic.scan.KeyedTableScanTask;
 import com.netease.arctic.table.PrimaryKeySpec;
 import com.netease.arctic.utils.map.StructLikeCollections;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.StructLike;
 import org.apache.iceberg.data.Record;
 import org.apache.iceberg.data.parquet.AdaptHiveGenericParquetReaders;
+import org.apache.iceberg.io.CloseableIterable;
+import org.apache.iceberg.io.CloseableIterator;
+import org.apache.iceberg.io.InputFile;
 import org.apache.iceberg.parquet.ParquetValueReader;
 import org.apache.iceberg.types.Type;
 import org.apache.parquet.schema.MessageType;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Implementation of {@link BaseArcticDataReader} with record type {@link Record}.
@@ -82,6 +91,43 @@ public class AdaptHiveGenericArcticDataReader extends AdaptHiveBaseArcticDataRea
   }
 
   @Override
+  public CloseableIterator<Record> readData(KeyedTableScanTask keyedTableScanTask) {
+    AdaptHiveGenericArcticDeleteFilter arcticDeleteFilter =
+        new AdaptHiveGenericArcticDeleteFilter(keyedTableScanTask, tableSchema, projectedSchema, primaryKeySpec,
+            sourceNodes, structLikeCollections);
+    Schema newProjectedSchema = arcticDeleteFilter.requiredSchema();
+
+    CloseableIterable<Record> dataIterable = CloseableIterable.concat(CloseableIterable.transform(
+        CloseableIterable.withNoopClose(keyedTableScanTask.dataTasks()),
+        fileScanTask -> arcticDeleteFilter.filter(newParquetIterable(fileScanTask, newProjectedSchema,
+            DataReaderCommon.getIdToConstant(fileScanTask, newProjectedSchema, convertConstant)))));
+    return dataIterable.iterator();
+  }
+
+  @Override
+  public CloseableIterator<Record> readDeletedData(KeyedTableScanTask keyedTableScanTask) {
+    List<PrimaryKeyedFile> equDeleteFiles = keyedTableScanTask.arcticEquityDeletes().stream()
+        .map(ArcticFileScanTask::file).collect(Collectors.toList());
+
+    if (!equDeleteFiles.isEmpty()) {
+      AdaptHiveGenericArcticDeleteFilter arcticDeleteFilter = new AdaptHiveGenericArcticDeleteFilter(
+          keyedTableScanTask, tableSchema, projectedSchema, primaryKeySpec, sourceNodes, structLikeCollections
+      );
+
+      Schema newProjectedSchema = arcticDeleteFilter.requiredSchema();
+
+      CloseableIterable<Record> dataIterable = CloseableIterable.concat(CloseableIterable.transform(
+          CloseableIterable.withNoopClose(keyedTableScanTask.dataTasks()),
+          fileScanTask -> arcticDeleteFilter.filterNegate(
+              newParquetIterable(fileScanTask, newProjectedSchema,
+                  DataReaderCommon.getIdToConstant(fileScanTask, newProjectedSchema, convertConstant)))));
+      return dataIterable.iterator();
+    } else {
+      return CloseableIterator.empty();
+    }
+  }
+
+  @Override
   protected Function<MessageType, ParquetValueReader<?>> getNewReaderFunction(
       Schema projectSchema,
       Map<Integer, ?> idToConstant) {
@@ -91,5 +137,54 @@ public class AdaptHiveGenericArcticDataReader extends AdaptHiveBaseArcticDataRea
   @Override
   protected Function<Schema, Function<Record, StructLike>> toStructLikeFunction() {
     return schema -> record -> new InternalRecordWrapper(schema.asStruct()).wrap(record);
+  }
+
+  private class AdaptHiveGenericArcticDeleteFilter extends AdaptHiveArcticDeleteFilter<Record> {
+
+    protected Function<Record, StructLike> asStructLike;
+
+    protected AdaptHiveGenericArcticDeleteFilter(
+        KeyedTableScanTask keyedTableScanTask,
+        Schema tableSchema, Schema requestedSchema, PrimaryKeySpec primaryKeySpec) {
+      super(keyedTableScanTask, tableSchema, requestedSchema, primaryKeySpec);
+      this.asStructLike = AdaptHiveGenericArcticDataReader.this.toStructLikeFunction().apply(requiredSchema());
+    }
+
+    protected AdaptHiveGenericArcticDeleteFilter(
+        KeyedTableScanTask keyedTableScanTask,
+        Schema tableSchema,
+        Schema requestedSchema,
+        PrimaryKeySpec primaryKeySpec,
+        Set<DataTreeNode> sourceNodes,
+        StructLikeCollections structLikeCollections) {
+      super(keyedTableScanTask, tableSchema, requestedSchema, primaryKeySpec,
+          sourceNodes, structLikeCollections);
+      this.asStructLike = AdaptHiveGenericArcticDataReader.this.toStructLikeFunction().apply(requiredSchema());
+    }
+
+    protected AdaptHiveGenericArcticDeleteFilter(
+        KeyedTableScanTask keyedTableScanTask,
+        Schema tableSchema,
+        Schema requestedSchema,
+        PrimaryKeySpec primaryKeySpec,
+        Set<DataTreeNode> sourceNodes) {
+      super(keyedTableScanTask, tableSchema, requestedSchema, primaryKeySpec, sourceNodes);
+      this.asStructLike = AdaptHiveGenericArcticDataReader.this.toStructLikeFunction().apply(requiredSchema());
+    }
+
+    @Override
+    protected StructLike asStructLike(Record record) {
+      return asStructLike.apply(record);
+    }
+
+    @Override
+    protected InputFile getInputFile(String location) {
+      return fileIO.newInputFile(location);
+    }
+
+    @Override
+    protected ArcticFileIO getArcticFileIo() {
+      return fileIO;
+    }
   }
 }
