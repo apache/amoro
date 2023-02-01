a1204c63718cb03973755312c1994151ee22e6ee
wangtao <wangtao3@corp.netease.com>
2023-02-01 21:18:02 +0800

    add comment

--- core/src/main/java/com/netease/arctic/data/file/FileNameHandle.java
+++ core/src/main/java/com/netease/arctic/data/file/FileNameHandle.java
@@ -33,10 +33,22 @@ import java.util.regex.Pattern;
 
 /**
  * File name pattern:${tree_node_id}-${file_type}-${transaction_id}-${partition_id}-${task_id}-{operation_id}-{count}
+ * <ul>
+ *   <li>tree_node_id: id of {@link com.netease.arctic.data.DataTreeNode} the file belong</li>
+ *   <li>file_type: short name of file's {@link com.netease.arctic.data.DataFileType} </li>
+ *   <li>transaction_id: id of transaction the file added</li>
+ *   <li>partition_id: id of partitioned data in parallel engine like spark & flink </li>
+ *   <li>task_id: id of write task within partition</li>
+ *   <li>operation_id: a random id to avoid duplicated file name</li>
+ *   <li>count: auto increment count within writer </li>
+ * </ul>
  * like:
- * 1-B-100-0-0-4217271085623029157-0.parquet
- * 4-ED-101-0-0-9009257362994691056-1.parquet
- * 4-I-101-0-0-9009257362994691056-2.parquet
+ * <ul>
+ *   <li>1-B-100-0-0-4217271085623029157-0.parquet</li>
+ *   <li>4-ED-101-0-0-9009257362994691056-1.parquet</li>
+ *   <li>4-I-101-0-0-9009257362994691056-2.parquet</li>
+ * </ul>
+ * 
  */
 public class FileNameHandle {
 
