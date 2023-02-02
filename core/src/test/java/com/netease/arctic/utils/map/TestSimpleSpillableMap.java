a3c237887623fd64f2738d06b0c4816d65b6eb91
luting <dylzlt93299@gmail.com>
2023-02-02 14:51:51 +0800

    fix unit test

--- core/src/test/java/com/netease/arctic/utils/map/TestSimpleSpillableMap.java
+++ core/src/test/java/com/netease/arctic/utils/map/TestSimpleSpillableMap.java
@@ -48,7 +48,7 @@ public class TestSimpleSpillableMap {
 
   private SimpleSpillableMap testMap(long expectMemorySize, int expectKeyCount) {
     SimpleSpillableMap actualMap = new SimpleSpillableMap(expectMemorySize * (keySize + valueSize),
-        null, new StructLikeWrapperSizeEstimator(), new DefaultSizeEstimator<Value>());
+        null, new DefaultSizeEstimator<Key>(), new DefaultSizeEstimator<Value>());
     Assert.assertTrue(actualMap.getSizeOfFileOnDiskInBytes() == 0);
     Map<Key, Value> expectedMap = Maps.newHashMap();
     for (int i = 0; i < expectKeyCount; i++) {
