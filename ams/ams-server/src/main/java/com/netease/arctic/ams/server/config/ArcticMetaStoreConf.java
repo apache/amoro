2f53fa9694d6e1ed6160261f40acd09b5266d049
wangzeyu <hameizi369@gmail.com>
2023-02-10 16:51:38 +0800

    fix

--- ams/ams-server/src/main/java/com/netease/arctic/ams/server/config/ArcticMetaStoreConf.java
+++ ams/ams-server/src/main/java/com/netease/arctic/ams/server/config/ArcticMetaStoreConf.java
@@ -20,141 +20,141 @@ package com.netease.arctic.ams.server.config;
 
 public class ArcticMetaStoreConf {
   public static final ConfigOption<String> CLUSTER_NAME =
-      ConfigOptions.key("cluster.name")
+      ConfigOptions.key("arctic.ams.cluster.name")
           .stringType()
           .defaultValue("default")
           .withDescription("arctic ams cluster name.");
 
   public static final ConfigOption<Boolean> HA_ENABLE =
-      ConfigOptions.key("ha.enabled")
+      ConfigOptions.key("arctic.ams.ha.enabled")
           .booleanType()
           .defaultValue(false)
           .withDescription("is arctic ams running high available.");
 
   public static final ConfigOption<Long> SERVER_MAX_MESSAGE_SIZE =
-      ConfigOptions.key("server.max.message.size")
+      ConfigOptions.key("arctic.ams.server.max.message.size")
           .longType()
           .defaultValue(100 * 1024 * 1024L)
           .withDescription("Maximum message size in bytes a AMS will accept.");
   public static final ConfigOption<Integer> THRIFT_WORKER_THREADS =
-      ConfigOptions.key("thrift.worker.threads")
+      ConfigOptions.key("arctic.ams.thrift.worker.threads")
           .intType()
           .defaultValue(20)
           .withDescription("The number of worker threads in the Thrift server's pool.");
   public static final ConfigOption<Integer> THRIFT_SELECTOR_THREADS =
-      ConfigOptions.key("thrift.selector.threads")
+      ConfigOptions.key("arctic.ams.thrift.selector.threads")
           .intType()
           .defaultValue(2)
           .withDescription("The number of selector threads in the Thrift server's pool.");
   public static final ConfigOption<Integer> THRIFT_QUEUE_SIZE_PER_THREAD =
-      ConfigOptions.key("thrift.selector.queue.size")
+      ConfigOptions.key("arctic.ams.thrift.selector.queue.size")
           .intType()
           .defaultValue(4)
           .withDescription("The number of queue size per selector thread in the Thrift server's pool.");
   public static final ConfigOption<Integer> THRIFT_BIND_PORT =
-      ConfigOptions.key("thrift.port")
+      ConfigOptions.key("arctic.ams.thrift.port")
           .intType()
           .defaultValue(9090)
           .withDescription("Arctic ams listener port");
   public static final ConfigOption<Integer> HTTP_SERVER_PORT =
-      ConfigOptions.key("http.port")
+      ConfigOptions.key("arctic.ams.http.port")
           .intType()
           .defaultValue(19090)
           .withDescription("Arctic ams http listener port");
 
   public static final ConfigOption<Boolean> TCP_KEEP_ALIVE =
-      ConfigOptions.key("server.tcp.keepalive")
+      ConfigOptions.key("arctic.ams.server.tcp.keepalive")
           .booleanType()
           .defaultValue(true)
           .withDescription("Whether to enable TCP keepalive for the ams server. " +
               "Keepalive will prevent accumulation of half-open connections.");
   public static final ConfigOption<Long> OPTIMIZE_CHECK_STATUS_INTERVAL =
-      ConfigOptions.key("optimize.check-status.interval")
+      ConfigOptions.key("arctic.ams.optimize.check-status.interval")
           .longType()
           .defaultValue(60000L)
           .withDescription("Optimize status check interval.");
   public static final ConfigOption<Integer> OPTIMIZE_CHECK_THREAD_POOL_SIZE =
-      ConfigOptions.key("optimize.check.thread.pool-size")
+      ConfigOptions.key("arctic.ams.optimize.check.thread.pool-size")
           .intType()
           .defaultValue(3)
           .withDescription("Number of threads in the thread pool.  " +
               "These will be used to execute all optimize check processes.");
   public static final ConfigOption<Integer> OPTIMIZE_COMMIT_THREAD_POOL_SIZE =
-      ConfigOptions.key("optimize.commit.thread.pool-size")
+      ConfigOptions.key("arctic.ams.optimize.commit.thread.pool-size")
           .intType()
           .defaultValue(10)
           .withDescription("Number of threads in the thread pool.  " +
               "These will be used to execute all optimize commit processes.");
   public static final ConfigOption<Integer> EXPIRE_THREAD_POOL_SIZE =
-      ConfigOptions.key("expire.thread.pool-size")
+      ConfigOptions.key("arctic.ams.expire.thread.pool-size")
           .intType()
           .defaultValue(10)
           .withDescription("Number of threads in the thread pool.  " +
               "These will be used to execute all expire processes.");
   public static final ConfigOption<Integer> ORPHAN_CLEAN_THREAD_POOL_SIZE =
-      ConfigOptions.key("orphan.clean.thread.pool-size")
+      ConfigOptions.key("arctic.ams.orphan.clean.thread.pool-size")
           .intType()
           .defaultValue(10)
           .withDescription("Number of threads in the thread pool.  " +
               "These will be used to execute all orphan file clean processes.");
   public static final ConfigOption<Integer> SUPPORT_HIVE_SYNC_THREAD_POOL_SIZE =
-      ConfigOptions.key("support.hive.sync.thread.pool-size")
+      ConfigOptions.key("arctic.ams.support.hive.sync.thread.pool-size")
           .intType()
           .defaultValue(10)
           .withDescription("Number of threads in the thread pool.  " +
               "These will be used to execute all support hive sync processes.");
   public static final ConfigOption<Integer> SYNC_FILE_INFO_CACHE_THREAD_POOL_SIZE =
-      ConfigOptions.key("file.sync.thread.pool-size")
+      ConfigOptions.key("arctic.ams.file.sync.thread.pool-size")
           .intType()
           .defaultValue(10)
           .withDescription("Number of threads in the thread pool.  " +
               "These will be used to execute all file sync processes.");
   public static final ConfigOption<String> THRIFT_BIND_HOST =
-      ConfigOptions.key("server-host")
+      ConfigOptions.key("arctic.ams.server-host")
           .stringType()
           .defaultValue("")
           .withDescription("Bind host on which to run the ams thrift service.");
   public static final ConfigOption<String> THRIFT_BIND_HOST_PREFIX =
-      ConfigOptions.key("server-host.prefix")
+      ConfigOptions.key("arctic.ams.server-host.prefix")
           .stringType()
           .defaultValue("")
           .withDescription("Bind host on which to run the ams thrift service.");
   public static final ConfigOption<String> ZOOKEEPER_SERVER =
-      ConfigOptions.key("zookeeper.server")
+      ConfigOptions.key("arctic.ams.zookeeper.server")
           .stringType()
           .defaultValue("")
           .withDescription("zookeeper server uri.");
   public static final ConfigOption<Boolean> USE_THRIFT_COMPACT_PROTOCOL =
-      ConfigOptions.key("thrift.compact.protocol.enabled")
+      ConfigOptions.key("arctic.ams.thrift.compact.protocol.enabled")
           .booleanType()
           .defaultValue(false)
           .withDescription("If true, the ams Thrift interface will use TCompactProtocol. " +
               "When false (default) TBinaryProtocol will be used.\n" +
               "Setting it to true will break compatibility with older clients running TBinaryProtocol.");
   public static final ConfigOption<String> MYBATIS_CONNECTION_URL =
-      ConfigOptions.key("mybatis.ConnectionURL")
+      ConfigOptions.key("arctic.ams.mybatis.ConnectionURL")
           .stringType()
           .defaultValue("jdbc:mysql://127.0.0.1:3306/metadata?" +
               "serverTimezone=Asia/Shanghai&useUnicode=true&characterEncoding=UTF8" +
               "&autoReconnect=true&useAffectedRows=true")
           .withDescription("The connection URL which to restore metadata.");
   public static final ConfigOption<String> MYBATIS_CONNECTION_DRIVER_CLASS_NAME =
-      ConfigOptions.key("mybatis.ConnectionDriverClassName")
+      ConfigOptions.key("arctic.ams.mybatis.ConnectionDriverClassName")
           .stringType()
           .defaultValue("com.mysql.jdbc.Driver")
           .withDescription("The connection driver class name which to connect database.");
   public static final ConfigOption<String> MYBATIS_CONNECTION_USER_NAME =
-      ConfigOptions.key("mybatis.ConnectionUserName")
+      ConfigOptions.key("arctic.ams.mybatis.ConnectionUserName")
           .stringType()
           .defaultValue("root")
           .withDescription("The user name in database.");
   public static final ConfigOption<String> MYBATIS_CONNECTION_PASSWORD =
-      ConfigOptions.key("mybatis.ConnectionPassword")
+      ConfigOptions.key("arctic.ams.mybatis.ConnectionPassword")
           .stringType()
           .defaultValue("93299")
           .withDescription("The password in database.");
   public static final ConfigOption<String> DB_TYPE =
-      ConfigOptions.key("database.type")
+      ConfigOptions.key("arctic.ams.database.type")
           .stringType()
           .defaultValue("mysql")
           .withDescription("Restore database type.");
@@ -197,39 +197,39 @@ public class ArcticMetaStoreConf {
   /**
    * config key prefix of terminal
    */
-  public static final String TERMINAL_PREFIX = "terminal.";
+  public static final String TERMINAL_PREFIX = "arctic.ams.terminal.";
   public static final ConfigOption<String> TERMINAL_BACKEND =
-      ConfigOptions.key("terminal.backend")
+      ConfigOptions.key("arctic.ams.terminal.backend")
           .stringType()
           .defaultValue("local")
           .withDescription("terminal backend implement. local, kyuubi are supported");
 
   public static final ConfigOption<String> TERMINAL_SESSION_FACTORY =
-      ConfigOptions.key("terminal.factory")
+      ConfigOptions.key("arctic.ams.terminal.factory")
           .stringType()
           .noDefaultValue()
           .withDescription("session factory implement of terminal.");
 
   public static final ConfigOption<Integer> TERMINAL_RESULT_LIMIT =
-      ConfigOptions.key("terminal.result.limit")
+      ConfigOptions.key("arctic.ams.terminal.result.limit")
           .intType()
           .defaultValue(1000)
           .withDescription("limit of result-set");
 
   public static final ConfigOption<Boolean> TERMINAL_STOP_ON_ERROR =
-      ConfigOptions.key("terminal.stop-on-error")
+      ConfigOptions.key("arctic.ams.terminal.stop-on-error")
           .booleanType()
           .defaultValue(false)
           .withDescription("stop script execution if any statement execute failed.");
 
   public static final ConfigOption<Integer> TERMINAL_SESSION_TIMEOUT =
-      ConfigOptions.key("terminal.session.timeout")
+      ConfigOptions.key("arctic.ams.terminal.session.timeout")
           .intType()
           .defaultValue(30)
           .withDescription("session timeout in minute");
 
   public static final ConfigOption<Long> BLOCKER_TIMEOUT =
-      ConfigOptions.key("blocker.timeout")
+      ConfigOptions.key("arctic.ams.blocker.timeout")
           .longType()
           .defaultValue(60000L)
           .withDescription("session timeout in Milliseconds");
