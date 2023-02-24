a2aea74c7d97f625300fb2f937f60fe25a40544d
huangjianmin <531493269@qq.com>
2023-02-24 16:28:38 +0800

    modify some logics.

--- ams/ams-server/src/main/java/com/netease/arctic/ams/server/repair/command/SimpleRegexCommandParser.java
+++ ams/ams-server/src/main/java/com/netease/arctic/ams/server/repair/command/SimpleRegexCommandParser.java
@@ -36,13 +36,19 @@ public class SimpleRegexCommandParser implements CommandParser {
 
   private static final String ANALYZE = "ANALYZE";
   private static final String REPAIR = "REPAIR";
+  private static final String THROUGH = "THROUGH";
+  private static final String FIND_BACK = "FIND_BACK";
+  private static final String SYNC_METADATA = "SYNC_METADATA";
+  private static final String ROLLBACK = "ROLLBACK";
   private static final String USE = "USE";
   private static final String OPTIMIZE = "OPTIMIZE";
+  private static final String START = "START";
+  private static final String STOP = "STOP";
   private static final String REFRESH = "REFRESH";
-  private static final String SHOW = "SHOW";
-  private static final String THROUGH = "THROUGH";
-  private static final String ROLLBACK = "ROLLBACK";
   private static final String FILE_CACHE = "FILE_CACHE";
+  private static final String SHOW = "SHOW";
+  private static final String DATABASES = "DATABASES";
+  private static final String TABLES = "TABLES";
 
 
   @Override
@@ -63,7 +69,8 @@ public class SimpleRegexCommandParser implements CommandParser {
             } else {
               return repairCallGenerator.generate(commandSplit[1], commandSplit[3], commandSplit[4]);
             }
-          } else {
+          } else if (StringUtils.equalsIgnoreCase(commandSplit[3], FIND_BACK) ||
+              StringUtils.equalsIgnoreCase(commandSplit[3], SYNC_METADATA)) {
             return repairCallGenerator.generate(commandSplit[1], commandSplit[3], null);
           }
         } else {
@@ -72,10 +79,14 @@ public class SimpleRegexCommandParser implements CommandParser {
       case USE:
         return useCallGenerator.generate(commandSplit[1]);
       case OPTIMIZE:
-        if (commandSplit.length < 3) {
+        if (!(commandSplit.length < 3)) {
+          if (StringUtils.equalsIgnoreCase(commandSplit[1], START) ||
+              StringUtils.equalsIgnoreCase(commandSplit[1], STOP)) {
+            return optimizeCallGenerator.generate(commandSplit[1], commandSplit[2]);
+          }
+        } else {
           throw new IllegalCommandException("Please check if your command is correct!");
         }
-        return optimizeCallGenerator.generate(commandSplit[1], commandSplit[2]);
       case REFRESH:
         if (StringUtils.equalsIgnoreCase(commandSplit[1], FILE_CACHE) && !(commandSplit.length < 3)) {
           return refreshCallGenerator.generate(commandSplit[2]);
@@ -83,7 +94,12 @@ public class SimpleRegexCommandParser implements CommandParser {
           throw new IllegalCommandException("Please check if your command is correct!");
         }
       case SHOW:
-        return showCallGenerator.generate(commandSplit[1]);
+        if (StringUtils.equalsIgnoreCase(commandSplit[1], DATABASES) ||
+            StringUtils.equalsIgnoreCase(commandSplit[1], TABLES)) {
+          return showCallGenerator.generate(commandSplit[1]);
+        } else {
+          throw new IllegalCommandException("Please check if your command is correct!");
+        }
     }
     throw new IllegalCommandException("Please check if your command is correct!");
   }
