467497b4215bf875530a22251c9afee46f7aaaf4
shidayang <530847445@qq.com>
2023-02-27 16:20:26 +0800

    Update ShowCall.java

--- ams/ams-server/src/main/java/com/netease/arctic/ams/server/repair/command/ShowCall.java
+++ ams/ams-server/src/main/java/com/netease/arctic/ams/server/repair/command/ShowCall.java
@@ -4,14 +4,14 @@ import com.netease.arctic.ams.server.repair.Context;
 
 public class ShowCall implements CallCommand{
 
-  public enum namespaces {
-    DATABASES, TABLES
-  }
-
   private namespaces namespaces;
 
   @Override
   public String call(Context context) {
     return null;
+  }
+  
+  public enum namespaces {
+    DATABASES, TABLES
   }
 }
