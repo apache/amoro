05076087ebae89ed99f44ac093ea56c48578e455
huangjianmin <531493269@qq.com>
2023-03-01 16:08:49 +0800

    Optimize some logics.

--- ams/ams-server/src/main/java/com/netease/arctic/ams/server/repair/command/ShowCall.java
+++ ams/ams-server/src/main/java/com/netease/arctic/ams/server/repair/command/ShowCall.java
@@ -19,28 +19,23 @@ public class ShowCall implements CallCommand {
 
   @Override
   public String call(Context context) throws FullTableNameException {
-    switch (this.namespaces) {
-      case CATALOGS:
-        return catalogManager.catalogs().stream().collect(Collectors.joining("\\n"));
-      case DATABASES:
-      case TABLES:
-        if (context.getCatalog() == null) {
-          throw new FullTableNameException("Can not find catalog name, your can use 'USE ${catalog}' statement");
-        }
-        ArcticCatalog arcticCatalog = catalogManager.getArcticCatalog(context.getCatalog());
-        if (this.namespaces == Namespaces.DATABASES) {
-          return arcticCatalog.listDatabases().stream().collect(Collectors.joining("\\n"));
-        } else {
-          if (context.getDb() == null) {
-            throw new FullTableNameException("Can not find database name, your can use 'USE ${database}' statement");
-          }
-          return arcticCatalog.listTables(context.getDb())
-              .stream()
-              .map(TableIdentifier::getTableName)
-              .collect(Collectors.joining("\\n"));
-        }
-      default:
-        throw new UnsupportedOperationException("not support show operation named:" + this.namespaces);
+    if (this.namespaces == Namespaces.CATALOGS){
+      return catalogManager.catalogs().stream().collect(Collectors.joining("\\n"));
+    }
+    if (context.getCatalog() == null) {
+      throw new FullTableNameException("Can not find catalog name, your can use 'USE ${catalog}' statement");
+    }
+    ArcticCatalog arcticCatalog = catalogManager.getArcticCatalog(context.getCatalog());
+    if (this.namespaces == Namespaces.DATABASES) {
+      return arcticCatalog.listDatabases().stream().collect(Collectors.joining("\\n"));
+    } else {
+      if (context.getDb() == null) {
+        throw new FullTableNameException("Can not find database name, your can use 'USE ${database}' statement");
+      }
+      return arcticCatalog.listTables(context.getDb())
+          .stream()
+          .map(TableIdentifier::getTableName)
+          .collect(Collectors.joining("\\n"));
     }
   }
 
