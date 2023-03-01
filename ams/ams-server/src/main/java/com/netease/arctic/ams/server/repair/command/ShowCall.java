93f3bdf760f6bca4a1f272f8d9b2449e5a47379a
huangjianmin <531493269@qq.com>
2023-03-01 16:01:14 +0800

    Optimize some logics.

--- ams/ams-server/src/main/java/com/netease/arctic/ams/server/repair/command/ShowCall.java
+++ ams/ams-server/src/main/java/com/netease/arctic/ams/server/repair/command/ShowCall.java
@@ -4,6 +4,7 @@ import com.netease.arctic.ams.server.repair.Context;
 import com.netease.arctic.catalog.ArcticCatalog;
 import com.netease.arctic.catalog.CatalogManager;
 import com.netease.arctic.table.TableIdentifier;
+
 import java.util.stream.Collectors;
 
 public class ShowCall implements CallCommand {
@@ -18,20 +19,26 @@ public class ShowCall implements CallCommand {
 
   @Override
   public String call(Context context) throws FullTableNameException {
-    if (context.getCatalog() == null) {
-      throw new FullTableNameException("Can not find catalog name, your can use 'USE ${catalog}' statement");
-    }
-    ArcticCatalog arcticCatalog = catalogManager.getArcticCatalog(context.getCatalog());
     switch (this.namespaces) {
       case CATALOGS:
         return catalogManager.catalogs().stream().collect(Collectors.joining("\\n"));
       case DATABASES:
-        return arcticCatalog.listDatabases().stream().collect(Collectors.joining("\\n"));
       case TABLES:
-        return arcticCatalog.listTables(context.getDb())
-            .stream()
-            .map(TableIdentifier::getTableName)
-            .collect(Collectors.joining("\\n"));
+        if (context.getCatalog() == null) {
+          throw new FullTableNameException("Can not find catalog name, your can use 'USE ${catalog}' statement");
+        }
+        ArcticCatalog arcticCatalog = catalogManager.getArcticCatalog(context.getCatalog());
+        if (this.namespaces == Namespaces.DATABASES) {
+          return arcticCatalog.listDatabases().stream().collect(Collectors.joining("\\n"));
+        } else {
+          if (context.getDb() == null) {
+            throw new FullTableNameException("Can not find database name, your can use 'USE ${database}' statement");
+          }
+          return arcticCatalog.listTables(context.getDb())
+              .stream()
+              .map(TableIdentifier::getTableName)
+              .collect(Collectors.joining("\\n"));
+        }
       default:
         throw new UnsupportedOperationException("not support show operation named:" + this.namespaces);
     }
