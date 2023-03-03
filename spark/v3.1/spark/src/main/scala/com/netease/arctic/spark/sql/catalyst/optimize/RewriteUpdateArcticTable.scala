a835fb359ae6c769f94da00b8cc9d5f4b93de52a
jinsilei <jinsilei@corp.netease.com>
2023-03-03 14:32:33 +0800

    remove useless code

--- spark/v3.1/spark/src/main/scala/com/netease/arctic/spark/sql/catalyst/optimize/RewriteUpdateArcticTable.scala
+++ spark/v3.1/spark/src/main/scala/com/netease/arctic/spark/sql/catalyst/optimize/RewriteUpdateArcticTable.scala
@@ -189,27 +189,4 @@ case class RewriteUpdateArcticTable(spark: SparkSession) extends Rule[LogicalPla
     })
     Project(outputWithValues, scanPlan)
   }
-
-  def buildJoinCondition(primaries: util.List[String], r: DataSourceV2Relation, insertPlan: LogicalPlan): Expression =  {
-    var i = 0
-    var joinCondition: Expression = null
-    var expressions: List[Expression] = List.empty
-    while ( i < primaries.size) {
-      val primary = primaries.get(i)
-      val primaryAttr = r.output.find(_.name == primary).get
-      val joinAttribute = insertPlan.output.find(_.name.replace("_arctic_after_", "") == primary).get
-      val experssion = EqualTo(primaryAttr, joinAttribute)
-      expressions = expressions :+ experssion
-      i += 1
-    }
-    expressions.foreach(e => {
-      if (joinCondition == null) {
-        joinCondition = e
-      } else {
-        joinCondition = And(joinCondition, e)
-      }
-    });
-    joinCondition
-  }
-
 }
