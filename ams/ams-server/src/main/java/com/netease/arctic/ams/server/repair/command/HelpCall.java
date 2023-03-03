94189a50ce6849ef5a2f2ededb66cd536918541b
huangjianmin <531493269@qq.com>
2023-03-03 14:23:13 +0800

    Add new option for JVM memory in repari.sh. Add repair.md for repair instruction document.

--- ams/ams-server/src/main/java/com/netease/arctic/ams/server/repair/command/HelpCall.java
+++ ams/ams-server/src/main/java/com/netease/arctic/ams/server/repair/command/HelpCall.java
@@ -7,26 +7,26 @@ public class HelpCall implements CallCommand {
   public String call(Context context) {
     return
         "Usage:\n" +
-        "\n" +
-        "SHOW [ CATALOGS | DATABASES | TABLES ]                      -- Show catalogs, databases or tables --\n" +
-        "\n" +
-        "USE [ ${catalog_name} | ${database_name} ]                  -- Use catalog or database --\n" +
-        "\n" +
-        "ANALYZE ${table_name}                                       -- Analyze your table which will" +
+            "\n" +
+            "SHOW [ CATALOGS | DATABASES | TABLES ]                      -- Show catalogs, databases or tables --\n" +
+            "\n" +
+            "USE [ ${catalog_name} | ${database_name} ]                  -- Use catalog or database --\n" +
+            "\n" +
+            "ANALYZE ${table_name}                                       -- Analyze your table which will" +
             " list the lost files and provide you with repair options --\n" +
-        "\n" +
-        "REPAIR ${table_name} THROUGH FIND_BACK                      -- Retrieve your lost files from trash --\n" +
-        "REPAIR ${table_name} THROUGH SYNC_METADATA                  -- Sync metadata which may" +
+            "\n" +
+            "REPAIR ${table_name} THROUGH FIND_BACK                      -- Retrieve your lost files from trash --\n" +
+            "REPAIR ${table_name} THROUGH SYNC_METADATA                  -- Sync metadata which may" +
             " delete non-existent files in the metadata --\n" +
-        "REPAIR ${table_name} THROUGH ROLLBACK ${snapshot_id}        -- Roll back to a snapshot" +
+            "REPAIR ${table_name} THROUGH ROLLBACK ${snapshot_id}        -- Roll back to a snapshot" +
             " without lost files --\n" +
-        "\n" +
-        "OPTIMIZE [ STOP | START ] ${table_name}                     -- Start or stop optimizing tasks" +
+            "\n" +
+            "OPTIMIZE [ STOP | START ] ${table_name}                     -- Start or stop optimizing tasks" +
             " for the table --\n" +
-        "\n" +
-        "REFRESH FILE_CACHE ${table_name}                            -- Flush the file cache for the table --\n" +
-        "\n" +
-        "QUIT                                                        -- Exit --"
+            "\n" +
+            "REFRESH FILE_CACHE ${table_name}                            -- Flush the file cache for the table --\n" +
+            "\n" +
+            "QUIT                                                        -- Exit --"
         ;
   }
 }
