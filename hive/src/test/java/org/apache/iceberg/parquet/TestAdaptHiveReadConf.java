f871b99bb1db91113358c6d4b19ee45e73dbd87d
wangzeyu <hameizi369@gmail.com>
2023-03-16 15:31:53 +0800

    add AdaptHiveApplyNameMapping

--- hive/src/test/java/org/apache/iceberg/parquet/TestAdaptHiveReadConf.java
+++ hive/src/test/java/org/apache/iceberg/parquet/TestAdaptHiveReadConf.java
@@ -18,7 +18,9 @@
 
 package org.apache.iceberg.parquet;
 
+import com.google.common.collect.Lists;
 import org.apache.iceberg.mapping.MappedField;
+import org.apache.iceberg.mapping.MappedFields;
 import org.apache.iceberg.mapping.NameMapping;
 import org.apache.parquet.schema.MessageType;
 import org.apache.parquet.schema.PrimitiveType;
@@ -31,13 +33,18 @@ public class TestAdaptHiveReadConf {
 
   @Test
   public void testConvertNameMapping() {
-    String fieldName = "Test";
-    MessageType fileSchema = Types.buildMessage().addField(Types.primitive(PrimitiveType.PrimitiveTypeName.DOUBLE,
-        Type.Repetition.REQUIRED).named(fieldName)).named("table");
-    MappedField mappedField = MappedField.of(1, fieldName.toLowerCase());
+    MessageType fileSchema = Types.buildMessage().addField(Types.buildGroup(Type.Repetition.OPTIONAL)
+        .addField(Types.primitive(PrimitiveType.PrimitiveTypeName.DOUBLE, Type.Repetition.REQUIRED).named("X"))
+        .addField(Types.primitive(PrimitiveType.PrimitiveTypeName.DOUBLE, Type.Repetition.REQUIRED).named("Y"))
+        .named("custom_element_name".toUpperCase())).named("table");
+    MappedFields mappedFields = MappedFields.of(Lists.newArrayList(
+        MappedField.of(2, "x"),
+        MappedField.of(3, "y")));
+    MappedField mappedField = MappedField.of(1, "custom_element_name", mappedFields);
     NameMapping nameMapping = NameMapping.of(mappedField);
-    NameMapping actual = AdaptHiveReadConf.convertNameMapping(fileSchema, nameMapping);
-    Assert.assertNotNull(actual.find(fieldName));
-    Assert.assertEquals(1, (int) actual.find(fieldName).id());
+    MessageType actual = (MessageType) ParquetTypeVisitor.visit(fileSchema, new AdaptHiveApplyNameMapping(nameMapping));
+    Assert.assertEquals("custom_element_name".toUpperCase(), actual.getFields().get(0).getName());
+    Assert.assertEquals(1, actual.getFields().get(0).getId().intValue());
+    Assert.assertEquals(2, actual.getFields().get(0).asGroupType().getType("X").getId().intValue());
   }
 }
