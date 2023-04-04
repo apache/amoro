1d32299c62526be4151ba1418efb9421a37fa6bd
jinsilei <jinsilei@corp.netease.com>
2023-04-04 19:49:06 +0800

    fix

--- spark/v3.1/spark/src/main/scala/org/apache/spark/sql/arctic/catalyst/ArcticSpark31CatalystHelper.scala
+++ spark/v3.1/spark/src/main/scala/org/apache/spark/sql/arctic/catalyst/ArcticSpark31CatalystHelper.scala
@@ -25,13 +25,13 @@ import org.apache.iceberg.Schema
 import org.apache.iceberg.spark.SparkSchemaUtil
 import org.apache.spark.sql.{catalyst, connector, AnalysisException}
 import org.apache.spark.sql.catalyst.{InternalRow, SQLConfHelper}
-import org.apache.spark.sql.catalyst.expressions.{Expression, IcebergBucketTransform, IcebergDayTransform, IcebergHourTransform, IcebergMonthTransform, IcebergYearTransform, NamedExpression, NullIntolerant}
+import org.apache.spark.sql.catalyst.expressions.{Expression, IcebergBucketTransform, IcebergDayTransform, IcebergHourTransform, IcebergMonthTransform, IcebergTruncateTransform, IcebergYearTransform, NamedExpression, NullIntolerant}
 import org.apache.spark.sql.catalyst.expressions.codegen.CodegenFallback
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.connector.catalog.CatalogV2Implicits
 import org.apache.spark.sql.connector.expressions._
 import org.apache.spark.sql.connector.iceberg.expressions.{NullOrdering, SortDirection, SortOrder}
-import org.apache.spark.sql.types.{DataType, LongType}
+import org.apache.spark.sql.types.{DataType, IntegerType, LongType}
 
 object ArcticSpark31CatalystHelper extends SQLConfHelper {
 
