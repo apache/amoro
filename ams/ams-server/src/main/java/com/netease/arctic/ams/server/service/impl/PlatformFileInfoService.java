1acbed4cc0d165295917f4d355fdc31fc7fb46a8
hzwangtao6 <hzwangtao6@corp.netease.com>
2023-04-23 09:45:48 +0800

    [Arctic-1120]remove unused method

--- ams/ams-server/src/main/java/com/netease/arctic/ams/server/service/impl/PlatformFileInfoService.java
+++ ams/ams-server/src/main/java/com/netease/arctic/ams/server/service/impl/PlatformFileInfoService.java
@@ -42,19 +42,6 @@ public class PlatformFileInfoService extends IJDBCService {
     }
   }
 
-  /**
-   * get file content
-   * @param fileId
-   * @return
-   */
-  public String getFileContentById(Integer fileId) {
-    try (SqlSession sqlSession = getSqlSession(true)) {
-      PlatformFileInfoMapper platformFileInfoMapper =
-              getMapper(sqlSession, PlatformFileInfoMapper.class);
-      return new String(Base64.getDecoder().decode(platformFileInfoMapper.getFileById(fileId)));
-    }
-  }
-
   public byte[] getFileContentBytesById(Integer fileId) {
     try (SqlSession sqlSession = getSqlSession(true)) {
       PlatformFileInfoMapper platformFileInfoMapper =
