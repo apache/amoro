35c1d23e1fb9a0158a110e13df985495591ab5ec
BaiyangTX <xiangnebula@163.com>
2023-05-06 14:09:45 +0800

    [ARCTIC-1062][AMS]Terminal support config spark properties in the local model (#1094)

--- ams/server/src/main/java/com/netease/arctic/server/terminal/local/LocalSessionFactory.java
+++ ams/server/src/main/java/com/netease/arctic/server/terminal/local/LocalSessionFactory.java
@@ -21,9 +21,11 @@ package com.netease.arctic.server.terminal.local;
 import com.netease.arctic.server.terminal.SparkContextUtil;
 import com.netease.arctic.server.terminal.TerminalSession;
 import com.netease.arctic.server.terminal.TerminalSessionFactory;
+import com.netease.arctic.server.utils.ConfigOptions;
 import com.netease.arctic.server.utils.Configurations;
 import com.netease.arctic.spark.ArcticSparkExtensions;
 import com.netease.arctic.table.TableMetaStore;
+import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
 import org.apache.iceberg.relocated.com.google.common.collect.Maps;
 import org.apache.iceberg.relocated.com.google.common.collect.Sets;
@@ -42,12 +44,14 @@ public class LocalSessionFactory implements TerminalSessionFactory {
   static final Set<String> STATIC_SPARK_CONF = Collections.unmodifiableSet(
       Sets.newHashSet("spark.sql.extensions")
   );
+  static final String SPARK_CONF_PREFIX = "spark.";
 
   SparkSession context = null;
+  Configurations conf;
 
   @Override
   public void initialize(Configurations properties) {
-
+    this.conf = properties;
   }
 
   @Override
@@ -82,6 +86,7 @@ public class LocalSessionFactory implements TerminalSessionFactory {
   }
 
   protected synchronized SparkSession lazyInitContext() {
+    Preconditions.checkNotNull(this.conf);
     if (context == null) {
       SparkConf sparkconf = new SparkConf()
           .setAppName("spark-local-context")
@@ -91,6 +96,14 @@ public class LocalSessionFactory implements TerminalSessionFactory {
       sparkconf.set("spark.network.timeout", "200s");
       sparkconf.set("spark.sql.extensions", ArcticSparkExtensions.class.getName() +
           "," + IcebergSparkSessionExtensions.class.getName());
+
+      for (String key : this.conf.keySet()) {
+        if (key.startsWith(SPARK_CONF_PREFIX)) {
+          String value = this.conf.getValue(ConfigOptions.key(key).stringType().noDefaultValue());
+          sparkconf.set(key, value);
+        }
+      }
+
       context = SparkSession
           .builder()
           .config(sparkconf)
