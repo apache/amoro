9cb2e7dc2e045c857335b883f593fef13f7f8b84
HuangFru <68625618+HuangFru@users.noreply.github.com>
2023-05-11 13:20:01 +0800

    [Arctic-1372][Refactor-AMS] Add unit test for executors (#1434)
    * resolve compile error.
    
    * resolve compile error.
    
    * add Executor unit test

---
+++ ams/server/src/test/java/com/netease/arctic/server/persistence/excutors/TestOrphanFileCleanHive.java
@@ -0,0 +1,111 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netease.arctic.server.persistence.excutors;
+
+import com.netease.arctic.ams.api.TableFormat;
+import com.netease.arctic.catalog.TableTestBase;
+import com.netease.arctic.hive.TestHMS;
+import com.netease.arctic.hive.catalog.HiveCatalogTestHelper;
+import com.netease.arctic.hive.catalog.HiveTableTestHelper;
+import com.netease.arctic.hive.table.SupportHive;
+import com.netease.arctic.server.dashboard.utils.AmsUtil;
+import com.netease.arctic.server.table.ServerTableIdentifier;
+import com.netease.arctic.server.table.TableRuntime;
+import com.netease.arctic.server.table.executor.OrphanFilesCleaningExecutor;
+import com.netease.arctic.table.UnkeyedTable;
+import org.apache.iceberg.io.OutputFile;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.netease.arctic.server.table.executor.OrphanFilesCleaningExecutor.DATA_FOLDER_NAME;
+
+@RunWith(Parameterized.class)
+public class TestOrphanFileCleanHive extends TableTestBase {
+
+  @ClassRule
+  public static TestHMS TEST_HMS = new TestHMS();
+
+  public TestOrphanFileCleanHive(boolean ifKeyed, boolean ifPartitioned) {
+    super(new HiveCatalogTestHelper(TableFormat.MIXED_HIVE, TEST_HMS.getHiveConf()),
+        new HiveTableTestHelper(ifKeyed, ifPartitioned));
+  }
+
+  @Parameterized.Parameters(name = "ifKeyed = {0}, ifPartitioned = {1}")
+  public static Object[][] parameters() {
+    return new Object[][]{
+        {true, true},
+        {true, false},
+        {false, true},
+        {false, false}};
+  }
+
+  private static OrphanFilesCleaningExecutor orphanFilesCleaningExecutor;
+
+  @Before
+  public void mock() {
+    orphanFilesCleaningExecutor = Mockito.mock(OrphanFilesCleaningExecutor.class);
+    TableRuntime tableRuntime = Mockito.mock(TableRuntime.class);
+    Mockito.when(tableRuntime.loadTable()).thenReturn(getArcticTable());
+    Mockito.when(tableRuntime.getTableIdentifier()).thenReturn(
+        ServerTableIdentifier.of(AmsUtil.toTableIdentifier(getArcticTable().id())));
+    Mockito.doCallRealMethod().when(orphanFilesCleaningExecutor).execute(tableRuntime);
+  }
+
+  @Test
+  public void orphanDataFileClean() throws IOException {
+    UnkeyedTable baseTable = isKeyedTable() ?
+        getArcticTable().asKeyedTable().baseTable() : getArcticTable().asUnkeyedTable();
+    String baseOrphanFilePath = baseTable.location() +
+        File.separator + DATA_FOLDER_NAME + File.separator + "orphan.parquet";
+    String hiveOrphanFilePath = ((SupportHive) getArcticTable()).hiveLocation() +
+        File.separator + DATA_FOLDER_NAME + File.separator + "orphan.parquet";
+    OutputFile baseOrphanDataFile = getArcticTable().io().newOutputFile(baseOrphanFilePath);
+    baseOrphanDataFile.createOrOverwrite().close();
+    OutputFile changeOrphanDataFile = getArcticTable().io().newOutputFile(hiveOrphanFilePath);
+    changeOrphanDataFile.createOrOverwrite().close();
+    Assert.assertTrue(getArcticTable().io().exists(baseOrphanFilePath));
+    Assert.assertTrue(getArcticTable().io().exists(hiveOrphanFilePath));
+    orphanFilesCleaningExecutor.cleanContentFiles(getArcticTable(), System.currentTimeMillis());
+    Assert.assertFalse(getArcticTable().io().exists(baseOrphanFilePath));
+    Assert.assertTrue(getArcticTable().io().exists(hiveOrphanFilePath));
+  }
+
+  @Test
+  public void orphanMetadataFileClean() throws IOException {
+    UnkeyedTable baseTable = isKeyedTable() ?
+        getArcticTable().asKeyedTable().baseTable() : getArcticTable().asUnkeyedTable();
+    String orphanFilePath = baseTable.location() + File.separator + "metadata" +
+        File.separator + "orphan.avro";
+    OutputFile baseOrphanDataFile = getArcticTable().io().newOutputFile(orphanFilePath);
+    baseOrphanDataFile.createOrOverwrite().close();
+    Assert.assertTrue(getArcticTable().io().exists(orphanFilePath));
+    orphanFilesCleaningExecutor.cleanMetadata(getArcticTable(), System.currentTimeMillis());
+    Assert.assertFalse(getArcticTable().io().exists(orphanFilePath));
+    ExecutorTestUtil.assertMetadataExists(getArcticTable());
+  }
+
+}
---
+++ ams/server/src/test/java/com/netease/arctic/server/persistence/excutors/TestSnapshotExpireIceberg.java
@@ -0,0 +1,153 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netease.arctic.server.persistence.excutors;
+
+import com.netease.arctic.BasicTableTestHelper;
+import com.netease.arctic.ams.api.TableFormat;
+import com.netease.arctic.catalog.BasicCatalogTestHelper;
+import com.netease.arctic.catalog.TableTestBase;
+import com.netease.arctic.server.dashboard.utils.AmsUtil;
+import com.netease.arctic.server.optimizing.OptimizingStatus;
+import com.netease.arctic.server.table.ServerTableIdentifier;
+import com.netease.arctic.server.table.TableRuntime;
+import com.netease.arctic.server.table.executor.SnapshotsExpiringExecutor;
+import com.netease.arctic.table.TableProperties;
+import com.netease.arctic.table.UnkeyedTable;
+import org.apache.iceberg.AppendFiles;
+import org.apache.iceberg.DataFile;
+import org.apache.iceberg.DeleteFiles;
+import org.apache.iceberg.relocated.com.google.common.collect.Iterables;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static com.netease.arctic.server.persistence.excutors.ExecutorTestUtil.writeAndCommitBaseStore;
+
+@RunWith(Parameterized.class)
+public class TestSnapshotExpireIceberg extends TableTestBase {
+
+  public TestSnapshotExpireIceberg(boolean ifKeyed, boolean ifPartitioned) {
+    super(new BasicCatalogTestHelper(TableFormat.ICEBERG),
+        new BasicTableTestHelper(ifKeyed, ifPartitioned));
+  }
+
+  @Parameterized.Parameters(name = "ifKeyed = {0}, ifPartitioned = {1}")
+  public static Object[][] parameters() {
+    return new Object[][]{
+        {false, true},
+        {false, false}};
+  }
+
+
+  @Test
+  public void testExpireTableFiles() {
+    UnkeyedTable table = getArcticTable().asUnkeyedTable();
+    List<DataFile> dataFiles = writeAndCommitBaseStore(table);
+
+    DeleteFiles deleteFiles = table.newDelete();
+    for (DataFile dataFile : dataFiles) {
+      Assert.assertTrue(table.io().exists(dataFile.path().toString()));
+      deleteFiles.deleteFile(dataFile);
+    }
+    deleteFiles.commit();
+
+    List<DataFile> newDataFiles = writeAndCommitBaseStore(table);
+    Assert.assertEquals(3, Iterables.size(table.snapshots()));
+    SnapshotsExpiringExecutor.expireSnapshots(table, System.currentTimeMillis(),
+        new HashSet<>());
+    Assert.assertEquals(1, Iterables.size(table.snapshots()));
+
+    dataFiles.forEach(file -> Assert.assertFalse(table.io().exists(file.path().toString())));
+    newDataFiles.forEach(file -> Assert.assertTrue(table.io().exists(file.path().toString())));
+  }
+
+  @Test
+  public void testNotExpireFlinkLatestCommit() {
+    UnkeyedTable table = getArcticTable().asUnkeyedTable();
+    writeAndCommitBaseStore(table);
+    Assert.assertEquals(Long.MAX_VALUE,
+        SnapshotsExpiringExecutor.fetchLatestFlinkCommittedSnapshotTime(table));
+
+    AppendFiles appendFiles = table.newAppend();
+    appendFiles.set(SnapshotsExpiringExecutor.FLINK_MAX_COMMITTED_CHECKPOINT_ID, "100");
+    appendFiles.commit();
+    long checkpointTime = table.currentSnapshot().timestampMillis();
+    Assert.assertEquals(checkpointTime,
+        SnapshotsExpiringExecutor.fetchLatestFlinkCommittedSnapshotTime(table));
+
+    AppendFiles appendFiles2 = table.newAppend();
+    appendFiles2.set(SnapshotsExpiringExecutor.FLINK_MAX_COMMITTED_CHECKPOINT_ID, "101");
+    appendFiles2.commit();
+    long checkpointTime2 = table.currentSnapshot().timestampMillis();
+    Assert.assertEquals(checkpointTime2,
+        SnapshotsExpiringExecutor.fetchLatestFlinkCommittedSnapshotTime(table));
+
+    writeAndCommitBaseStore(table);
+    Assert.assertEquals(checkpointTime2,
+        SnapshotsExpiringExecutor.fetchLatestFlinkCommittedSnapshotTime(table));
+
+    table.updateProperties().set(TableProperties.BASE_SNAPSHOT_KEEP_MINUTES, "0").commit();
+    TableRuntime tableRuntime = Mockito.mock(TableRuntime.class);
+    Mockito.when(tableRuntime.getTableIdentifier()).thenReturn(
+        ServerTableIdentifier.of(AmsUtil.toTableIdentifier(table.id())));
+    Mockito.when(tableRuntime.getOptimizingStatus()).thenReturn(OptimizingStatus.IDLE);
+
+    Assert.assertEquals(4, Iterables.size(table.snapshots()));
+    SnapshotsExpiringExecutor.expireArcticTable(
+        table, tableRuntime);
+
+    Assert.assertEquals(2, Iterables.size(table.snapshots()));
+  }
+
+  @Test
+  public void testNotExpireOptimizeCommit() {
+    UnkeyedTable testTable = getArcticTable().asUnkeyedTable();
+    // commit snapshot
+    testTable.newAppend().commit();
+    testTable.newAppend().commit();
+    testTable.updateProperties().set(TableProperties.BASE_SNAPSHOT_KEEP_MINUTES, "0").commit();
+
+    TableRuntime tableRuntime = Mockito.mock(TableRuntime.class);
+    Mockito.when(tableRuntime.getTableIdentifier()).thenReturn(
+        ServerTableIdentifier.of(AmsUtil.toTableIdentifier(testTable.id())));
+    Mockito.when(tableRuntime.getOptimizingStatus()).thenReturn(OptimizingStatus.IDLE);
+    SnapshotsExpiringExecutor.expireArcticTable(testTable, tableRuntime);
+    Assert.assertEquals(1, Iterables.size(testTable.snapshots()));
+
+    testTable.newAppend().commit();
+
+    // mock tableRuntime which has optimizing task not committed
+    long optimizeSnapshotId = testTable.currentSnapshot().snapshotId();
+    Mockito.when(tableRuntime.getOptimizingStatus()).thenReturn(OptimizingStatus.COMMITTING);
+    Mockito.when(tableRuntime.getCurrentSnapshotId()).thenReturn(optimizeSnapshotId);
+
+
+    testTable.newAppend().commit();
+    testTable.newAppend().commit();
+
+    SnapshotsExpiringExecutor.expireArcticTable(testTable, tableRuntime);
+    Assert.assertEquals(3, Iterables.size(testTable.snapshots()));
+  }
+
+}
