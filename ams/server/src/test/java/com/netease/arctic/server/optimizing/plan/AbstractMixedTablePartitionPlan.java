f71d6f2242111b5d59ff3eae6e2bd279dcec26bf
wangtao <wangtao3@corp.netease.com>
2023-05-16 21:04:40 +0800

    refactor test case AbstractMixedTablePartitionPlan

--- ams/server/src/test/java/com/netease/arctic/server/optimizing/plan/AbstractMixedTablePartitionPlan.java
+++ ams/server/src/test/java/com/netease/arctic/server/optimizing/plan/AbstractMixedTablePartitionPlan.java
@@ -1,10 +1,15 @@
 package com.netease.arctic.server.optimizing.plan;
 
+import com.google.common.collect.Maps;
 import com.netease.arctic.TableTestHelper;
 import com.netease.arctic.catalog.CatalogTestHelper;
 import com.netease.arctic.catalog.TableTestBase;
+import com.netease.arctic.data.DataTreeNode;
 import com.netease.arctic.data.IcebergContentFile;
+import com.netease.arctic.data.PrimaryKeyedFile;
+import com.netease.arctic.hive.optimizing.MixFormatRewriteExecutorFactory;
 import com.netease.arctic.io.DataTestHelpers;
+import com.netease.arctic.optimizing.OptimizingInputProperties;
 import com.netease.arctic.server.dashboard.utils.AmsUtil;
 import com.netease.arctic.server.optimizing.OptimizingConfig;
 import com.netease.arctic.server.optimizing.scan.KeyedTableFileScanHelper;
@@ -22,6 +27,7 @@ import org.mockito.Mockito;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -62,7 +68,7 @@ public abstract class AbstractMixedTablePartitionPlan extends TableTestBase {
     transactionId = beginTransaction();
     DataTestHelpers.writeAndCommitBaseStore(getArcticTable(), transactionId, newRecords, false);
 
-    TableFileScanHelper tableFileScanHelper = getTableFileScanHelper();
+    TableFileScanHelper tableFileScanHelper = getBaseTableFileScanHelper();
     AbstractPartitionPlan partitionPlan = getPartitionPlan();
     List<TableFileScanHelper.FileScanResult> scan = tableFileScanHelper.scan();
     for (TableFileScanHelper.FileScanResult fileScanResult : scan) {
@@ -72,13 +78,38 @@ public abstract class AbstractMixedTablePartitionPlan extends TableTestBase {
     return partitionPlan.splitTasks(0);
   }
 
-  private void assertTask(TaskDescriptor expect, TaskDescriptor actual) {
+  protected Map<String, String> buildProperties() {
+    Map<String, String> properties = Maps.newHashMap();
+    properties.put(OptimizingInputProperties.TASK_EXECUTOR_FACTORY_IMPL,
+        MixFormatRewriteExecutorFactory.class.getName());
+    return properties;
+  }
+
+  protected Map<DataTreeNode, List<TableFileScanHelper.FileScanResult>> scanBaseFilesGroupByNode() {
+    TableFileScanHelper tableFileScanHelper = getBaseTableFileScanHelper();
+    List<TableFileScanHelper.FileScanResult> scan = tableFileScanHelper.scan();
+    return scan.stream().collect(Collectors.groupingBy(f -> {
+      PrimaryKeyedFile primaryKeyedFile = (PrimaryKeyedFile) f.file().internalFile();
+      return primaryKeyedFile.node();
+    }));
+  }
+
+  protected List<TableFileScanHelper.FileScanResult> scanBaseFiles() {
+    TableFileScanHelper tableFileScanHelper = getBaseTableFileScanHelper();
+    return tableFileScanHelper.scan();
+  }
+
+  protected void assertTask(TaskDescriptor expect, TaskDescriptor actual) {
     Assert.assertEquals(expect.getPartition(), actual.getPartition());
     assertFiles(expect.getInput().rewrittenDeleteFiles(), actual.getInput().rewrittenDeleteFiles());
     assertFiles(expect.getInput().rewrittenDataFiles(), actual.getInput().rewrittenDataFiles());
     assertFiles(expect.getInput().readOnlyDeleteFiles(), actual.getInput().readOnlyDeleteFiles());
     assertFiles(expect.getInput().rePosDeletedDataFiles(), actual.getInput().rePosDeletedDataFiles());
-    Assert.assertEquals(expect.properties(), actual.properties());
+    assertTaskProperties(expect.properties(), actual.properties());
+  }
+  
+  protected void assertTaskProperties(Map<String, String> expect, Map<String, String> actual) {
+    Assert.assertEquals(expect, actual);
   }
 
   private void assertFiles(IcebergContentFile<?>[] expect, IcebergContentFile<?>[] actual) {
@@ -100,7 +131,7 @@ public abstract class AbstractMixedTablePartitionPlan extends TableTestBase {
     return isPartitionedTable() ? "op_time_day=2022-01-01" : "";
   }
 
-  private TableFileScanHelper getTableFileScanHelper() {
+  private TableFileScanHelper getBaseTableFileScanHelper() {
     if (isKeyedTable()) {
       return new KeyedTableFileScanHelper(getArcticTable().asKeyedTable(),
           getArcticTable().asKeyedTable().baseTable().currentSnapshot().snapshotId(), -1, null, null);
@@ -108,7 +139,6 @@ public abstract class AbstractMixedTablePartitionPlan extends TableTestBase {
       return new UnkeyedTableFileScanHelper(getArcticTable().asUnkeyedTable(),
           getArcticTable().asUnkeyedTable().currentSnapshot().snapshotId());
     }
-
   }
 
   private long beginTransaction() {
--- ams/server/src/test/java/com/netease/arctic/server/optimizing/plan/UnkeyedPartitionPlan.java
+++ ams/server/src/test/java/com/netease/arctic/server/optimizing/plan/UnkeyedPartitionPlan.java
@@ -1,36 +1,64 @@
 package com.netease.arctic.server.optimizing.plan;
 
+import com.google.common.collect.Maps;
 import com.netease.arctic.BasicTableTestHelper;
+import com.netease.arctic.TableTestHelper;
 import com.netease.arctic.ams.api.TableFormat;
 import com.netease.arctic.catalog.BasicCatalogTestHelper;
+import com.netease.arctic.catalog.CatalogTestHelper;
+import com.netease.arctic.data.IcebergContentFile;
+import com.netease.arctic.data.IcebergDataFile;
+import com.netease.arctic.hive.optimizing.MixFormatRewriteExecutorFactory;
+import com.netease.arctic.optimizing.OptimizingInputProperties;
+import com.netease.arctic.optimizing.RewriteFilesInput;
+import com.netease.arctic.server.optimizing.scan.TableFileScanHelper;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RunWith(Parameterized.class)
 public class UnkeyedPartitionPlan extends AbstractMixedTablePartitionPlan {
 
-  public UnkeyedPartitionPlan(boolean hasPartition) {
-    super(new BasicCatalogTestHelper(TableFormat.MIXED_ICEBERG),
-        new BasicTableTestHelper(false, hasPartition));
+  public UnkeyedPartitionPlan(CatalogTestHelper catalogTestHelper,
+                              TableTestHelper tableTestHelper) {
+    super(catalogTestHelper, tableTestHelper);
   }
 
-  @Parameterized.Parameters(name = "hasPartition={0}")
+  @Parameterized.Parameters(name = "{0}, {1}")
   public static Object[][] parameters() {
     return new Object[][] {
-        {true},
-        {false}};
+        {new BasicCatalogTestHelper(TableFormat.MIXED_ICEBERG),
+            new BasicTableTestHelper(false, true)},
+        {new BasicCatalogTestHelper(TableFormat.MIXED_ICEBERG),
+            new BasicTableTestHelper(false, false)}};
   }
 
   @Test
   public void testFragmentFiles() {
     List<TaskDescriptor> taskDescriptors = testOptimizeFragmentFiles();
     Assert.assertEquals(1, taskDescriptors.size());
-    
-    // TODO
+    List<TableFileScanHelper.FileScanResult> baseFiles = scanBaseFiles();
+    Assert.assertEquals(2, baseFiles.size());
+    List<IcebergDataFile> files = baseFiles.stream()
+        .map(TableFileScanHelper.FileScanResult::file)
+        .collect(Collectors.toList());
+    TaskDescriptor actual = taskDescriptors.get(0);
+    RewriteFilesInput rewriteFilesInput = new RewriteFilesInput(files.toArray(new IcebergDataFile[0]),
+        Collections.emptySet().toArray(new IcebergDataFile[0]),
+        Collections.emptySet().toArray(new IcebergContentFile[0]),
+        Collections.emptySet().toArray(new IcebergContentFile[0]), getArcticTable());
+
+    Map<String, String> properties = Maps.newHashMap();
+    properties.put(OptimizingInputProperties.TASK_EXECUTOR_FACTORY_IMPL,
+        MixFormatRewriteExecutorFactory.class.getName());
+    TaskDescriptor expect = new TaskDescriptor(getPartition(), rewriteFilesInput, properties);
+    assertTask(expect, actual);
   }
 
   protected AbstractPartitionPlan getPartitionPlan() {
