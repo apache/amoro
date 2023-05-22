ca52e3c6a5d591b1a9e8d6d7c74f75e46a3e2514
jinsilei <jinsilei@corp.netease.com>
2023-05-22 16:54:36 +0800

    fix #1456

--- spark/v3.1/spark/src/test/java/com/netease/arctic/spark/hive/TestKeyedHiveInsertOverwriteStatic.java
+++ spark/v3.1/spark/src/test/java/com/netease/arctic/spark/hive/TestKeyedHiveInsertOverwriteStatic.java
@@ -20,7 +20,10 @@ package com.netease.arctic.spark.hive;
 
 
 import com.netease.arctic.spark.SparkTestBase;
+import com.netease.arctic.table.ArcticTable;
 import org.apache.hadoop.hive.metastore.api.Partition;
+import org.apache.iceberg.StructLike;
+import org.apache.iceberg.util.StructLikeMap;
 import org.apache.thrift.TException;
 import org.junit.After;
 import org.junit.Assert;
@@ -28,7 +31,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TestKeyedHiveInsertOverwriteStatic extends SparkTestBase {
   private final String database = "db";
@@ -113,5 +118,34 @@ public class TestKeyedHiveInsertOverwriteStatic extends SparkTestBase {
     rows = sql("select * from {0}.{1} order by id", database, table);
     Assert.assertEquals(5, rows.size());
     assertContainIdSet(rows, 0, 4, 5, 6, 2, 3);
+  }
+
+  @Test
+  public void testInsertOverwriteNonePartition() throws TException {
+    sql("create table {0}.{1}( \n" +
+        " id int, \n" +
+        " name string, \n" +
+        " data string, primary key(id))\n" +
+        " using arctic partitioned by (data) ", database, "testPks");
+    sql("insert overwrite {0}.{1} \n" +
+        "partition( dt = ''2021-1-1'')  select id, name from {0}.{2}", database, table, "testPks");
+
+    rows = sql("select * from {0}.{1} order by id", database, table);
+    Assert.assertEquals(2, rows.size());
+    List<Partition> partitions = hms.getClient().listPartitions(
+        database,
+        table,
+        (short) -1);
+    Assert.assertEquals(2, partitions.size());
+    ArcticTable t = loadTable(catalogNameHive, database, table);
+    StructLikeMap<Map<String, String>> partitionProperty = t.asKeyedTable().baseTable().partitionProperty();
+    Map<String, StructLike> icebergPartitionMap = new HashMap<>();
+    for (StructLike structLike : partitionProperty.keySet()) {
+      System.out.println(structLike);
+      icebergPartitionMap.put(t.spec().partitionToPath(structLike), structLike);
+    }
+    Map<String, String> propertiesMap = partitionProperty.get(icebergPartitionMap.get("dt=2021-1-1"));
+    Assert.assertFalse(propertiesMap.containsKey("hive-location"));
+    sql("drop table if exists {0}.{1}", database, "testPks");
   }
 }
