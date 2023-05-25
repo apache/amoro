5b11023156fe91156ca69052f9550dfad6c3f94c
wangtao <wangtao3@corp.netease.com>
2023-05-25 16:04:03 +0800

    fix checkstyle

--- ams/server/src/main/java/com/netease/arctic/server/dashboard/ServerTableDescriptor.java
+++ ams/server/src/main/java/com/netease/arctic/server/dashboard/ServerTableDescriptor.java
@@ -25,8 +25,6 @@ import org.apache.iceberg.TableMetadataParser;
 import org.apache.iceberg.util.PropertyUtil;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
