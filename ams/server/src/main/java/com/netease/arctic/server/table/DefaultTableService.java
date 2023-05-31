313bd2fb0bbb15eb3c390158ea5fc794e47d124a
HuangFru <68625618+HuangFru@users.noreply.github.com>
2023-05-31 10:12:47 +0800

    [Refactor-AMS] Fix TableRuntime recreate (#1488)
    * resolve compile error.
    
    * resolve compile error.
    
    * add Executor unit test
    
    * refactor package level
    
    * abstracted test code to reduce duplication of code
    
    * fix some bugs and polish ut test
    
    * fix ut case
    
    * fix some bugs
    
    * fix some bugs
    
    * fix some bugs
    
    * fix some bugs
    
    * fix some bugs
    
    * fix some bugs
    
    * modify upgrade sql
    
    * fix table runtime recreate
    
    * add unique to derby init
    
    * resolve conflict
    
    * resolve conflict
    
    * resolve conflict
    
    * resolve conflict
    
    * resolve conflict

--- ams/server/src/main/java/com/netease/arctic/server/table/DefaultTableService.java
+++ ams/server/src/main/java/com/netease/arctic/server/table/DefaultTableService.java
@@ -438,21 +438,23 @@ public class DefaultTableService extends PersistentBase implements TableService 
   }
 
   private void syncTable(ExternalCatalog externalCatalog, TableIdentity tableIdentity) {
+    doAsTransaction(
+        () -> externalCatalog.syncTable(tableIdentity.getDatabase(), tableIdentity.getTableName()),
+        () -> handleTableRuntimeAdded(externalCatalog, tableIdentity)
+    );
+  }
+
+  private void handleTableRuntimeAdded(ExternalCatalog externalCatalog, TableIdentity tableIdentity) {
     ServerTableIdentifier tableIdentifier =
-        externalCatalog.syncTable(tableIdentity.getDatabase(), tableIdentity.getTableName());
-    try {
-      ArcticTable table = externalCatalog.loadTable(
-          tableIdentifier.getDatabase(),
-          tableIdentifier.getTableName());
-      TableRuntime tableRuntime = new TableRuntime(tableIdentifier, this, table.properties());
-      tableRuntimeMap.put(tableIdentifier, tableRuntime);
-      if (headHandler != null) {
-        headHandler.fireTableAdded(table, tableRuntime);
-      }
-    } catch (Exception e) {
-      disposeTable(externalCatalog, tableIdentifier);
-      LOG.error("sync table {} error while build table runtime", tableIdentifier, e);
+        externalCatalog.getServerTableIdentifier(tableIdentity.getDatabase(), tableIdentity.getTableName());
+    ArcticTable table = externalCatalog.loadTable(
+        tableIdentifier.getDatabase(),
+        tableIdentifier.getTableName());
+    TableRuntime tableRuntime = new TableRuntime(tableIdentifier, this, table.properties());
+    if (headHandler != null) {
+      headHandler.fireTableAdded(table, tableRuntime);
     }
+    tableRuntimeMap.put(tableIdentifier, tableRuntime);
   }
 
   private void disposeTable(ExternalCatalog externalCatalog, ServerTableIdentifier tableIdentifier) {
