8c8a586b8b0cbf751cc8f8e4475200155fd2fbe8
Xianxun Ye <yxx_cmhd@163.com>
2023-06-08 00:06:26 +0800

    [ARCTIC-5] Support MOR with Flink Engine in runtime batch mode. (#1415)

--- flink/v1.14/flink/src/main/java/com/netease/arctic/flink/read/FlinkSplitPlanner.java
+++ flink/v1.14/flink/src/main/java/com/netease/arctic/flink/read/FlinkSplitPlanner.java
@@ -22,16 +22,23 @@ import com.netease.arctic.IcebergFileEntry;
 import com.netease.arctic.data.DataFileType;
 import com.netease.arctic.data.DefaultKeyedFile;
 import com.netease.arctic.flink.read.hybrid.split.ArcticSplit;
+import com.netease.arctic.flink.read.hybrid.split.ChangelogSplit;
+import com.netease.arctic.flink.read.hybrid.split.MergeOnReadSplit;
 import com.netease.arctic.flink.read.hybrid.split.SnapshotSplit;
 import com.netease.arctic.scan.ArcticFileScanTask;
 import com.netease.arctic.scan.BasicArcticFileScanTask;
 import com.netease.arctic.scan.CombinedScanTask;
+import com.netease.arctic.scan.KeyedTableScan;
 import com.netease.arctic.scan.TableEntriesScan;
 import com.netease.arctic.table.KeyedTable;
 import org.apache.iceberg.DataFile;
 import org.apache.iceberg.PartitionSpec;
+import org.apache.iceberg.expressions.Expression;
 import org.apache.iceberg.io.CloseableIterable;
 import org.apache.iceberg.io.CloseableIterator;
+import org.apache.iceberg.relocated.com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -51,11 +58,32 @@ import java.util.stream.Collectors;
  * An util class that plans arctic table(base and change) or just plans change table. invoked by arctic enumerator.
  */
 public class FlinkSplitPlanner {
+  private static final Logger LOG = LoggerFactory.getLogger(FlinkSplitPlanner.class);
+
   private FlinkSplitPlanner() {
   }
 
   public static List<ArcticSplit> planFullTable(KeyedTable keyedTable, AtomicInteger splitCount) {
     CloseableIterable<CombinedScanTask> combinedScanTasks = keyedTable.newScan().planTasks();
+    BaseAndChangeTask baseAndChangeTask = BaseAndChangeTask.of(combinedScanTasks);
+    return planFullTable(baseAndChangeTask, splitCount);
+  }
+
+  /**
+   * Plans full table scanning for a {@link KeyedTable} with optional filters and a specified split count.
+   *
+   * @param keyedTable The {@link KeyedTable} to scan.
+   * @param filters    Optional list of filters to apply to the scan.
+   * @param splitCount The atomic integer to track the split count.
+   * @return The list of planned {@link ArcticSplit} included {@link SnapshotSplit}, {@link ChangelogSplit}.
+   */
+  public static List<ArcticSplit> planFullTable(
+      KeyedTable keyedTable, List<Expression> filters, AtomicInteger splitCount) {
+    KeyedTableScan keyedTableScan = keyedTable.newScan();
+    if (filters != null) {
+      filters.forEach(keyedTableScan::filter);
+    }
+    CloseableIterable<CombinedScanTask> combinedScanTasks = keyedTableScan.planTasks();
     BaseAndChangeTask baseAndChangeTask = BaseAndChangeTask.of(combinedScanTasks);
     return planFullTable(baseAndChangeTask, splitCount);
   }
@@ -75,6 +103,35 @@ public class FlinkSplitPlanner {
     return allSplits;
   }
 
+  /**
+   * Plans full table scanning for a {@link KeyedTable} with optional filters and a specified split count.
+   *
+   * @param keyedTable The {@link KeyedTable} to scan.
+   * @param filters    Optional list of filters to apply to the scan.
+   * @param splitCount The atomic integer to track the split count.
+   * @return The list of planned {@link ArcticSplit} included {@link MergeOnReadSplit}.
+   */
+  public static List<ArcticSplit> mergeOnReadPlan(
+      KeyedTable keyedTable, List<Expression> filters, AtomicInteger splitCount) {
+    KeyedTableScan keyedTableScan = keyedTable.newScan();
+    if (filters != null) {
+      filters.forEach(keyedTableScan::filter);
+    }
+    CloseableIterable<CombinedScanTask> combinedScanTasks = keyedTableScan.planTasks();
+    List<ArcticSplit> morSplits = Lists.newArrayList();
+    try (CloseableIterator<CombinedScanTask> initTasks = combinedScanTasks.iterator()) {
+
+      while (initTasks.hasNext()) {
+        CombinedScanTask combinedScanTask = initTasks.next();
+        combinedScanTask.tasks().forEach(
+            keyedTableScanTask -> morSplits.add(new MergeOnReadSplit(splitCount.get(), keyedTableScanTask)));
+      }
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+    return morSplits;
+  }
+
   public static List<ArcticSplit> planChangeTable(TableEntriesScan tableEntriesScan, Long fromSequence,
                                                   PartitionSpec spec, AtomicInteger splitCount) {
     CloseableIterable<IcebergFileEntry> entries = tableEntriesScan.entries();
@@ -89,10 +146,11 @@ public class FlinkSplitPlanner {
     List<ArcticSplit> changeTasks = new ArrayList<>(transactionTasks.size());
     transactionTasks
         .forEach(transactionTask -> {
-          PartitionAndNodeGroup partitionAndNodeGroup = new PartitionAndNodeGroup()
-              .insertFileScanTask(transactionTask.insertTasks)
-              .deleteFileScanTask(transactionTask.deleteTasks)
-              .splitCount(splitCount);
+          PartitionAndNodeGroup partitionAndNodeGroup =
+              new PartitionAndNodeGroup()
+                  .insertFileScanTask(transactionTask.insertTasks)
+                  .deleteFileScanTask(transactionTask.deleteTasks)
+                  .splitCount(splitCount);
           changeTasks.addAll(partitionAndNodeGroup.planSplits());
         });
 
@@ -143,12 +201,16 @@ public class FlinkSplitPlanner {
                                               Long fromSequence) {
       try (CloseableIterator<IcebergFileEntry> entriesIterator = entries.iterator()) {
         Map<Long, TransactionTask> transactionTasks = new HashMap<>();
-
+        long startTime = System.currentTimeMillis();
+        int count = 0, greaterThanCount = 0;
+        LOG.info("Reading change log from {}", entriesIterator.getClass());
         while (entriesIterator.hasNext()) {
+          count++;
           IcebergFileEntry entry = entriesIterator.next();
           if (fromSequence != null && entry.getSequenceNumber() <= fromSequence) {
             continue;
           }
+          greaterThanCount++;
           DefaultKeyedFile keyedFile =
               DefaultKeyedFile.parseChange((DataFile) entry.getFile(), entry.getSequenceNumber());
           BasicArcticFileScanTask task = new BasicArcticFileScanTask(keyedFile, null, spec, null);
@@ -164,6 +226,8 @@ public class FlinkSplitPlanner {
                     task.fileType()));
           }
         }
+        LOG.info("Read change log from {} in {} ms, count: {}, greater than fromSequence {} count: {}.",
+            entriesIterator.getClass(), System.currentTimeMillis() - startTime, count, fromSequence, greaterThanCount);
         return new BaseAndChangeTask(Collections.emptySet(), transactionTasks);
       } catch (IOException e) {
         throw new UncheckedIOException(e);
