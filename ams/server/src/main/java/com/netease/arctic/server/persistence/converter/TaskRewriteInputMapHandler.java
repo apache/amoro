a800f0394bdac2850ad5d1752dcabb76918c488b
BaiyangTX <xiangnebula@163.com>
2023-07-04 19:40:21 +0800

    make TaskRewriteInputMapHandler extend Object2ByteArrayConvert

--- ams/server/src/main/java/com/netease/arctic/server/persistence/converter/TaskRewriteInputMapHandler.java
+++ ams/server/src/main/java/com/netease/arctic/server/persistence/converter/TaskRewriteInputMapHandler.java
@@ -3,19 +3,17 @@ package com.netease.arctic.server.persistence.converter;
 import com.google.common.collect.Maps;
 import com.netease.arctic.optimizing.RewriteFilesInput;
 import com.netease.arctic.utils.SerializationUtil;
-import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 
-import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 import java.util.Map;
 
-public class TaskRewriteInputMapHandler extends BaseTypeHandler<Map<Integer, RewriteFilesInput>> {
+public class TaskRewriteInputMapHandler extends Object2ByteArrayConvert<Map<Integer, RewriteFilesInput>> {
+
   @Override
-  public void setNonNullParameter(
+  public void setParameter(
       PreparedStatement ps, int i, Map<Integer, RewriteFilesInput> parameter, JdbcType jdbcType) throws SQLException {
     if (parameter == null) {
       ps.setNull(i, Types.BLOB);
@@ -24,34 +22,6 @@ public class TaskRewriteInputMapHandler extends BaseTypeHandler<Map<Integer, Rew
     byte[] values = serialize(parameter);
     ps.setBytes(i, values);
   }
-
-  @Override
-  public Map<Integer, RewriteFilesInput> getNullableResult(ResultSet rs, String columnName) throws SQLException {
-    byte[] bytes = rs.getBytes(columnName);
-    if (bytes == null) {
-      return null;
-    }
-    return SerializationUtil.simpleDeserialize(bytes);
-  }
-
-  @Override
-  public Map<Integer, RewriteFilesInput> getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-    byte[] bytes = rs.getBytes(columnIndex);
-    if (bytes == null) {
-      return null;
-    }
-    return SerializationUtil.simpleDeserialize(bytes);
-  }
-
-  @Override
-  public Map<Integer, RewriteFilesInput> getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-    byte[] bytes = cs.getBytes(columnIndex);
-    if (bytes == null) {
-      return null;
-    }
-    return SerializationUtil.simpleDeserialize(bytes);
-  }
-
 
   private byte[] serialize(Map<Integer, RewriteFilesInput> target) {
     Map<Integer, RewriteFilesInput> copied = Maps.newHashMapWithExpectedSize(target.size());
