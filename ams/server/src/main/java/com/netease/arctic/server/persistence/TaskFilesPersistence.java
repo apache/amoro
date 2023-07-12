f29139e256d12e32622f4fbc32a2e6ed6871b46b
ZhouJinsong <zhoujinsong0505@163.com>
2023-07-12 19:31:19 +0800

    Revert "[ARCTIC-1687] Exclude kryo dependency which in ams-core from ams-optimizer module" (#1692)
    Revert "[ARCTIC-1687] Exclude kryo dependency which in ams-core from ams-optimizer module (#1688)"
    
    This reverts commit 910393fda048d04a5a875c7ece3496b9040d07e5.

--- ams/server/src/main/java/com/netease/arctic/server/persistence/TaskFilesPersistence.java
+++ ams/server/src/main/java/com/netease/arctic/server/persistence/TaskFilesPersistence.java
@@ -6,8 +6,6 @@ import com.netease.arctic.server.optimizing.TaskRuntime;
 import com.netease.arctic.server.persistence.mapper.OptimizingMapper;
 import com.netease.arctic.server.utils.CompressUtil;
 import com.netease.arctic.utils.SerializationUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -16,8 +14,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class TaskFilesPersistence {
-
-  private static final Logger LOG = LoggerFactory.getLogger(TaskFilesPersistence.class);
 
   private static final DatabasePersistence persistence = new DatabasePersistence();
 
@@ -32,14 +28,7 @@ public class TaskFilesPersistence {
     if (bytes == null) {
       return Collections.emptyMap();
     } else {
-      byte[] originalBytes;
-      try {
-        originalBytes = CompressUtil.unGzip(bytes.get(0));
-      } catch (RuntimeException e) {
-        LOG.warn("Fail to unzip, use original bytes", e);
-        originalBytes = bytes.get(0);
-      }
-      return SerializationUtil.simpleDeserialize(originalBytes);
+      return SerializationUtil.simpleDeserialize(CompressUtil.unGzip(bytes.get(0)));
     }
   }
 
