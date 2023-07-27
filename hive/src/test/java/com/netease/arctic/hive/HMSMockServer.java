ed622ca04cf48c45dfd151e2334f703c98900273
BaiyangTX <xiangnebula@163.com>
2023-07-27 17:16:59 +0800

    remove commented codes.

--- hive/src/test/java/com/netease/arctic/hive/HMSMockServer.java
+++ hive/src/test/java/com/netease/arctic/hive/HMSMockServer.java
@@ -262,7 +262,6 @@ public class HMSMockServer {
   public HiveMetaStoreClient getClient() {
     if (client == null) {
       try {
-        // this.client = new HiveMetaStoreClient(hiveConf);
         this.client = HMS_CLIENT_CTOR.newInstance(hiveConf);
       } catch (Exception e) {
         throw new RuntimeException(e);
