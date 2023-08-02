e84287eed6ec421f14c84f5cb9900521a4032253
zhoujinsong <463763777@qq.com>
2023-08-02 12:55:28 +0800

    remove useless imports

--- ams/server/src/main/java/com/netease/arctic/server/table/executor/OptimizingCommitExecutor.java
+++ ams/server/src/main/java/com/netease/arctic/server/table/executor/OptimizingCommitExecutor.java
@@ -1,7 +1,6 @@
 package com.netease.arctic.server.table.executor;
 
 import com.netease.arctic.server.optimizing.OptimizingStatus;
-import com.netease.arctic.server.table.TableConfiguration;
 import com.netease.arctic.server.table.TableManager;
 import com.netease.arctic.server.table.TableRuntime;
 import com.netease.arctic.table.ArcticTable;
