579239774c31763ae6a004dae8008fa3c93e0557
baiyangtx <xiangnebula@163.com>
2023-08-04 09:26:58 +0800

    [AMORO-1782]: Support enviroment variable `AMORO_CONF_DIR` (#1785)
    * support env var AMORO_CONF_DIR
    
    * support env var AMORO_CONF_DIR
    
    * support env var AMORO_CONF_DIR
    
    * support env var AMORO_CONF_DIR
    
    * support env var AMORO_CONF_DIR

--- ams/api/src/main/java/com/netease/arctic/ams/api/Environments.java
+++ ams/api/src/main/java/com/netease/arctic/ams/api/Environments.java
@@ -3,12 +3,26 @@ package com.netease.arctic.ams.api;
 public class Environments {
 
   public static final String SYSTEM_ARCTIC_HOME = "ARCTIC_HOME";
+  public static final String AMORO_CONF_DIR = "AMORO_CONF_DIR";
+  public static final String AMORO_HOME = "AMORO_HOME";
 
-  public static String getArcticHome() {
+  public static String getHomePath() {
     String arcticHome = System.getenv(SYSTEM_ARCTIC_HOME);
     if (arcticHome != null) {
       return arcticHome;
     }
-    return System.getProperty(SYSTEM_ARCTIC_HOME);
+    arcticHome = System.getenv(AMORO_HOME);
+    if (arcticHome != null) {
+      return arcticHome;
+    }
+    return System.getProperty(AMORO_HOME);
+  }
+
+  public static String getConfigPath() {
+    String amoroConfDir = System.getenv(AMORO_CONF_DIR);
+    if (amoroConfDir != null) {
+      return amoroConfDir;
+    }
+    return getHomePath() + "/conf";
   }
 }
