9ba447a2331ca3279bc20bac2f578105d0caf315
wangtao <wangtao3@corp.netease.com>
2023-08-07 11:10:26 +0800

    unify retry task

--- ams/server/src/main/java/com/netease/arctic/server/optimizing/OptimizingQueue.java
+++ ams/server/src/main/java/com/netease/arctic/server/optimizing/OptimizingQueue.java
@@ -181,13 +181,15 @@ public class OptimizingQueue extends PersistentBase implements OptimizingService
       task.schedule(thread);
     } catch (Throwable throwable) {
       LOG.error("Schedule task {} failed, put it to retry queue", task.getTaskId(), throwable);
-      retryQueue.offer(task);
+      retryTask(task, false);
       throw throwable;
     }
   }
 
   private void retryTask(TaskRuntime taskRuntime, boolean incRetryCount) {
-    taskRuntime.reset(incRetryCount);
+    if (incRetryCount || taskRuntime.getStatus() != TaskRuntime.Status.PLANNED) {
+      taskRuntime.reset(incRetryCount);
+    }
     retryQueue.offer(taskRuntime);
   }
 
@@ -236,14 +238,9 @@ public class OptimizingQueue extends PersistentBase implements OptimizingService
     suspendingTasks.forEach(task -> {
       LOG.info("Task {} is suspending, since it's optimizer is expired, put it to retry queue, optimizer {}",
           task.getTaskId(), task.getOptimizingThread());
+      //optimizing task of suspending optimizer would not be counted for retrying
+      retryTask(task, false);
       executingTaskMap.remove(task.getTaskId());
-      try {
-        //optimizing task of suspending optimizer would not be counted for retrying
-        retryTask(task, false);
-      } catch (Throwable t) {
-        LOG.error("Retry task {} failed, put it back to executing tasks", task.getTaskId(), t);
-        executingTaskMap.put(task.getTaskId(), task);
-      }
     });
     return expiredOptimizers;
   }
