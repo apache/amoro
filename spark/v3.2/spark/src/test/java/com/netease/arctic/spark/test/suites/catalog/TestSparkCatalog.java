1b6ab208363f621dbcebdf6727f5d9d8f177fc3b
BaiyangTX <xiangnebula@163.com>
2023-08-09 17:36:35 +0800

    catalog tests

--- spark/v3.2/spark/src/test/java/com/netease/arctic/spark/test/suites/catalog/TestSparkCatalog.java
+++ spark/v3.2/spark/src/test/java/com/netease/arctic/spark/test/suites/catalog/TestSparkCatalog.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-@Tag("integration")
+
 @EnableCatalogSelect
 @EnableCatalogSelect.SelectCatalog(byTableFormat = true)
 public class TestSparkCatalog extends SparkTableTestBase {
