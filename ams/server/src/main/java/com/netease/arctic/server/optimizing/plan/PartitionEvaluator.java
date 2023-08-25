0913c4d84a9e52a8231d9ab1df40caf7fd327e80
wangtao <wangtao3@corp.netease.com>
2023-08-25 10:27:22 +0800

    add docs for PartitionEvaluator

--- ams/server/src/main/java/com/netease/arctic/server/optimizing/plan/PartitionEvaluator.java
+++ ams/server/src/main/java/com/netease/arctic/server/optimizing/plan/PartitionEvaluator.java
@@ -25,6 +25,9 @@ import com.netease.arctic.server.optimizing.OptimizingType;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * PartitionEvaluator is used to evaluate whether a partition is necessary to be optimized.
+ */
 public interface PartitionEvaluator {
 
   /**
@@ -34,40 +37,95 @@ public interface PartitionEvaluator {
 
   }
 
+  /**
+   * Get the partition name.
+   *
+   * @return the partition name
+   */
   String getPartition();
 
   /**
    * Add a Data file and its related Delete files to this evaluator
+   *
    * @param dataFile - Data file
    * @param deletes  - Delete files
    * @return true if the file is added successfully, false if the file will not be optimized
    */
   boolean addFile(IcebergDataFile dataFile, List<IcebergContentFile<?>> deletes);
-  
+
+  /**
+   * Add some properties of this partition. It should be noted that properties should be added before adding any files.
+   *
+   * @param properties - properties of this partition
+   */
   void addPartitionProperties(Map<String, String> properties);
 
+  /**
+   * Whether this partition is necessary to optimize.
+   *
+   * @return true for is necessary to optimize, false for not necessary
+   */
   boolean isNecessary();
 
+  /**
+   * Get the cost of optimizing for this partition.
+   *
+   * @return the cost of optimizing
+   */
   long getCost();
-  
+
+  /**
+   * Get the weight of this partition which determines the priority of partition execution.
+   *
+   * @return the weight of this partition
+   */
   Weight getWeight();
 
+  /**
+   * Get the optimizing type of this partition.
+   *
+   * @return the OptimizingType
+   */
   OptimizingType getOptimizingType();
 
+  /**
+   * Get the count of fragment files involved in optimizing.
+   */
   int getFragmentFileCount();
 
+  /**
+   * Get the total size of fragment files involved in optimizing.
+   */
   long getFragmentFileSize();
 
+  /**
+   * Get the count of segment files involved in optimizing.
+   */
   int getSegmentFileCount();
 
+  /**
+   * Get the total size of segment files involved in optimizing.
+   */
   long getSegmentFileSize();
 
+  /**
+   * Get the count of equality delete files involved in optimizing.
+   */
   int getEqualityDeleteFileCount();
 
+  /**
+   * Get the total size of equality delete files involved in optimizing.
+   */
   long getEqualityDeleteFileSize();
 
+  /**
+   * Get the count of positional delete files involved in optimizing.
+   */
   int getPosDeleteFileCount();
 
+  /**
+   * Get the total size of positional delete files involved in optimizing.
+   */
   long getPosDeleteFileSize();
 
 }
