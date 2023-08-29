b2a87b97bace3e0f6a428b1638a41b054616c40e
huyuanfeng <huyuanfeng@huya.com>
2023-08-29 10:09:23 +0800

    fix sql

--- ams/server/src/main/resources/mysql/upgrade-0.5.0-to-0.5.1.sql
+++ ams/server/src/main/resources/mysql/upgrade-0.5.0-to-0.5.1.sql
@@ -1,2 +1,2 @@
-DELETE FROM `table_optimizing_process` WHERE `status` ='RUNNING';
+UPDATE `table_optimizing_process` SET `status`  = 'CLOSED' WHERE `status` ='RUNNING';
 UPDATE `table_runtime` SET `optimizing_status`  = 'IDLE';
