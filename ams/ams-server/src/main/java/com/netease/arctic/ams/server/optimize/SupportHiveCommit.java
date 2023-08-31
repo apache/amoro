d9c0f05d4cf40cf54077f0a4d6369f3275b702b6
wangtao <wangtao3@corp.netease.com>
2023-08-31 19:35:04 +0800

    move files to iceberg hive location

--- ams/ams-server/src/main/java/com/netease/arctic/ams/server/optimize/SupportHiveCommit.java
+++ ams/ams-server/src/main/java/com/netease/arctic/ams/server/optimize/SupportHiveCommit.java
@@ -25,18 +25,22 @@ import com.netease.arctic.ams.server.model.BasicOptimizeTask;
 import com.netease.arctic.ams.server.model.OptimizeTaskRuntime;
 import com.netease.arctic.data.file.FileNameGenerator;
 import com.netease.arctic.hive.HMSClientPool;
+import com.netease.arctic.hive.HiveTableProperties;
 import com.netease.arctic.hive.table.SupportHive;
 import com.netease.arctic.hive.utils.HivePartitionUtil;
 import com.netease.arctic.hive.utils.HiveTableUtil;
 import com.netease.arctic.hive.utils.TableTypeUtil;
 import com.netease.arctic.table.ArcticTable;
+import com.netease.arctic.table.UnkeyedTable;
 import com.netease.arctic.utils.SerializationUtils;
 import com.netease.arctic.utils.TableFileUtils;
+import com.netease.arctic.utils.TablePropertyUtil;
 import org.apache.hadoop.hive.metastore.api.Partition;
 import org.apache.hadoop.hive.metastore.api.Table;
 import org.apache.iceberg.DataFile;
 import org.apache.iceberg.StructLike;
 import org.apache.iceberg.types.Types;
+import org.apache.iceberg.util.StructLikeMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -106,29 +110,45 @@ public class SupportHiveCommit extends BasicOptimizeCommit {
 
   private String getPartitionPath(HMSClientPool hiveClient, long maxTransactionId, DataFile targetFile,
                                   Types.StructType partitionSchema) {
-    List<String> partitionValues = HivePartitionUtil.partitionValuesAsList(targetFile.partition(), partitionSchema);
-    String partitionPath;
+    // get iceberg partition path
+    String icebergPartitionLocation = getIcebergPartitionLocation(targetFile.partition());
+    if (icebergPartitionLocation != null) {
+      return icebergPartitionLocation;
+    }
+    // get hive partition path
     if (arcticTable.spec().isUnpartitioned()) {
       try {
         Table hiveTable = ((SupportHive) arcticTable).getHMSClient().run(client ->
             client.getTable(arcticTable.id().getDatabase(), arcticTable.id().getTableName()));
-        partitionPath = hiveTable.getSd().getLocation();
+        return hiveTable.getSd().getLocation();
       } catch (Exception e) {
         throw new RuntimeException("Get hive table failed", e);
       }
     } else {
+      List<String> partitionValues = HivePartitionUtil.partitionValuesAsList(targetFile.partition(), partitionSchema);
       String hiveSubdirectory = arcticTable.isKeyedTable() ?
           HiveTableUtil.newHiveSubdirectory(maxTransactionId) : HiveTableUtil.newHiveSubdirectory();
 
       Partition p = HivePartitionUtil.getPartition(hiveClient, arcticTable, partitionValues);
       if (p == null) {
-        partitionPath = HiveTableUtil.newHiveDataLocation(((SupportHive) arcticTable).hiveLocation(),
+        return HiveTableUtil.newHiveDataLocation(((SupportHive) arcticTable).hiveLocation(),
             arcticTable.spec(), targetFile.partition(), hiveSubdirectory);
       } else {
-        partitionPath = p.getSd().getLocation();
+        return p.getSd().getLocation();
       }
     }
-    return partitionPath;
+  }
+
+  private String getIcebergPartitionLocation(StructLike partitionData) {
+    UnkeyedTable baseTable = arcticTable.isKeyedTable() ?
+        arcticTable.asKeyedTable().baseTable() : arcticTable.asUnkeyedTable();
+    StructLikeMap<Map<String, String>> partitionProperty = baseTable.partitionProperty();
+    Map<String, String> property =
+        partitionProperty.get(arcticTable.spec().isUnpartitioned() ? TablePropertyUtil.EMPTY_STRUCT : partitionData);
+    if (property == null) {
+      return null;
+    }
+    return property.get(HiveTableProperties.PARTITION_PROPERTIES_KEY_HIVE_LOCATION);
   }
 
   protected boolean isPartitionMajorOptimizeSupportHive(String partition, List<OptimizeTaskItem> optimizeTaskItems) {
