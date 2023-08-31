3600570f1c43016b95f3e56cbe9b00cbc942703a
cell <celltobig@163.com>
2023-08-31 13:46:07 +0800

    [AMORO-1870]: Support table filters to make AMS ignore tables that are not needed (#1879)
    * [AMORO-1870]: table filter #1870 \n * add table filter
    
    * fix check style
    
    * fix gitignore
    
    * fix db table filter
    
    * fix table filter
    
    * Update managing-catalogs.md and change docs
    
    * fix checkstyle
    
    * fix change table filter key
    
    * change table filter md
    
    * Update managing-catalogs.md
    
    * Add Deprecated annotation
    
    * Update docs/admin-guides/managing-catalogs.md
    
    Co-authored-by: ZhouJinsong <zhoujinsong0505@163.com>
    
    * Update ams/api/src/main/java/com/netease/arctic/ams/api/properties/CatalogMetaProperties.java
    
    Co-authored-by: ZhouJinsong <zhoujinsong0505@163.com>
    
    * fix check style
    
    ---------
    
    Co-authored-by: 刘政 <zhengliu@gaojihealth.com>
    Co-authored-by: wangtaohz <103108928+wangtaohz@users.noreply.github.com>
    Co-authored-by: wangtao <wangtao3@corp.netease.com>
    Co-authored-by: ZhouJinsong <zhoujinsong0505@163.com>

--- core/src/main/java/com/netease/arctic/catalog/IcebergCatalogWrapper.java
+++ core/src/main/java/com/netease/arctic/catalog/IcebergCatalogWrapper.java
@@ -57,6 +57,7 @@ public class IcebergCatalogWrapper implements ArcticCatalog {
   private CatalogMeta meta;
   private Map<String, String> customProperties;
   private Pattern databaseFilterPattern;
+  private Pattern tableFilterPattern;
   private transient TableMetaStore tableMetaStore;
   private transient Catalog icebergCatalog;
 
@@ -90,6 +91,15 @@ public class IcebergCatalogWrapper implements ArcticCatalog {
     } else {
       databaseFilterPattern = null;
     }
+
+    if (meta.getCatalogProperties().containsKey(CatalogMetaProperties.KEY_TABLE_FILTER)) {
+      String tableFilter =
+              meta.getCatalogProperties().get(CatalogMetaProperties.KEY_TABLE_FILTER);
+      tableFilterPattern = Pattern.compile(tableFilter);
+    } else {
+      tableFilterPattern = null;
+    }
+
   }
 
   public IcebergCatalogWrapper(CatalogMeta meta, Map<String, String> properties) {
@@ -148,7 +158,9 @@ public class IcebergCatalogWrapper implements ArcticCatalog {
   @Override
   public List<TableIdentifier> listTables(String database) {
     return tableMetaStore.doAs(() -> icebergCatalog.listTables(Namespace.of(database)).stream()
-        .filter(tableIdentifier -> tableIdentifier.namespace().levels().length == 1)
+        .filter(tableIdentifier -> tableIdentifier.namespace().levels().length == 1 &&
+            (tableFilterPattern == null ||
+                tableFilterPattern.matcher((database + "." + tableIdentifier.name())).matches()))
         .map(tableIdentifier -> TableIdentifier.of(name(), database, tableIdentifier.name()))
         .collect(Collectors.toList()));
   }
