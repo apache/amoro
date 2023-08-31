dcbb41cff15f4a511c65baaa9f9227c39145bd90
wangtaohz <103108928+wangtaohz@users.noreply.github.com>
2023-08-31 09:57:20 +0800

    Update ams/api/src/main/java/com/netease/arctic/ams/api/properties/CatalogMetaProperties.java
    Co-authored-by: ZhouJinsong <zhoujinsong0505@163.com>

--- ams/api/src/main/java/com/netease/arctic/ams/api/properties/CatalogMetaProperties.java
+++ ams/api/src/main/java/com/netease/arctic/ams/api/properties/CatalogMetaProperties.java
@@ -41,7 +41,9 @@ public class CatalogMetaProperties {
   @Deprecated
   public static final String KEY_WAREHOUSE_DIR = "warehouse.dir";
   public static final String KEY_WAREHOUSE = "warehouse";
-  //Deprecated from version v0.7.0, use KEY_TABLE_FILTER
+/**
+   * @deprecated since 0.6.0, will be removed in 0.7.0; use {@link CatalogMetaProperties#KEY_TABLE_FILTER} instead.
+   */
   @Deprecated
   public static final String KEY_DATABASE_FILTER_REGULAR_EXPRESSION = "database.filter-regular-expression";
   public static final String KEY_TABLE_FILTER = "table-filter";
