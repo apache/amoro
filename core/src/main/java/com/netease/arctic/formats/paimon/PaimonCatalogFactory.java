06926dcbccdfaa63cfc966f6e4edd35f94c7bd93
shidayang <530847445@qq.com>
2023-09-13 10:45:10 +0800

    Integration paimon

--- core/src/main/java/com/netease/arctic/formats/paimon/PaimonCatalogFactory.java
+++ core/src/main/java/com/netease/arctic/formats/paimon/PaimonCatalogFactory.java
@@ -5,6 +5,7 @@ import com.netease.arctic.ams.api.TableFormat;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.paimon.catalog.CatalogContext;
 import org.apache.paimon.catalog.CatalogFactory;
+import org.apache.paimon.options.CatalogOptions;
 import org.apache.paimon.options.Options;
 
 import java.util.Map;
@@ -15,6 +16,16 @@ public class PaimonCatalogFactory implements FormatCatalogFactory {
       String name, String metastoreType, Map<String, String> properties, Configuration configuration) {
 
     Options options = Options.fromMap(properties);
+
+    String type;
+    if ("hadoop".equalsIgnoreCase(metastoreType)) {
+      type = "filesystem";
+    } else if ("hive".equalsIgnoreCase(metastoreType)) {
+      type = "hive";
+    } else {
+      type = metastoreType;
+    }
+    options.set(CatalogOptions.METASTORE, type);
 
     CatalogContext catalogContext = CatalogContext.create(options, configuration);
 
