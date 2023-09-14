c57c24aceec2f90ec5cad88080bbcb85662ab213
Yi Xie <xieyi01@rd.netease.com>
2023-09-14 18:17:31 +0800

    change sparkWriter to  junit5

--- spark/v3.2/spark/src/test/java/com/netease/arctic/spark/writer/TestSparkWriter.java
+++ spark/v3.2/spark/src/test/java/com/netease/arctic/spark/writer/TestSparkWriter.java
@@ -1,19 +1,14 @@
 package com.netease.arctic.spark.writer;
 
 
-import com.netease.arctic.BasicTableTestHelper;
-import com.netease.arctic.TableTestHelper;
 import com.netease.arctic.ams.api.TableFormat;
-import com.netease.arctic.catalog.BasicCatalogTestHelper;
-import com.netease.arctic.catalog.CatalogTestHelper;
-import com.netease.arctic.catalog.TableTestBase;
-import com.netease.arctic.hive.TestHMS;
-import com.netease.arctic.hive.catalog.HiveCatalogTestHelper;
-import com.netease.arctic.hive.catalog.HiveTableTestHelper;
 import com.netease.arctic.spark.reader.SparkParquetReaders;
+import com.netease.arctic.spark.test.SparkTableTestBase;
 import com.netease.arctic.table.ArcticTable;
+import com.netease.arctic.table.PrimaryKeySpec;
 import org.apache.iceberg.DataFile;
 import org.apache.iceberg.Files;
+import org.apache.iceberg.PartitionSpec;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.io.CloseableIterable;
 import org.apache.iceberg.orc.ORC;
@@ -21,22 +16,21 @@ import org.apache.iceberg.parquet.AdaptHiveParquet;
 import org.apache.iceberg.relocated.com.google.common.collect.Iterators;
 import org.apache.iceberg.spark.SparkSchemaUtil;
 import org.apache.iceberg.spark.data.SparkOrcReader;
+import org.apache.iceberg.types.Types;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.catalyst.expressions.GenericInternalRow;
 import org.apache.spark.sql.connector.write.DataWriter;
 import org.apache.spark.sql.connector.write.LogicalWriteInfoImpl;
 import org.apache.spark.sql.connector.write.Write;
-import org.apache.spark.sql.types.Decimal;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.sql.util.CaseInsensitiveStringMap;
 import org.apache.spark.unsafe.types.UTF8String;
-import org.junit.Assert;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -44,91 +38,69 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import static com.netease.arctic.table.TableProperties.BASE_FILE_FORMAT;
+import static com.netease.arctic.table.TableProperties.CHANGE_FILE_FORMAT;
+import static com.netease.arctic.table.TableProperties.DEFAULT_FILE_FORMAT;
 import static com.netease.arctic.table.TableProperties.DEFAULT_FILE_FORMAT_ORC;
+import static com.netease.arctic.table.TableProperties.DEFAULT_FILE_FORMAT_PARQUET;
 
-@RunWith(Parameterized.class)
-public class TestSparkWriter extends TableTestBase {
-  @ClassRule
-  public static TestHMS TEST_HMS = new TestHMS();
+public class TestSparkWriter extends SparkTableTestBase {
 
-  public TestSparkWriter(CatalogTestHelper catalogTestHelper,
-      TableTestHelper tableTestHelper) {
-    super(catalogTestHelper, tableTestHelper);
+  static final Schema schema = new Schema(
+      Types.NestedField.required(1, "id", Types.IntegerType.get()),
+      Types.NestedField.required(2, "data", Types.StringType.get()),
+      Types.NestedField.required(3, "pt", Types.StringType.get())
+  );
+
+  static final PrimaryKeySpec idPrimaryKeySpec = PrimaryKeySpec.builderFor(schema)
+      .addColumn("id").build();
+
+  static final PartitionSpec ptSpec = PartitionSpec.builderFor(schema)
+      .identity("pt").build();
+
+  public static Stream<Arguments> testWrite() {
+    return Stream.of(
+        Arguments.of(MIXED_HIVE, WriteMode.APPEND, schema, idPrimaryKeySpec, ptSpec, DEFAULT_FILE_FORMAT_PARQUET),
+        Arguments.of(MIXED_HIVE, WriteMode.APPEND, schema, noPrimaryKey, ptSpec, DEFAULT_FILE_FORMAT_PARQUET),
+        Arguments.of(MIXED_HIVE, WriteMode.APPEND, schema, idPrimaryKeySpec, unpartitioned, DEFAULT_FILE_FORMAT_PARQUET),
+        Arguments.of(MIXED_HIVE, WriteMode.APPEND, schema, noPrimaryKey, unpartitioned, DEFAULT_FILE_FORMAT_PARQUET),
+        Arguments.of(MIXED_HIVE, WriteMode.APPEND, schema, idPrimaryKeySpec, ptSpec, DEFAULT_FILE_FORMAT_ORC),
+        Arguments.of(MIXED_HIVE, WriteMode.APPEND, schema, noPrimaryKey, ptSpec, DEFAULT_FILE_FORMAT_ORC),
+        Arguments.of(MIXED_HIVE, WriteMode.APPEND, schema, idPrimaryKeySpec, unpartitioned, DEFAULT_FILE_FORMAT_ORC),
+        Arguments.of(MIXED_HIVE, WriteMode.APPEND, schema, noPrimaryKey, unpartitioned, DEFAULT_FILE_FORMAT_ORC),
+
+        Arguments.of(MIXED_HIVE, WriteMode.OVERWRITE_DYNAMIC, schema, idPrimaryKeySpec, ptSpec, DEFAULT_FILE_FORMAT_PARQUET),
+        Arguments.of(MIXED_HIVE, WriteMode.OVERWRITE_DYNAMIC, schema, noPrimaryKey, ptSpec, DEFAULT_FILE_FORMAT_PARQUET),
+        Arguments.of(MIXED_HIVE, WriteMode.OVERWRITE_DYNAMIC, schema, idPrimaryKeySpec, unpartitioned, DEFAULT_FILE_FORMAT_PARQUET),
+        Arguments.of(MIXED_HIVE, WriteMode.OVERWRITE_DYNAMIC, schema, noPrimaryKey, unpartitioned, DEFAULT_FILE_FORMAT_PARQUET),
+        Arguments.of(MIXED_HIVE, WriteMode.OVERWRITE_DYNAMIC, schema, idPrimaryKeySpec, ptSpec, DEFAULT_FILE_FORMAT_ORC),
+        Arguments.of(MIXED_HIVE, WriteMode.OVERWRITE_DYNAMIC, schema, noPrimaryKey, ptSpec, DEFAULT_FILE_FORMAT_ORC),
+        Arguments.of(MIXED_HIVE, WriteMode.OVERWRITE_DYNAMIC, schema, idPrimaryKeySpec, unpartitioned, DEFAULT_FILE_FORMAT_ORC),
+        Arguments.of(MIXED_HIVE, WriteMode.OVERWRITE_DYNAMIC, schema, noPrimaryKey, unpartitioned, DEFAULT_FILE_FORMAT_ORC)
+    );
   }
 
-  @Parameterized.Parameters(name = "{0}, {1}")
-  public static Object[] parameters() {
-    return new Object[][] {
-        {new BasicCatalogTestHelper(TableFormat.MIXED_ICEBERG),
-            new BasicTableTestHelper(true, true)},
-        {new BasicCatalogTestHelper(TableFormat.MIXED_ICEBERG),
-            new BasicTableTestHelper(true, false)},
-        {new BasicCatalogTestHelper(TableFormat.MIXED_ICEBERG),
-            new BasicTableTestHelper(false, true)},
-        {new BasicCatalogTestHelper(TableFormat.MIXED_ICEBERG),
-            new BasicTableTestHelper(false, false)},
-        {new BasicCatalogTestHelper(TableFormat.MIXED_ICEBERG),
-            new BasicTableTestHelper(true, true, DEFAULT_FILE_FORMAT_ORC)},
-        {new BasicCatalogTestHelper(TableFormat.MIXED_ICEBERG),
-            new BasicTableTestHelper(true, false, DEFAULT_FILE_FORMAT_ORC)},
-        {new BasicCatalogTestHelper(TableFormat.MIXED_ICEBERG),
-            new BasicTableTestHelper(false, true, DEFAULT_FILE_FORMAT_ORC)},
-        {new BasicCatalogTestHelper(TableFormat.MIXED_ICEBERG),
-            new BasicTableTestHelper(false, false, DEFAULT_FILE_FORMAT_ORC)},
-        {new HiveCatalogTestHelper(TableFormat.MIXED_HIVE, TEST_HMS.getHiveConf()),
-            new HiveTableTestHelper(true, true)},
-        {new HiveCatalogTestHelper(TableFormat.MIXED_HIVE, TEST_HMS.getHiveConf()),
-            new HiveTableTestHelper(true, false)},
-        {new HiveCatalogTestHelper(TableFormat.MIXED_HIVE, TEST_HMS.getHiveConf()),
-            new HiveTableTestHelper(false, true)},
-        {new HiveCatalogTestHelper(TableFormat.MIXED_HIVE, TEST_HMS.getHiveConf()),
-            new HiveTableTestHelper(false, false)},
-        {new HiveCatalogTestHelper(TableFormat.MIXED_HIVE, TEST_HMS.getHiveConf()),
-            new HiveTableTestHelper(true, true, DEFAULT_FILE_FORMAT_ORC)},
-        {new HiveCatalogTestHelper(TableFormat.MIXED_HIVE, TEST_HMS.getHiveConf()),
-            new HiveTableTestHelper(true, false, DEFAULT_FILE_FORMAT_ORC)},
-        {new HiveCatalogTestHelper(TableFormat.MIXED_HIVE, TEST_HMS.getHiveConf()),
-            new HiveTableTestHelper(false, true, DEFAULT_FILE_FORMAT_ORC)},
-        {new HiveCatalogTestHelper(TableFormat.MIXED_HIVE, TEST_HMS.getHiveConf()),
-            new HiveTableTestHelper(false, false, DEFAULT_FILE_FORMAT_ORC)}
-    };
-  }
-
-
-  @Test
-  public void testAppend() throws IOException {
+  @DisplayName("Test write mix_hive Table")
+  @ParameterizedTest
+  @MethodSource
+  public void testWrite(TableFormat format, WriteMode writeMode, Schema schema,
+      PrimaryKeySpec keySpec, PartitionSpec ptSpec, String fileFormat) throws IOException {
+    ArcticTable table = createTarget(schema, tableBuilder ->
+        tableBuilder.withPrimaryKeySpec(keySpec)
+            .withProperty(CHANGE_FILE_FORMAT, fileFormat)
+            .withProperty(BASE_FILE_FORMAT, fileFormat)
+            .withProperty(DEFAULT_FILE_FORMAT, fileFormat)
+            .withPartitionSpec(ptSpec));
     Map<String, String> map = new HashMap<>();
-    map.put(WriteMode.WRITE_MODE_KEY,WriteMode.APPEND.mode);
-    testWriteData(map);
+    map.put(WriteMode.WRITE_MODE_KEY,writeMode.mode);
+    testWriteData(table, map);
   }
 
-  @Test
-  public void testOverWriteDynamic() throws IOException {
-    Map<String, String> map = new HashMap<>();
-    map.put(WriteMode.WRITE_MODE_KEY,WriteMode.OVERWRITE_DYNAMIC.mode);
-    testWriteData(map);
-  }
-
-  @Test
-  public void testOverWriteByFilter() throws IOException {
-    Map<String, String> map = new HashMap<>();
-    map.put(WriteMode.WRITE_MODE_KEY,WriteMode.OVERWRITE_BY_FILTER.mode);
-    testWriteData(map);
-  }
-
-  @Test
-  public void testUpsertWrite() throws IOException {
-    Map<String, String> map = new HashMap<>();
-    map.put(WriteMode.WRITE_MODE_KEY,WriteMode.UPSERT.mode);
-    testWriteData(map);
-  }
-
-
-  private void testWriteData(Map<String, String> map) throws IOException {
-    ArcticTable table = getArcticTable();
+  private void testWriteData(ArcticTable table, Map<String, String> map) throws IOException {
     StructType structType = SparkSchemaUtil.convert(table.schema());
     LogicalWriteInfoImpl info = new LogicalWriteInfoImpl("queryId", structType, new CaseInsensitiveStringMap(map));
-    ArcticSparkWriteBuilder builder = new ArcticSparkWriteBuilder(table, info, getCatalog());
+    ArcticSparkWriteBuilder builder = new ArcticSparkWriteBuilder(table, info, catalog());
     Write write = builder.build();
     DataWriter<InternalRow> writer = write.toBatch().createBatchWriterFactory(null).createWriter(0,0);
     //create record
@@ -153,7 +125,7 @@ public class TestSparkWriter extends TableTestBase {
         ).collect(Collectors.toList()));
     Set<InternalRow> result = new HashSet<>();
     Iterators.addAll(result, concat.iterator());
-    Assert.assertEquals(result, records.stream().collect(Collectors.toSet()));
+    Assertions.assertEquals(result, records.stream().collect(Collectors.toSet()));
   }
 
 
@@ -180,18 +152,7 @@ public class TestSparkWriter extends TableTestBase {
   }
 
   private InternalRow geneRowData() {
-    InternalRow record;
-    if (getArcticTable().format() == TableFormat.MIXED_HIVE) {
-      record =
-          new GenericInternalRow(new Object[] {1, UTF8String.fromString("lily"), 0L, 1641009600000L, 1641009600000L,
-              Decimal.apply(new BigDecimal("0")), UTF8String.fromString("2022-01-01")});
-    } else {
-      record =
-          new GenericInternalRow(new Object[] {1, UTF8String.fromString("lily"), 0L, 1641009600000L});
-    }
-    return record;
+    return new GenericInternalRow(new Object[] {1, UTF8String.fromString("aaa"), UTF8String.fromString("AAA")});
   }
 
-
 }
-
