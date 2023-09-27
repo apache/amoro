1f96a15ac1f2eb6521552d287b254b2a011d5dac
baiyangtx <xiangnebula@163.com>
2023-09-27 19:47:54 +0800

    add console log level control

---
+++ docker/amoro/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+###############################################################################
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+args=("$@")
+if [ $1 == "help" ]; then
+    printf "Usage: $(basename $0) [ams|optimizer] [args]\n"
+    printf "   Or: $(basename $0) help \n\n"
+    exit 0
+elif [ "$1" == "ams" ]; then
+    args=("${args[@]:1}")
+
+    echo "Start Amoro Management Service"
+
+    exec "$AMORO_HOME/bin/ams.sh start"
+elif [ "$1" == "optimizer" ]; then
+
+fi
+
