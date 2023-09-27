a405874926d383d0eeaeb5cdba83240a3df71c5a
baiyangtx <xiangnebula@163.com>
2023-09-27 16:00:56 +0800

    ams startup scripts

--- dist/src/main/arctic-bin/bin/optimizer.sh
+++ dist/src/main/arctic-bin/bin/optimizer.sh
@@ -18,32 +18,36 @@
 #
 
 CURRENT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-AMORO_HOME="$( cd "$CURRENT_DIR/../" ; pwd -P )"
-export AMORO_HOME
+
+. $CURRENT_DIR/load-config.sh
 
 LIB_PATH=$AMORO_HOME/lib
-LOG_DIR=$AMORO_HOME/logs
-STDERR_LOG=${LOG_DIR}/localOptimize.log.err
+STDERR_LOG=${AMORO_LOG_DIR}/optimizer.log.err
 
-if [ -z "$AMORO_CONF_DIR" ]; then
-    AMORO_CONF_DIR=$AMORO_HOME/conf
-fi
 
 export CLASSPATH=$CLASSPATH:$(find $LIB_PATH/ -type f -name "*.jar" | paste -sd':' -):$AMORO_CONF_DIR/optimize
 if [ -z $(find $LIB_PATH/ -type f -name "*.jar" | paste -sd':' -) ]; then
   echo "Launching the localOptimize task lacks relevant jars, please check" >&2
-  exit -1
+  exit 1
 fi
-if [[ -d $JAVA_HOME ]]; then
-    JAVA_RUN=$JAVA_HOME/bin/java
-else
-    JAVA_RUN=java
-fi
+
 
 if [ ! -f $STDERR_LOG ];then
     touch $STDERR_LOG
 fi
-JAVA_OPTS="-Xmx$1m -Dlog.home=${LOG_DIR} -Dlog.subdir=localOptimizer-$(date +%s)"
+
+ARGS=${@:2}
+
+if [ -z "$OPTIMIZER_LOG_DIR_NAME" ]; then
+  OPTIMIZER_LOG_DIR=$AMORO_LOG_DIR/optimizer-$(date +%s)
+else
+  OPTIMIZER_LOG_DIR=$AMORO_LOG_DIR/$OPTIMIZER_LOG_DIR_NAME
+fi
+if [ ! -f "${OPTIMIZER_LOG_DIR}" ]; then
+  mkdir -p "${OPTIMIZER_LOG_DIR}"
+fi
+
+JAVA_OPTS="-Xmx$1m -Dlog.dir=${OPTIMIZER_LOG_DIR}"
 RUN_SERVER="com.netease.arctic.optimizer.local.LocalOptimizer"
-CMDS="$JAVA_RUN $JAVA_OPTS $RUN_SERVER ${@:2}"
+CMDS="$JAVA_RUN $JAVA_OPTS $RUN_SERVER $ARGS"
 nohup ${CMDS} >/dev/null 2>${STDERR_LOG} &
