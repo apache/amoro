dff89461141c41d5798eb787e2a4176a2527cb57
shidayang <530847445@qq.com>
2023-10-24 14:16:35 +0800

    Rename update.sql file to update-0.5.1-to-0.6.0.sql (#2174)

--- ams/server/src/main/resources/postgres/update.sql
+++ ams/server/src/main/resources/postgres/update.sql
@@ -1,15 +1,2 @@
--- ADD format to table_identifier
-ALTER TABLE table_identifier ADD format VARCHAR(32)  NOT NULL;
-COMMENT ON COLUMN table_identifier.format IS 'Format';
-
-UPDATE table_identifier A
-SET format = (catalog_properties::json) ->> 'table-formats'
-FROM catalog_metadata B
-WHERE A.catalog_name = B.catalog_name;
-
-UPDATE table_identifier I
-SET format = B.format
-FROM table_metadata B
-WHERE I.table_id = B.table_id;
-
-ALTER  TABLE table_metadata DROP format;
+-- If you have any changes to the AMS database, please record them in this file.
+-- We will confirm the corresponding version of these upgrade scripts when releasing.
