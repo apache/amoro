d4ac1f8e78b8fe33c4f2d94c126ee34a8de0fc71
Qishang Zhong <zhongqishang@gmail.com>
2023-11-09 18:37:29 +0800

    fix comment

--- ams/server/src/main/java/com/netease/arctic/server/optimizing/MetricsSummary.java
+++ ams/server/src/main/java/com/netease/arctic/server/optimizing/MetricsSummary.java
@@ -10,41 +10,27 @@ import org.apache.iceberg.relocated.com.google.common.base.MoreObjects;
 import java.util.Collection;
 
 public class MetricsSummary {
-  /**
-   * 为了兼容 0.6.0之前的版本
-   */
-  @JsonIgnoreProperties
-  private long newDataSize = 0;
-  @JsonIgnoreProperties
-  private int newDataFileCnt = 0;
-  @JsonIgnoreProperties
-  private long newDataRecordCnt = 0;
-  /** Only position delete files will be generated */
-  @JsonIgnoreProperties
-  private long newDeleteSize = 0;
+  @JsonIgnoreProperties private long newDataSize = 0;
 
-  @JsonIgnoreProperties
-  private int newDeleteFileCnt = 0;
-  @JsonIgnoreProperties
-  private long newDeleteRecordCnt = 0;
+  @JsonIgnoreProperties private int newDataFileCnt = 0;
+  @JsonIgnoreProperties private long newDataRecordCnt = 0;
+  /** Only position delete files will be generated */
+  @JsonIgnoreProperties private long newDeleteSize = 0;
+
+  @JsonIgnoreProperties private int newDeleteFileCnt = 0;
+  @JsonIgnoreProperties private long newDeleteRecordCnt = 0;
   private long rewriteDataSize = 0;
   private long rewritePosDataSize = 0;
   private long equalityDeleteSize = 0;
-  @JsonIgnoreProperties
-  private long positionDeleteSize = 0;
+  @JsonIgnoreProperties private long positionDeleteSize = 0;
   private int rewriteDataFileCnt = 0;
-  @JsonIgnoreProperties
-  private int rewritePosDataFileCnt = 0;
+  @JsonIgnoreProperties private int rewritePosDataFileCnt = 0;
   private int eqDeleteFileCnt = 0;
   private int posDeleteFileCnt = 0;
-  @JsonIgnoreProperties
-  private int rewriteDataRecordCnt = 0;
-  @JsonIgnoreProperties
-  private int rewritePosDataRecordCnt = 0;
-  @JsonIgnoreProperties
-  private int eqDeleteRecordCnt = 0;
-  @JsonIgnoreProperties
-  private int posDeleteRecordCnt = 0;
+  @JsonIgnoreProperties private int rewriteDataRecordCnt = 0;
+  @JsonIgnoreProperties private int rewritePosDataRecordCnt = 0;
+  @JsonIgnoreProperties private int eqDeleteRecordCnt = 0;
+  @JsonIgnoreProperties private int posDeleteRecordCnt = 0;
 
   public MetricsSummary() {}
 
