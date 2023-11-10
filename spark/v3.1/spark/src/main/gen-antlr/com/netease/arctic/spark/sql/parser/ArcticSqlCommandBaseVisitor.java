efc84fea1696fd209cf05447abb7ec5957dab250
wangzeyu <hameizi369@gmail.com>
2023-11-10 17:02:18 +0800

    revert unnecessory file

--- spark/v3.1/spark/src/main/gen-antlr/com/netease/arctic/spark/sql/parser/ArcticSqlCommandBaseVisitor.java
+++ spark/v3.1/spark/src/main/gen-antlr/com/netease/arctic/spark/sql/parser/ArcticSqlCommandBaseVisitor.java
@@ -1,4 +1,23 @@
-package com.netease.arctic.spark.sql.parser;// Generated from E:/workspace/code/github-arctic-refactor/spark/v3.1/spark/src/main/antlr4/com/netease/arctic/spark/sql/parser/ArcticSqlCommand.g4 by ANTLR 4.13.1
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Generated from com/netease/arctic/spark/sql/parser/ArcticSqlCommand.g4 by ANTLR 4.7
+package com.netease.arctic.spark.sql.parser;
 import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
 
 /**
@@ -9,7 +28,6 @@ import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
  * @param <T> The return type of the visit operation. Use {@link Void} for
  * operations with no return type.
  */
-@SuppressWarnings("CheckReturnValue")
 public class ArcticSqlCommandBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements ArcticSqlCommandVisitor<T> {
 	/**
 	 * {@inheritDoc}
@@ -25,13 +43,6 @@ public class ArcticSqlCommandBaseVisitor<T> extends AbstractParseTreeVisitor<T> 
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
 	@Override public T visitMigrateStatement(ArcticSqlCommandParser.MigrateStatementContext ctx) { return visitChildren(ctx); }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
-	 */
-	@Override public T visitOptimizeStatement(ArcticSqlCommandParser.OptimizeStatementContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
--- spark/v3.1/spark/src/main/gen-antlr/com/netease/arctic/spark/sql/parser/ArcticSqlCommandVisitor.java
+++ spark/v3.1/spark/src/main/gen-antlr/com/netease/arctic/spark/sql/parser/ArcticSqlCommandVisitor.java
@@ -1,4 +1,23 @@
-package com.netease.arctic.spark.sql.parser;// Generated from E:/workspace/code/github-arctic-refactor/spark/v3.1/spark/src/main/antlr4/com/netease/arctic/spark/sql/parser/ArcticSqlCommand.g4 by ANTLR 4.13.1
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Generated from com/netease/arctic/spark/sql/parser/ArcticSqlCommand.g4 by ANTLR 4.7
+package com.netease.arctic.spark.sql.parser;
 import org.antlr.v4.runtime.tree.ParseTreeVisitor;
 
 /**
@@ -22,13 +41,6 @@ public interface ArcticSqlCommandVisitor<T> extends ParseTreeVisitor<T> {
 	 * @return the visitor result
 	 */
 	T visitMigrateStatement(ArcticSqlCommandParser.MigrateStatementContext ctx);
-	/**
-	 * Visit a parse tree produced by the {@code optimizeStatement}
-	 * labeled alternative in {@link ArcticSqlCommandParser#arcticStatement}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitOptimizeStatement(ArcticSqlCommandParser.OptimizeStatementContext ctx);
 	/**
 	 * Visit a parse tree produced by {@link ArcticSqlCommandParser#singleStatement}.
 	 * @param ctx the parse tree
