3b18abe253ca5fac5517313ac702da4574dc398f
Yi Xie <xieyi01@rd.netease.com>
2023-11-13 10:55:05 +0800

    fix check style

--- flink/flink-common/src/main/java/com/netease/arctic/flink/catalog/ArcticCatalog.java
+++ flink/flink-common/src/main/java/com/netease/arctic/flink/catalog/ArcticCatalog.java
@@ -625,7 +625,8 @@ public class ArcticCatalog extends AbstractCatalog {
 
     for (int i = computeIndex + 1; computeIndex > -1 && i < tableColumns.size(); i++) {
       if (tableColumns.get(i) instanceof PhysicalColumn) {
-        throw new IllegalStateException("compute column must be listed after all physical columns ");
+        throw new IllegalStateException(
+            "compute column must be listed after all physical columns. ");
       }
     }
   }
