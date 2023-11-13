2f5ccc4d3decac27d2be140d93f8b6235cb7de5f
wangtao <wangtao3@corp.netease.com>
2023-11-13 10:40:00 +0800

    add auto create tag max delay

--- core/src/main/java/com/netease/arctic/table/TableProperties.java
+++ core/src/main/java/com/netease/arctic/table/TableProperties.java
@@ -203,6 +203,10 @@ public class TableProperties {
       "tag.auto-create.trigger.offset.minutes";
   public static final int AUTO_CREATE_TAG_TRIGGER_OFFSET_MINUTES_DEFAULT = 0;
 
+  public static final String AUTO_CREATE_TAG_MAX_DELAY_MINUTES =
+      "tag.auto-create.trigger.max-delay.minutes";
+  public static final int AUTO_CREATE_TAG_MAX_DELAY_MINUTES_DEFAULT = 60;
+
   public static final String AUTO_CREATE_TAG_FORMAT = "tag.auto-create.daily.tag-format";
   public static final String AUTO_CREATE_TAG_FORMAT_DEFAULT = "'tag-day-'yyyyMMdd";
 
