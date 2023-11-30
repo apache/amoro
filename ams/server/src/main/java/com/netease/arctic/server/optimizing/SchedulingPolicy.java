f0712427f9e4200b49e36fb0203c3a211cb26060
tcodehuber <tcodehuber@gmail.com>
2023-11-30 10:21:29 +0800

    change field type for schedulingpolicy

--- ams/server/src/main/java/com/netease/arctic/server/optimizing/SchedulingPolicy.java
+++ ams/server/src/main/java/com/netease/arctic/server/optimizing/SchedulingPolicy.java
@@ -21,36 +21,36 @@ public class SchedulingPolicy {
   private static final String QUOTA = "quota";
   private static final String BALANCED = "balanced";
 
-  private final String name;
+  private String policyName;
 
   private final Map<ServerTableIdentifier, TableRuntime> tableRuntimeMap = new HashMap<>();
   private Comparator<TableRuntime> tableSorter;
   private final Lock tableLock = new ReentrantLock();
 
   public SchedulingPolicy(ResourceGroup group) {
-    this.name =
-        Optional.ofNullable(group.getProperties())
-            .orElseGet(Maps::newHashMap)
-            .getOrDefault(SCHEDULING_POLICY_PROPERTY_NAME, QUOTA);
     setTableSorterIfNeeded(group);
   }
 
   public void setTableSorterIfNeeded(ResourceGroup optimizerGroup) {
-    if (this.name.equalsIgnoreCase(QUOTA)) {
+    policyName =
+        Optional.ofNullable(optimizerGroup.getProperties())
+            .orElseGet(Maps::newHashMap)
+            .getOrDefault(SCHEDULING_POLICY_PROPERTY_NAME, QUOTA);
+    if (policyName.equalsIgnoreCase(QUOTA)) {
       if (tableSorter == null || !(tableSorter instanceof QuotaOccupySorter)) {
         tableSorter = new QuotaOccupySorter();
       }
-    } else if (this.name.equalsIgnoreCase(BALANCED)) {
+    } else if (policyName.equalsIgnoreCase(BALANCED)) {
       if (tableSorter == null || !(tableSorter instanceof BalancedSorter)) {
         tableSorter = new BalancedSorter();
       }
     } else {
-      throw new IllegalArgumentException("Illegal scheduling policy: " + this.name);
+      throw new IllegalArgumentException("Illegal scheduling policy: " + policyName);
     }
   }
 
   public String name() {
-    return name;
+    return policyName;
   }
 
   public List<TableRuntime> scheduleTables() {
