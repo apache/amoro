81894f9758a02ecb0e8bea22b16616809b4a6076
wangzeyu <hameizi369@gmail.com>
2023-12-05 16:35:25 +0800

    [AMORO-2378] The optimizer based on Flink on YARN should prioritize loading the optimizer-job.jar (#2379)
    * load optimizer jar first
    
    * fix code style
    
    * change config name
    
    * add config taskmanager.memory.managed.fraction
    
    * fix

--- ams/server/src/main/java/com/netease/arctic/server/manager/FlinkOptimizerContainer.java
+++ ams/server/src/main/java/com/netease/arctic/server/manager/FlinkOptimizerContainer.java
@@ -186,6 +186,8 @@ public class FlinkOptimizerContainer extends AbstractResourceContainer {
     String flinkAction = target.isApplicationMode() ? "run-application" : "run";
     if (Target.KUBERNETES_APPLICATION == target) {
       addKubernetesProperties(resource, resourceFlinkConf);
+    } else if (Target.YARN_PER_JOB == target || Target.YARN_APPLICATION == target) {
+      addYarnProperties(resourceFlinkConf);
     }
     String flinkOptions = resourceFlinkConf.toCliOptions();
 
@@ -224,6 +226,12 @@ public class FlinkOptimizerContainer extends AbstractResourceContainer {
     String resourceLabel = Joiner.on(',').join(labels);
     flinkConf.putToOptions(FlinkConfKeys.KUBERNETES_TASKMANAGER_LABLES, resourceLabel);
     flinkConf.putToOptions(FlinkConfKeys.KUBERNETES_JOBMANAGER_LABLES, resourceLabel);
+  }
+
+  private void addYarnProperties(FlinkConf flinkConf) {
+    // Load optimizer jar first
+    flinkConf.putToOptions(
+        FlinkConfKeys.CLASSPATH_INCLUDE_USER_JAR, FlinkConfKeys.CLASSPATH_INCLUDE_USER_JAR_DEFAULT);
   }
 
   /**
@@ -416,8 +424,11 @@ public class FlinkOptimizerContainer extends AbstractResourceContainer {
     public static final String JOB_MANAGER_TOTAL_PROCESS_MEMORY = "jobmanager.memory.process.size";
     public static final String TASK_MANAGER_TOTAL_PROCESS_MEMORY =
         "taskmanager.memory.process.size";
-    public static final String KUBERNETES_IMAGE_REF = "kubernetes.container.image";
 
+    public static final String CLASSPATH_INCLUDE_USER_JAR = "yarn.per-job-cluster.include-user-jar";
+    public static final String CLASSPATH_INCLUDE_USER_JAR_DEFAULT = "FIRST";
+
+    public static final String KUBERNETES_IMAGE_REF = "kubernetes.container.image";
     public static final String KUBERNETES_CLUSTER_ID = "kubernetes.cluster-id";
     public static final String KUBERNETES_TASKMANAGER_LABLES = "kubernetes.taskmanager.labels";
     public static final String KUBERNETES_JOBMANAGER_LABLES = "kubernetes.jobmanager.labels";
