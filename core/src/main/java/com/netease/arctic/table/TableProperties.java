5f35ce2095c6977faa1f3e3abdb20814fc19470a
Qishang Zhong <zhongqishang@gmail.com>
2023-12-19 19:59:16 +0800

    [AMORO-2330] Improve major plan (#2332)
    * [AMORO-2330][AMS] Improve major plan
    
    * [AMORO-2330][AMS] remove filter dataFileWith1Pos
    
    * Fix comments
    
    * Fix
    
    * Move the rollback logic of undersized segment to the split task stage
    
    * Rename
    
    * Fix
    
    * Rollback method name `fileShouldRewrite`
    
    * Rollback mixed table format full trigger condition & reuse `isUndersizedSegmentFile`
    
    * Rollback testSegmentFilesBase()
    
    * TreeNodeTaskSplitter logic keep same with bin-pack
    
    * Improve code duplicate
    
    ---------
    
    Co-authored-by: ZhouJinsong <zhoujinsong0505@163.com>

--- core/src/main/java/com/netease/arctic/table/TableProperties.java
+++ core/src/main/java/com/netease/arctic/table/TableProperties.java
@@ -95,6 +95,10 @@ public class TableProperties {
   public static final String SELF_OPTIMIZING_FRAGMENT_RATIO = "self-optimizing.fragment-ratio";
   public static final int SELF_OPTIMIZING_FRAGMENT_RATIO_DEFAULT = 8;
 
+  public static final String SELF_OPTIMIZING_MIN_TARGET_SIZE_RATIO =
+      "self-optimizing.min-target-size-ratio";
+  public static final double SELF_OPTIMIZING_MIN_TARGET_SIZE_RATIO_DEFAULT = 0.75;
+
   public static final String SELF_OPTIMIZING_MINOR_TRIGGER_FILE_CNT =
       "self-optimizing.minor.trigger.file-count";
   public static final int SELF_OPTIMIZING_MINOR_TRIGGER_FILE_CNT_DEFAULT = 12;
