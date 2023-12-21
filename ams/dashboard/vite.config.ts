d016b4cb77a0fc95cc480ff880251de7bb1b3f45
zhangmo8 <wegi866@gmail.com>
2023-12-21 22:52:20 +0800

    fix: resolve svg component & auto import antd-vue component, style & make up the license

--- ams/dashboard/vite.config.ts
+++ ams/dashboard/vite.config.ts
@@ -1,7 +1,28 @@
+/*
+  * Licensed to the Apache Software Foundation (ASF) under one
+  * or more contributor license agreements.  See the NOTICE file
+  * distributed with this work for additional information
+  * regarding copyright ownership.  The ASF licenses this file
+  * to you under the Apache License, Version 2.0 (the
+  * "License"); you may not use this file except in compliance
+  * with the License.  You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteMockServe } from 'vite-plugin-mock'
 import path from 'node:path'
+import {createSvgIconsPlugin} from 'vite-plugin-svg-icons'
+import ViteComponents from 'unplugin-vue-components/vite'
+import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers'
 
 const css = {
   preprocessorOptions: {
@@ -36,6 +57,12 @@ export default defineConfig({
     viteMockServe({
       mockPath: 'mock',
       enable: true,
+    }),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons/svg')],
+    }),
+    ViteComponents({
+      resolvers: [AntDesignVueResolver()],
     })
   ],
   server: {
