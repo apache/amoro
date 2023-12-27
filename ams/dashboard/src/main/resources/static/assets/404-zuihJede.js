b6506009b14234100d3090b8f1578fc78ff9eebc
wangtao <wangtao3@corp.netease.com>
2023-12-27 13:45:16 +0800

    dashboard: build

---
+++ ams/dashboard/src/main/resources/static/assets/404-zuihJede.js
@@ -0,0 +1,20 @@
+
+/*
+  * Licensed to the Apache Software Foundation (ASF) under one
+  * or more contributor license agreements.  See the NOTICE file
+  * distributed with this work for additional information
+  * regarding copyright ownership.  The ASF licenses this file
+  * to you under the Apache License, Version 2.0 (the
+  * "License"); you may not use this file except in compliance
+  * with the License.  You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+import{v as n,x as r,r as _,o as p,e as d,z as a,u as t,h as o,a5 as i,y as l,a1 as u,p as m,a as f}from"./index-42Pv8_vo.js";/* empty css              */const h=n({name:"Page404"}),x=e=>(m("data-v-5c997eea"),e=e(),f(),e),g={class:"g-flex-center"},v={class:"g-flex-col container"},k=x(()=>a("p",{class:"title"},"404 - Not Found",-1));function $(e,B,I,N,S,b){const s=u,c=_("router-link");return p(),d("div",g,[a("div",v,[k,t(c,{to:"/",style:{"text-align":"center"}},{default:o(()=>[t(s,{class:"button g-mt-32"},{default:o(()=>[i(l(e.$t("backHome")),1)]),_:1})]),_:1})])])}const V=r(h,[["render",$],["__scopeId","data-v-5c997eea"]]);export{V as default};
