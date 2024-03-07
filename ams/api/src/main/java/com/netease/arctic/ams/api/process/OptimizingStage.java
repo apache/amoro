4d31f10d7f36b7f945cf7f450596a868a3c61119
majin1102 <majin1102@gmail.com>
2024-03-07 14:32:48 +0800

    Add necessary comments for OptimizingStage

--- ams/api/src/main/java/com/netease/arctic/ams/api/process/OptimizingStage.java
+++ ams/api/src/main/java/com/netease/arctic/ams/api/process/OptimizingStage.java
@@ -20,20 +20,61 @@ package com.netease.arctic.ams.api.process;
 
 /** The stage of the optimizing process. */
 public enum OptimizingStage {
+
+  /**
+   * Full optimizing executing phase
+   */
   FULL_OPTIMIZING("full", true),
+
+  /**
+   * Major optimizing executing phase
+   */
   MAJOR_OPTIMIZING("major", true),
+
+  /**
+   * Minor optimizing executing phase
+   */
   MINOR_OPTIMIZING("minor", true),
+
+  /**
+   * Committing phase of optimizing
+   */
   COMMITTING("committing", true),
+
+  /**
+   * Planning phase of optimizing
+   */
   PLANNING("planning", false),
+
+  /**
+   * When input data has been collected but waiting for quota available(not scheduled yet)
+   */
   PENDING("pending", false),
+
+  /**
+   * When waiting for input data
+   */
   IDLE("idle", false),
+
+  /**
+   * When the process has been scheduled but being waiting for quota available
+   */
   SUSPENDING("suspending", false),
+
+
+  /**
+   * Mainly for external process submitting to external resources
+   */
   SUBMITTING("submitting", false);
 
-  // The display description of the stage.
+  /**
+   * The display description of the stage.
+   */
   private final String displayValue;
 
-  // Whether the stage is an optimizing executing stage.
+  /*
+   * Whether the stage is an optimizing executing stage.
+   */
   private final boolean isOptimizing;
 
   OptimizingStage(String displayValue, boolean isProcessing) {
