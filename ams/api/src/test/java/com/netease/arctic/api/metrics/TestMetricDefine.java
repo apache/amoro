ec6ca7dd36b67165e911526a0b853a7212cb6323
majin1102 <majin1102@gmail.com>
2024-03-13 21:47:35 +0800

    spotless apply

--- ams/api/src/test/java/com/netease/arctic/api/metrics/TestMetricDefine.java
+++ ams/api/src/test/java/com/netease/arctic/api/metrics/TestMetricDefine.java
@@ -18,6 +18,9 @@
 
 package com.netease.arctic.api.metrics;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -25,9 +28,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class TestMetricDefine {
 
