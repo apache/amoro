eba0367fc6e609b5b75a115a98380910d649dbce
huyuanfeng <huyuanfeng@huya.com>
2024-03-26 15:00:49 +0800

    [AMORO-2652] Support Flink Optimizer run in session cluster

---
+++ ams/server/src/main/java/com/netease/arctic/server/utils/FlinkClientUtil.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netease.arctic.server.utils;
+
+import org.apache.flink.client.program.rest.RestClusterClient;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.rest.RestClient;
+
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+
+public class FlinkClientUtil {
+
+  /**
+   * Get Flink RestClusterClient.
+   *
+   * @param flinkRestConf {@link org.apache.flink.configuration.RestOptions}
+   */
+  public static RestClusterClient<String> getRestClusterClient(Map<String, String> flinkRestConf)
+      throws Exception {
+    Configuration configuration = Configuration.fromMap(flinkRestConf);
+    return new RestClusterClient<>(configuration, "Amoro-optimizer-session-cluster");
+  }
+
+  /**
+   * Get Flink RestClient.
+   *
+   * @param flinkRestConf {@link org.apache.flink.configuration.RestOptions}
+   */
+  public static RestClient getRestClient(Map<String, String> flinkRestConf, ExecutorService service)
+      throws Exception {
+    Configuration configuration = Configuration.fromMap(flinkRestConf);
+    return new RestClient(configuration, service);
+  }
+}
