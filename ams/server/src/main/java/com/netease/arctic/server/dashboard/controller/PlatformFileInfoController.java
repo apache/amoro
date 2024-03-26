6fd1c31904f563a5865631e0ecfcb0d7b78d45cc
tcodehuber <tcodehuber@gmail.com>
2024-03-27 00:49:32 +0800

    [AMORO-2520] Validate hive-site.xml when uploading file for Hive-catalog

--- ams/server/src/main/java/com/netease/arctic/server/dashboard/controller/PlatformFileInfoController.java
+++ ams/server/src/main/java/com/netease/arctic/server/dashboard/controller/PlatformFileInfoController.java
@@ -24,7 +24,9 @@ import com.netease.arctic.server.dashboard.response.OkResponse;
 import io.javalin.http.Context;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Base64;
@@ -45,6 +47,17 @@ public class PlatformFileInfoController {
     InputStream bodyAsInputStream = ctx.uploadedFile("file").getContent();
     String name = ctx.uploadedFile("file").getFilename();
     byte[] bytes = IOUtils.toByteArray(bodyAsInputStream);
+
+    // validate xml config
+    if (name.toLowerCase().endsWith(".xml")) {
+      try {
+        Configuration configuration = new Configuration();
+        configuration.addResource(new ByteArrayInputStream(bytes));
+        configuration.setDeprecatedProperties();
+      } catch (Exception e) {
+        throw new IllegalArgumentException("Uploaded file is not in valid XML format");
+      }
+    }
     String content = Base64.getEncoder().encodeToString(bytes);
     Integer fid = platformFileInfoService.addFile(name, content);
     Map<String, String> result = new HashMap<>();
