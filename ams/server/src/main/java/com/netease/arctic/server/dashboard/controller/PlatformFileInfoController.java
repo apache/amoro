fd2b4f3292cfc18e958c0f37015787dd5088b12e
klion26 <qcx978132955@gmail.com>
2024-03-28 11:45:53 +0800

    fix all invalid guava import

--- ams/server/src/main/java/com/netease/arctic/server/dashboard/controller/PlatformFileInfoController.java
+++ ams/server/src/main/java/com/netease/arctic/server/dashboard/controller/PlatformFileInfoController.java
@@ -34,9 +34,7 @@ import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * The controller that handles file requests.
- */
+/** The controller that handles file requests. */
 public class PlatformFileInfoController {
 
   private final PlatformFileManager platformFileInfoService;
@@ -45,9 +43,7 @@ public class PlatformFileInfoController {
     this.platformFileInfoService = platformFileInfoService;
   }
 
-  /**
-   * Upload file.
-   */
+  /** Upload file. */
   public void uploadFile(Context ctx) throws IOException {
     InputStream bodyAsInputStream = ctx.uploadedFile("file").getContent();
     String name = ctx.uploadedFile("file").getFilename();
@@ -72,9 +68,7 @@ public class PlatformFileInfoController {
     ctx.json(OkResponse.of(result));
   }
 
-  /**
-   * Download file.
-   */
+  /** Download file. */
   public void downloadFile(Context ctx) {
     String fileId = ctx.pathParam("fileId");
     Preconditions.checkArgument(StringUtils.isNumeric(fileId), "Invalid file id");
