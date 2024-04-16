7d693dffaf29377775de5f22ef64bd4e2969afd4
zhoujinsong <zhoujinsong0505@163.com>
2024-04-16 11:50:28 +0800

    Change package name for mixed-format/flink

---
+++ mixed-format/flink/flink-common/src/main/java/org/apache/amoro/flink/write/AutomaticDoubleWriteStatus.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.amoro.flink.write;
+
+import com.netease.arctic.table.ArcticTable;
+import org.apache.amoro.flink.table.ArcticTableLoader;
+import org.apache.amoro.flink.table.descriptors.ArcticValidator;
+import org.apache.amoro.flink.util.ArcticUtils;
+import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.iceberg.UpdateProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.time.Duration;
+import java.util.Map;
+
+/** This is an automatic logstore writer util class. */
+public class AutomaticDoubleWriteStatus implements Serializable {
+  private static final Logger LOG = LoggerFactory.getLogger(AutomaticDoubleWriteStatus.class);
+
+  private static final long serialVersionUID = 1L;
+  private final ArcticTableLoader tableLoader;
+  private final AutomaticWriteSpecification specification;
+  private ArcticTable table;
+  private transient boolean shouldDoubleWrite = false;
+  private int subtaskId;
+
+  public AutomaticDoubleWriteStatus(
+      ArcticTableLoader tableLoader, Duration writeLogstoreWatermarkGap) {
+    this.tableLoader = tableLoader;
+    this.specification = new AutomaticWriteSpecification(writeLogstoreWatermarkGap);
+  }
+
+  public void setup(int indexOfThisSubtask) {
+    this.subtaskId = indexOfThisSubtask;
+  }
+
+  public void open() {
+    table = ArcticUtils.loadArcticTable(tableLoader);
+    sync();
+  }
+
+  public boolean isDoubleWrite() {
+    return shouldDoubleWrite;
+  }
+
+  public void processWatermark(Watermark mark) {
+    if (isDoubleWrite()) {
+      return;
+    }
+    if (specification.shouldDoubleWrite(mark.getTimestamp())) {
+      shouldDoubleWrite = true;
+      LOG.info(
+          "processWatermark {}, subTaskId is {}, should double write is true.", mark, subtaskId);
+      LOG.info(
+          "begin update arctic table, set {} to true", ArcticValidator.LOG_STORE_CATCH_UP.key());
+      UpdateProperties updateProperties = table.updateProperties();
+      updateProperties.set(ArcticValidator.LOG_STORE_CATCH_UP.key(), String.valueOf(true));
+      updateProperties.set(
+          ArcticValidator.LOG_STORE_CATCH_UP_TIMESTAMP.key(),
+          String.valueOf(System.currentTimeMillis()));
+      updateProperties.remove(ArcticValidator.AUTO_EMIT_LOGSTORE_WATERMARK_GAP.key());
+      updateProperties.commit();
+      LOG.info("end update arctic table.");
+    }
+  }
+
+  public void sync() {
+    table.refresh();
+    Map<String, String> properties = table.properties();
+    shouldDoubleWrite =
+        !properties.containsKey(ArcticValidator.AUTO_EMIT_LOGSTORE_WATERMARK_GAP.key());
+    LOG.info(
+        "AutomaticDoubleWriteStatus sync, subTaskId: {}, should double write: {}",
+        subtaskId,
+        shouldDoubleWrite);
+  }
+}
