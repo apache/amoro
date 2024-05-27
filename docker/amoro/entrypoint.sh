a705e9dede693cf584e8dd3ad26a03bfa149063d
zhoujinsong <zhoujinsong0505@163.com>
2024-05-27 18:03:38 +0800

    Fix ams start cmd

--- docker/amoro/entrypoint.sh
+++ docker/amoro/entrypoint.sh
@@ -52,7 +52,7 @@ if [ $1 == "help" ]; then
 elif [ "$1" == "ams" ]; then
   args=("${args[@]:1}")
   echo "Start Amoro Management Service"
-  exec "$AMORO_HOME/bin/ams.sh"
+  exec "$AMORO_HOME/bin/ams.sh" "start"
 elif [ "$1" == "optimizer" ]; then
   args=("${args[@]:1}")
   echo "Start Amoro Optimizer"
