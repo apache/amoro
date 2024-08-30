2df7e78e1a3d9fa16220b8e4cd2a4f8fde223b3b
zhoujinsong <zhoujinsong0505@163.com>
2024-08-30 14:31:40 +0800

    Fix some compile errors

--- amoro-ams/amoro-ams-server/src/main/java/org/apache/amoro/server/table/TableSummaryMetrics.java
+++ amoro-ams/amoro-ams-server/src/main/java/org/apache/amoro/server/table/TableSummaryMetrics.java
@@ -18,13 +18,13 @@
 
 package org.apache.amoro.server.table;
 
-import static org.apache.amoro.api.metrics.MetricDefine.defineGauge;
+import static org.apache.amoro.metrics.MetricDefine.defineGauge;
 
-import org.apache.amoro.api.ServerTableIdentifier;
-import org.apache.amoro.api.metrics.Gauge;
-import org.apache.amoro.api.metrics.Metric;
-import org.apache.amoro.api.metrics.MetricDefine;
-import org.apache.amoro.api.metrics.MetricKey;
+import org.apache.amoro.ServerTableIdentifier;
+import org.apache.amoro.metrics.Gauge;
+import org.apache.amoro.metrics.Metric;
+import org.apache.amoro.metrics.MetricDefine;
+import org.apache.amoro.metrics.MetricKey;
 import org.apache.amoro.server.metrics.MetricRegistry;
 import org.apache.amoro.server.optimizing.plan.OptimizingEvaluator;
 import org.apache.amoro.shade.guava32.com.google.common.collect.ImmutableMap;
