b95329aa0cc43114528b7ad0c135b411e5d2ed5e
zhangwl9 <1298877813@qq.com>
2024-11-11 10:49:23 +0800

    [hotfix] Replace OptimizingProcess#Status with ProcessStatus. (#3326)
    (hotfix) Replace OptimizingProcess#Status with ProcessStatus.
    
    Co-authored-by: 张文领 <zhangwl9@chinatelecom.cn>

--- amoro-ams/src/main/java/org/apache/amoro/server/optimizing/OptimizingProcessMeta.java
+++ amoro-ams/src/main/java/org/apache/amoro/server/optimizing/OptimizingProcessMeta.java
@@ -18,6 +18,8 @@
 
 package org.apache.amoro.server.optimizing;
 
+import org.apache.amoro.process.ProcessStatus;
+
 import java.util.Map;
 
 /** Meta of optimizing process. */
@@ -30,7 +32,7 @@ public class OptimizingProcessMeta {
   private String tableName;
   private Long targetSnapshotId;
   private Long targetChangeSnapshotId;
-  private OptimizingProcess.Status status;
+  private ProcessStatus processStatus;
   private OptimizingType optimizingType;
   private long planTime;
   private long endTime;
@@ -89,12 +91,12 @@ public class OptimizingProcessMeta {
     this.targetSnapshotId = targetSnapshotId;
   }
 
-  public OptimizingProcess.Status getStatus() {
-    return status;
+  public ProcessStatus getStatus() {
+    return processStatus;
   }
 
-  public void setStatus(OptimizingProcess.Status status) {
-    this.status = status;
+  public void setStatus(ProcessStatus processStatus) {
+    this.processStatus = processStatus;
   }
 
   public OptimizingType getOptimizingType() {
