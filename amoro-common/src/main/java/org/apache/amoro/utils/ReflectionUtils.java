c75f9038cdc398d08eb66e541d4292026fd3bf29
13574798@qq.com <13574798@qq.com>
2024-12-23 19:34:57 +0800

    add utils for reflection and deal with IBM JDK

--- amoro-common/src/main/java/org/apache/amoro/utils/ReflectionUtils.java
+++ amoro-common/src/main/java/org/apache/amoro/utils/ReflectionUtils.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.amoro.utils;
 
 import java.lang.reflect.InvocationTargetException;
@@ -7,17 +25,21 @@ import java.util.Map;
 
 public class ReflectionUtils {
 
-    private final static Map<String, Class> CLASS_MAP = new HashMap<>();
+  private static final Map<String, Class> CLASS_MAP = new HashMap<>();
 
-    public static Object invoke(String className, String methodName) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
-        Class<?> classRef = CLASS_MAP.computeIfAbsent(className, key -> {
-            try {
+  public static Object invoke(String className, String methodName)
+      throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+    Class<?> classRef =
+        CLASS_MAP.computeIfAbsent(
+            className,
+            key -> {
+              try {
                 return Class.forName(className);
-            } catch (ClassNotFoundException e) {
+              } catch (ClassNotFoundException e) {
                 throw new RuntimeException(e);
-            }
-        });
-        Method method = classRef.getDeclaredMethod(methodName);
-        return method.invoke(null);
-    }
+              }
+            });
+    Method method = classRef.getDeclaredMethod(methodName);
+    return method.invoke(null);
+  }
 }
