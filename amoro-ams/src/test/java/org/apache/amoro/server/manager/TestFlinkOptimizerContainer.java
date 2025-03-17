4b37913aa6791903cee05433c43bba584341648d
jzjsnow <snow.jiangzj@gmail.com>
2025-03-17 18:54:06 +0800

    [Hotfix] Add double quotes to the flink config values when generating the flink optimizer startup command

--- amoro-ams/src/test/java/org/apache/amoro/server/manager/TestFlinkOptimizerContainer.java
+++ amoro-ams/src/test/java/org/apache/amoro/server/manager/TestFlinkOptimizerContainer.java
@@ -18,8 +18,15 @@
 
 package org.apache.amoro.server.manager;
 
+import static org.apache.amoro.server.manager.FlinkOptimizerContainer.FlinkConfKeys.JOB_MANAGER_TOTAL_PROCESS_MEMORY;
+import static org.apache.amoro.server.manager.FlinkOptimizerContainer.FlinkConfKeys.TASK_MANAGER_TOTAL_PROCESS_MEMORY;
+
 import org.apache.amoro.OptimizerProperties;
+import org.apache.amoro.resource.Resource;
+import org.apache.amoro.resource.ResourceGroup;
+import org.apache.amoro.resource.ResourceType;
 import org.apache.amoro.shade.guava32.com.google.common.collect.Maps;
+import org.apache.amoro.utils.MemorySize;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -88,12 +95,8 @@ public class TestFlinkOptimizerContainer {
             .toString(),
         "config.yaml");
     Map<String, String> flinkConfig = container.loadFlinkConfigForYAML(newFlinkConfResourceUrl);
-    Assert.assertEquals(
-        flinkConfig.get(FlinkOptimizerContainer.FlinkConfKeys.TASK_MANAGER_TOTAL_PROCESS_MEMORY),
-        "1728m");
-    Assert.assertEquals(
-        flinkConfig.get(FlinkOptimizerContainer.FlinkConfKeys.JOB_MANAGER_TOTAL_PROCESS_MEMORY),
-        "1600m");
+    Assert.assertEquals(flinkConfig.get(TASK_MANAGER_TOTAL_PROCESS_MEMORY), "1728m");
+    Assert.assertEquals(flinkConfig.get(JOB_MANAGER_TOTAL_PROCESS_MEMORY), "1600m");
   }
 
   @Test
@@ -114,28 +117,24 @@ public class TestFlinkOptimizerContainer {
             .build();
     String flinkOptions = conf.toCliOptions();
     Assert.assertEquals(3, flinkOptions.split(" ").length);
-    Assert.assertTrue(flinkOptions.contains("-Dkey1=value1"));
-    Assert.assertTrue(flinkOptions.contains("-Dkey2=value4"));
-    Assert.assertTrue(flinkOptions.contains("-Dkey5=value5"));
+    Assert.assertTrue(flinkOptions.contains("-Dkey1=\"value1\""));
+    Assert.assertTrue(flinkOptions.contains("-Dkey2=\"value4\""));
+    Assert.assertTrue(flinkOptions.contains("-Dkey5=\"value5\""));
   }
 
   @Test
   public void testGetMemorySizeValue() {
     HashMap<String, String> prop = new HashMap<>();
-    prop.put(FlinkOptimizerContainer.FlinkConfKeys.TASK_MANAGER_TOTAL_PROCESS_MEMORY, "100");
-    prop.put(FlinkOptimizerContainer.FlinkConfKeys.JOB_MANAGER_TOTAL_PROCESS_MEMORY, "100");
+    prop.put(TASK_MANAGER_TOTAL_PROCESS_MEMORY, "100");
+    prop.put(JOB_MANAGER_TOTAL_PROCESS_MEMORY, "100");
 
     FlinkOptimizerContainer.FlinkConf conf =
         FlinkOptimizerContainer.FlinkConf.buildFor(prop, Maps.newHashMap()).build();
 
     Assert.assertEquals(
-        0,
-        container.getMemorySizeValue(
-            prop, conf, FlinkOptimizerContainer.FlinkConfKeys.TASK_MANAGER_TOTAL_PROCESS_MEMORY));
+        0, container.getMemorySizeValue(prop, conf, TASK_MANAGER_TOTAL_PROCESS_MEMORY));
     Assert.assertEquals(
-        0,
-        container.getMemorySizeValue(
-            prop, conf, FlinkOptimizerContainer.FlinkConfKeys.JOB_MANAGER_TOTAL_PROCESS_MEMORY));
+        0, container.getMemorySizeValue(prop, conf, JOB_MANAGER_TOTAL_PROCESS_MEMORY));
 
     Map<String, String> containerProperties = Maps.newHashMap();
     containerProperties.put("flink-conf.jobmanager.memory.process.size", "200 M");
@@ -143,50 +142,84 @@ public class TestFlinkOptimizerContainer {
     conf = FlinkOptimizerContainer.FlinkConf.buildFor(prop, containerProperties).build();
     prop.clear();
     Assert.assertEquals(
-        0,
-        container.getMemorySizeValue(
-            prop, conf, FlinkOptimizerContainer.FlinkConfKeys.TASK_MANAGER_TOTAL_PROCESS_MEMORY));
+        0, container.getMemorySizeValue(prop, conf, TASK_MANAGER_TOTAL_PROCESS_MEMORY));
     Assert.assertEquals(
-        200L,
-        container.getMemorySizeValue(
-            prop, conf, FlinkOptimizerContainer.FlinkConfKeys.JOB_MANAGER_TOTAL_PROCESS_MEMORY));
+        200L, container.getMemorySizeValue(prop, conf, JOB_MANAGER_TOTAL_PROCESS_MEMORY));
 
     prop.clear();
     containerProperties = Maps.newHashMap();
     conf = FlinkOptimizerContainer.FlinkConf.buildFor(prop, containerProperties).build();
 
-    prop.put(FlinkOptimizerContainer.FlinkConfKeys.TASK_MANAGER_TOTAL_PROCESS_MEMORY, "300 M");
-    prop.put(FlinkOptimizerContainer.FlinkConfKeys.JOB_MANAGER_TOTAL_PROCESS_MEMORY, "300");
+    prop.put(TASK_MANAGER_TOTAL_PROCESS_MEMORY, "300 M");
+    prop.put(JOB_MANAGER_TOTAL_PROCESS_MEMORY, "300");
     Assert.assertEquals(
-        300L,
-        container.getMemorySizeValue(
-            prop, conf, FlinkOptimizerContainer.FlinkConfKeys.TASK_MANAGER_TOTAL_PROCESS_MEMORY));
+        300L, container.getMemorySizeValue(prop, conf, TASK_MANAGER_TOTAL_PROCESS_MEMORY));
     Assert.assertEquals(
-        0,
-        container.getMemorySizeValue(
-            prop, conf, FlinkOptimizerContainer.FlinkConfKeys.JOB_MANAGER_TOTAL_PROCESS_MEMORY));
+        0, container.getMemorySizeValue(prop, conf, JOB_MANAGER_TOTAL_PROCESS_MEMORY));
 
     conf = FlinkOptimizerContainer.FlinkConf.buildFor(Maps.newHashMap(), Maps.newHashMap()).build();
     prop.clear();
     Assert.assertEquals(
-        0L,
-        container.getMemorySizeValue(
-            prop, conf, FlinkOptimizerContainer.FlinkConfKeys.TASK_MANAGER_TOTAL_PROCESS_MEMORY));
+        0L, container.getMemorySizeValue(prop, conf, TASK_MANAGER_TOTAL_PROCESS_MEMORY));
     Assert.assertEquals(
-        0L,
-        container.getMemorySizeValue(
-            prop, conf, FlinkOptimizerContainer.FlinkConfKeys.JOB_MANAGER_TOTAL_PROCESS_MEMORY));
+        0L, container.getMemorySizeValue(prop, conf, JOB_MANAGER_TOTAL_PROCESS_MEMORY));
 
-    prop.put(FlinkOptimizerContainer.FlinkConfKeys.JOB_MANAGER_TOTAL_PROCESS_MEMORY, "400 MB");
-    prop.put(FlinkOptimizerContainer.FlinkConfKeys.TASK_MANAGER_TOTAL_PROCESS_MEMORY, "400 MB");
+    prop.put(JOB_MANAGER_TOTAL_PROCESS_MEMORY, "400 MB");
+    prop.put(TASK_MANAGER_TOTAL_PROCESS_MEMORY, "400 MB");
     conf = FlinkOptimizerContainer.FlinkConf.buildFor(prop, Maps.newHashMap()).build();
     Assert.assertEquals(
-        400L,
-        container.getMemorySizeValue(
-            prop, conf, FlinkOptimizerContainer.FlinkConfKeys.TASK_MANAGER_TOTAL_PROCESS_MEMORY));
+        400L, container.getMemorySizeValue(prop, conf, TASK_MANAGER_TOTAL_PROCESS_MEMORY));
     Assert.assertEquals(
-        400L,
-        container.getMemorySizeValue(
-            prop, conf, FlinkOptimizerContainer.FlinkConfKeys.JOB_MANAGER_TOTAL_PROCESS_MEMORY));
+        400L, container.getMemorySizeValue(prop, conf, JOB_MANAGER_TOTAL_PROCESS_MEMORY));
+  }
+
+  @Test
+  public void testBuildFlinkOptionsWithSpaces() {
+    Map<String, String> containerProperties = Maps.newHashMap(this.containerProperties);
+    containerProperties.put("flink-conf.key1", "value1");
+    containerProperties.put(
+        "flink-conf.taskmanager.memory.process.size", MemorySize.parse("10240mb").toString());
+    containerProperties.put("jobmanager.memory", "value3"); // non "flink-conf." property
+
+    // Create some optimizing group properties
+    Map<String, String> groupProperties = new HashMap<>();
+    groupProperties.put("flink-conf.key1", "value1 with spaces");
+    groupProperties.put(
+        "flink-conf.jobmanager.memory.process.size", MemorySize.parse("1G").toString());
+
+    FlinkOptimizerContainer.FlinkConf conf =
+        FlinkOptimizerContainer.FlinkConf.buildFor(Maps.newHashMap(), containerProperties)
+            .withGroupProperties(groupProperties)
+            .build();
+    String flinkOptions = conf.toCliOptions();
+    Assert.assertTrue(flinkOptions.contains("-Dkey1=\"value1 with spaces\""));
+    Assert.assertTrue(flinkOptions.contains("-Dtaskmanager.memory.process.size=\"10 gb\""));
+    Assert.assertTrue(flinkOptions.contains("-Djobmanager.memory.process.size=\"1 gb\""));
+  }
+
+  @Test
+  public void testBuildFlinkOptimizerStartupArgsString() {
+    Map<String, String> containerProperties = Maps.newHashMap(this.containerProperties);
+    containerProperties.put("jobmanager.memory.process.size", "100MB");
+    containerProperties.put("taskmanager.memory.process.size", "10240 MB");
+    containerProperties.put("flink-conf.key1", "value1");
+    containerProperties.put("key2", "value2");
+
+    ResourceGroup resourceGroup =
+        new ResourceGroup.Builder("default", "flinkContainer")
+            .addProperties(containerProperties)
+            .build();
+    Resource resource =
+        new Resource.Builder(
+                resourceGroup.getContainer(), resourceGroup.getName(), ResourceType.OPTIMIZER)
+            .setProperties(resourceGroup.getProperties())
+            .setThreadCount(2)
+            .build();
+    container.init("test", containerProperties);
+    String startUpArgs = container.buildOptimizerStartupArgsString(resource);
+
+    Assert.assertTrue(startUpArgs.contains("-Dkey1=\"value1\""));
+    Assert.assertTrue(startUpArgs.contains("-Djobmanager.memory.process.size=\"100 mb\""));
+    Assert.assertTrue(startUpArgs.contains("-Dtaskmanager.memory.process.size=\"10 gb\""));
   }
 }
