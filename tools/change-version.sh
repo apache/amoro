d3bd90de49363985dea0d976ac4fbc56438d5f01
Aireed <wtrunwolf@gmail.com>
2025-04-01 10:55:06 +0800

    change Apache amoro version 0.8-snapshot to 0.8.0-incubating

--- tools/change-version.sh
+++ tools/change-version.sh
@@ -17,8 +17,8 @@
 # limitations under the License.
 ################################################################################
 
-OLD="0.7-SNAPSHOT"
-NEW="0.8-SNAPSHOT"
+OLD="0.8-SNAPSHOT"
+NEW="0.8.0-incubating"
 
 
 HERE=` basename "$PWD"`
