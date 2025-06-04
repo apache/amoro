a0a87d704b086a0000cf170681d8f0829c89d6ee
Darcy <lintingbin@lilith.com>
2025-06-04 20:03:02 +0800

    Optimize log output for SchedulingPolicy (#3592)
    (cherry picked from commit 9df3f3ef2ece22f6fc339f3df9fd028857fe010f)

--- amoro-ams/src/main/java/org/apache/amoro/server/optimizing/SchedulingPolicy.java
+++ amoro-ams/src/main/java/org/apache/amoro/server/optimizing/SchedulingPolicy.java
@@ -101,7 +101,7 @@ public class SchedulingPolicy {
   private Comparator<TableRuntime> createSorterByPolicy() {
     if (sorterFactoryCache.get(policyName) != null) {
       SorterFactory sorterFactory = sorterFactoryCache.get(policyName);
-      LOG.info(
+      LOG.debug(
           "Using sorter instance {} corresponding to the scheduling policy {}",
           sorterFactory.getClass().getName(),
           policyName);
