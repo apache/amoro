05ea1f858cc1cd5a6416e0be299fad87a0457c1a
Marig_Weizhi <marig.weizhi@foxmail.com>
2025-07-24 00:57:46 +0800

    set clients if not exists

--- amoro-ams/src/test/java/org/apache/amoro/server/table/TestCatalogService.java
+++ amoro-ams/src/test/java/org/apache/amoro/server/table/TestCatalogService.java
@@ -18,6 +18,8 @@
 
 package org.apache.amoro.server.table;
 
+import static org.apache.amoro.properties.CatalogMetaProperties.CLIENT_POOL_SIZE;
+
 import org.apache.amoro.TableFormat;
 import org.apache.amoro.TestedCatalogs;
 import org.apache.amoro.api.CatalogMeta;
@@ -133,6 +135,16 @@ public class TestCatalogService extends AMSServiceTestBase {
         IllegalMetadataException.class,
         () -> CATALOG_MANAGER.dropCatalog(catalogMeta.getCatalogName()));
     catalog.dropDatabase("test_db");
+    CATALOG_MANAGER.dropCatalog(catalogMeta.getCatalogName());
+  }
+
+  @Test
+  public void testCreateCatalogWithProperties() {
+    CatalogMeta catalogMeta = catalogTestHelper.buildCatalogMeta("/tmp");
+    catalogMeta.getCatalogProperties().put(CLIENT_POOL_SIZE, "15");
+    CATALOG_MANAGER.createCatalog(catalogMeta);
+    CatalogMeta readCatalogMeta = CATALOG_MANAGER.getCatalogMeta(catalogMeta.getCatalogName());
+    Assert.assertEquals("15", readCatalogMeta.getCatalogProperties().get(CLIENT_POOL_SIZE));
     CATALOG_MANAGER.dropCatalog(catalogMeta.getCatalogName());
   }
 }
