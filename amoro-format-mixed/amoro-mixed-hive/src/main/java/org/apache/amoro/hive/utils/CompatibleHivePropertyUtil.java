45313a007e013841bd86a4c25ecf9886ae0067b1
SalvadorRomo <salvador.romo@wizeline.com>
2025-08-11 00:17:16 -0600

    [AMORO-3691] Remove deprecated metrics and update related logic in deprectaed dependencies (#3709)
    * remove deprectaed variables
    
    * fix test case
    
    * fix test cases
    
    * fix test cases
    
    * fix branch
    
    * fix test case

--- amoro-format-mixed/amoro-mixed-hive/src/main/java/org/apache/amoro/hive/utils/CompatibleHivePropertyUtil.java
+++ amoro-format-mixed/amoro-mixed-hive/src/main/java/org/apache/amoro/hive/utils/CompatibleHivePropertyUtil.java
@@ -18,7 +18,6 @@
 
 package org.apache.amoro.hive.utils;
 
-import org.apache.amoro.properties.HiveTableProperties;
 import org.apache.iceberg.util.PropertyUtil;
 
 import java.util.Map;
@@ -30,54 +29,26 @@ public class CompatibleHivePropertyUtil {
 
   public static boolean propertyAsBoolean(
       Map<String, String> properties, String property, boolean defaultValue) {
-    return PropertyUtil.propertyAsBoolean(
-        properties, getCompatibleProperty(properties, property), defaultValue);
+    return PropertyUtil.propertyAsBoolean(properties, property, defaultValue);
   }
 
   public static double propertyAsDouble(
       Map<String, String> properties, String property, double defaultValue) {
-    return PropertyUtil.propertyAsDouble(
-        properties, getCompatibleProperty(properties, property), defaultValue);
+    return PropertyUtil.propertyAsDouble(properties, property, defaultValue);
   }
 
   public static int propertyAsInt(
       Map<String, String> properties, String property, int defaultValue) {
-    return PropertyUtil.propertyAsInt(
-        properties, getCompatibleProperty(properties, property), defaultValue);
+    return PropertyUtil.propertyAsInt(properties, property, defaultValue);
   }
 
   public static long propertyAsLong(
       Map<String, String> properties, String property, long defaultValue) {
-    return PropertyUtil.propertyAsLong(
-        properties, getCompatibleProperty(properties, property), defaultValue);
+    return PropertyUtil.propertyAsLong(properties, property, defaultValue);
   }
 
   public static String propertyAsString(
       Map<String, String> properties, String property, String defaultValue) {
-    return PropertyUtil.propertyAsString(
-        properties, getCompatibleProperty(properties, property), defaultValue);
-  }
-
-  private static String getCompatibleProperty(Map<String, String> properties, String property) {
-    String legacyProperty = getLegacyProperty(property);
-    if (legacyProperty != null
-        && properties.containsKey(legacyProperty)
-        && !properties.containsKey(property)) {
-      return legacyProperty;
-    } else {
-      return property;
-    }
-  }
-
-  private static String getLegacyProperty(String property) {
-    if (property == null) {
-      return null;
-    }
-    switch (property) {
-      case HiveTableProperties.MIXED_TABLE_FLAG:
-        return HiveTableProperties.AMORO_TABLE_FLAG_LEGACY;
-      default:
-        return null;
-    }
+    return PropertyUtil.propertyAsString(properties, property, defaultValue);
   }
 }
