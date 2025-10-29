ac2f468ed28b447a070e788ba0cd7f8ce745a9cf
wardli <wardli@tencent.com>
2025-10-29 17:49:41 +0800

    [Subtask]: Use a new configuration item to control whether master & slave mode is enabled. #3845

--- amoro-ams/src/main/java/org/apache/amoro/server/ha/ZkHighAvailabilityContainer.java
+++ amoro-ams/src/main/java/org/apache/amoro/server/ha/ZkHighAvailabilityContainer.java
@@ -140,6 +140,10 @@ public class ZkHighAvailabilityContainer implements HighAvailabilityContainer, L
     LOG.info("Became the leader of AMS");
   }
 
+  public void registAndElect() throws Exception {
+    // TODO Here you can register for AMS and participate in the election.
+  }
+
   @Override
   public void registAndElect() throws Exception {
     // TODO Here you can register for AMS and participate in the election.
