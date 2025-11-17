3d1e3bc18334fe0549a010f968ebd7c3528daa59
Wang, Fei <fwang12@ebay.com>
2025-11-17 11:20:35 -0800

    [AMORO-3873][FOLLOWUP] Rename rest auth bearer type to JWT

--- amoro-common/src/main/java/org/apache/amoro/authentication/TokenAuthenticationProvider.java
+++ amoro-common/src/main/java/org/apache/amoro/authentication/TokenAuthenticationProvider.java
@@ -24,12 +24,11 @@ import java.security.Principal;
 
 public interface TokenAuthenticationProvider {
   /**
-   * TokenAuthenticationProvider is used by the Amoro server authentication layer to validate Bearer
-   * tokens, such as JWT (JSON Web Token), provided in client requests. If the token is invalid,
-   * expired, or fails signature verification, a {@link SignatureCheckException} should be thrown to
-   * deny access.
+   * TokenAuthenticationProvider is used by the Amoro server authentication layer to validate JSON
+   * Web Token (JWT) provided in client requests. If the token is invalid, expired, or fails
+   * signature verification, a {@link SignatureCheckException} should be thrown to deny access.
    *
-   * @param credential The Bearer token credential (e.g., JWT) received in the connection request
+   * @param credential The JSON Web Token credential received in the connection request
    * @return The {@link Principal} associated with the authenticated token
    * @throws SignatureCheckException If the token is invalid, expired, or fails verification
    */
