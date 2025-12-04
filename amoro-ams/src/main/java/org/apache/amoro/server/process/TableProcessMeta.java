1b03bb77846a5084175e26283b743d8a7f592921
LiangDai-Mars <dd574852610@gmail.com>
2025-12-04 21:33:24 +0800

    [Feature] Introduce a new framework that supports scheduling for Formats and Processes (#3924)
    * process service poc
    
    * Refactor table process framework
    
    * Optimize and refactor the process service framework & support framework test case
    
    ---------
    
    Co-authored-by: dailiang <dailiang@bytedance.com>
    Co-authored-by: zhangyongxiang.alpha <zhangyongxiang.alpha@bytedance.com>
    Co-authored-by: baiyangtx <xiangnebula@163.com>

--- amoro-ams/src/main/java/org/apache/amoro/server/process/TableProcessMeta.java
+++ amoro-ams/src/main/java/org/apache/amoro/server/process/TableProcessMeta.java
@@ -19,19 +19,25 @@
 package org.apache.amoro.server.process;
 
 import org.apache.amoro.process.ProcessStatus;
+import org.apache.amoro.process.TableProcessState;
+import org.apache.amoro.process.TableProcessStore;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class TableProcessMeta {
   private long processId;
   private long tableId;
+  private volatile String externalProcessIdentifier;
   private ProcessStatus status;
   private String processType;
   private String processStage;
   private String executionEngine;
+  private int retryNumber;
   private long createTime;
   private long finishTime;
   private String failMessage;
+  private Map<String, String> processParameters;
   private Map<String, String> summary;
 
   public long getProcessId() {
@@ -112,5 +118,116 @@ public class TableProcessMeta {
 
   public void setSummary(Map<String, String> summary) {
     this.summary = summary;
+  }
+
+  public String getExternalProcessIdentifier() {
+    return externalProcessIdentifier;
+  }
+
+  public void setExternalProcessIdentifier(String externalProcessIdentifier) {
+    this.externalProcessIdentifier = externalProcessIdentifier;
+  }
+
+  public int getRetryNumber() {
+    return retryNumber;
+  }
+
+  public void setRetryNumber(int retryNumber) {
+    this.retryNumber = retryNumber;
+  }
+
+  public Map<String, String> getProcessParameters() {
+    return processParameters;
+  }
+
+  public void setProcessParameters(Map<String, String> processParameters) {
+    this.processParameters = processParameters;
+  }
+
+  public TableProcessMeta copy() {
+    TableProcessMeta meta = new TableProcessMeta();
+
+    meta.setProcessId(this.processId);
+    meta.setTableId(this.tableId);
+    meta.setRetryNumber(this.retryNumber);
+    meta.setCreateTime(this.createTime);
+    meta.setFinishTime(this.finishTime);
+
+    meta.setExternalProcessIdentifier(this.externalProcessIdentifier);
+    meta.setProcessType(this.processType);
+    meta.setProcessStage(this.processStage);
+    meta.setExecutionEngine(this.executionEngine);
+    meta.setFailMessage(this.failMessage);
+
+    meta.setStatus(this.status);
+
+    if (this.processParameters != null) {
+      meta.setProcessParameters(new HashMap<>(this.processParameters));
+    }
+    if (this.summary != null) {
+      meta.setSummary(new HashMap<>(this.summary));
+    }
+
+    return meta;
+  }
+
+  public static TableProcessMeta fromTableProcessStore(TableProcessStore tableProcessStore) {
+    TableProcessMeta tableProcessMeta = new TableProcessMeta();
+    tableProcessMeta.setProcessId(tableProcessStore.getProcessId());
+    tableProcessMeta.setTableId(tableProcessStore.getTableId());
+    tableProcessMeta.setExternalProcessIdentifier(tableProcessStore.getExternalProcessIdentifier());
+    tableProcessMeta.setStatus(tableProcessStore.getStatus());
+    tableProcessMeta.setProcessType(tableProcessStore.getProcessType());
+    tableProcessMeta.setProcessStage(tableProcessStore.getProcessStage());
+    tableProcessMeta.setExecutionEngine(tableProcessStore.getExecutionEngine());
+    tableProcessMeta.setRetryNumber(tableProcessStore.getRetryNumber());
+    tableProcessMeta.setCreateTime(tableProcessStore.getCreateTime());
+    tableProcessMeta.setFinishTime(tableProcessStore.getFinishTime());
+    tableProcessMeta.setFailMessage(tableProcessStore.getFailMessage());
+    tableProcessMeta.setProcessParameters(tableProcessStore.getProcessParameters());
+    tableProcessMeta.setSummary(tableProcessStore.getSummary());
+    return tableProcessMeta;
+  }
+
+  @Deprecated
+  public static TableProcessMeta fromTableProcessState(TableProcessState tableProcessState) {
+    TableProcessMeta tableProcessMeta = new TableProcessMeta();
+    tableProcessMeta.setProcessId(tableProcessState.getId());
+    tableProcessMeta.setTableId(tableProcessState.getTableIdentifier().getId());
+    tableProcessMeta.setExternalProcessIdentifier(tableProcessState.getExternalProcessIdentifier());
+    tableProcessMeta.setStatus(tableProcessState.getStatus());
+    tableProcessMeta.setProcessType(tableProcessState.getAction().getName());
+    tableProcessMeta.setProcessStage(tableProcessState.getStage().getDesc());
+    tableProcessMeta.setExecutionEngine(tableProcessState.getExecutionEngine());
+    tableProcessMeta.setRetryNumber(tableProcessState.getRetryNumber());
+    tableProcessMeta.setCreateTime(tableProcessState.getStartTime());
+    tableProcessMeta.setFinishTime(tableProcessState.getEndTime());
+    tableProcessMeta.setFailMessage(tableProcessState.getFailedReason());
+    tableProcessMeta.setProcessParameters(tableProcessState.getProcessParameters());
+    tableProcessMeta.setSummary(tableProcessState.getSummary());
+    return tableProcessMeta;
+  }
+
+  public static TableProcessMeta of(
+      long processId,
+      long tableId,
+      String actionName,
+      String executionEngine,
+      Map<String, String> processParameters) {
+    TableProcessMeta tableProcessMeta = new TableProcessMeta();
+    tableProcessMeta.setProcessId(processId);
+    tableProcessMeta.setTableId(tableId);
+    tableProcessMeta.setExternalProcessIdentifier("");
+    tableProcessMeta.setStatus(ProcessStatus.UNKNOWN);
+    tableProcessMeta.setProcessType(actionName);
+    tableProcessMeta.setProcessStage(ProcessStatus.UNKNOWN.name());
+    tableProcessMeta.setExecutionEngine(executionEngine);
+    tableProcessMeta.setRetryNumber(0);
+    tableProcessMeta.setCreateTime(System.currentTimeMillis());
+    tableProcessMeta.setFinishTime(0);
+    tableProcessMeta.setFailMessage("");
+    tableProcessMeta.setProcessParameters(processParameters);
+    tableProcessMeta.setSummary(new HashMap<>());
+    return tableProcessMeta;
   }
 }
