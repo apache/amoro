731ccb7aca9d1e49f9cf29fc25bc000e3869ea30
zhangyongxiang.alpha <zhangyongxiang.alpha@bytedance.com>
2026-02-18 18:34:38 +0800

    improvement: refactor process API and table runtime extension interfaces
    # Context
    
    Split from upstream PR apache/amoro#4081 to make review easier.
    
    This MR introduces the new process extension APIs in common module, without touching most AMS internals yet.
    
    # Changes
    
    - Promote `ProcessFactory` to a richer abstraction in `amoro-common`, preparing for plugin-based table process extension.
    - Adjust `Action`/`IcebergActions` and `TableRuntime` to align with the new process model.
    - Move `ActionCoordinator` into `amoro-common` so it can be shared as a public abstraction.
    - Introduce `ProcessTriggerStrategy` to describe trigger policies for processes.
    - Clean up legacy process state classes which will be replaced by the new abstractions in follow-up MRs.
    
    # Notes
    
    - This commit only updates the common module and the shared `ActionCoordinator` API; AMS-side wiring and runtime refactors will be done in separate branches/MRs as discussed.
    
    Co-Authored-By: Aime <aime@bytedance.com>
    Change-Id: If84ada8fcae1cfb11577d56d3866db7ce0949102

--- amoro-common/src/main/java/org/apache/amoro/process/ActionCoordinator.java
+++ amoro-common/src/main/java/org/apache/amoro/process/ActionCoordinator.java
@@ -16,25 +16,19 @@
  * limitations under the License.
  */
 
-package org.apache.amoro.server.process;
+package org.apache.amoro.process;
 
 import org.apache.amoro.Action;
-import org.apache.amoro.ActivePlugin;
 import org.apache.amoro.TableFormat;
 import org.apache.amoro.TableRuntime;
-import org.apache.amoro.process.TableProcess;
-import org.apache.amoro.process.TableProcessStore;
-import org.apache.amoro.server.utils.SnowflakeIdGenerator;
+
+import java.util.Optional;
 
 /**
  * Coordinator for a specific {@link org.apache.amoro.Action} to manage table processes. Provides
  * scheduling parameters and lifecycle hooks to create/recover/cancel/retry table processes.
  */
-public interface ActionCoordinator extends ActivePlugin {
-
-  String PROPERTY_PARALLELISM = "parallelism";
-
-  SnowflakeIdGenerator SNOWFLAKE_ID_GENERATOR = new SnowflakeIdGenerator();
+public interface ActionCoordinator {
 
   /**
    * Check whether the given table format is supported by this coordinator.
@@ -57,13 +51,6 @@ public interface ActionCoordinator extends ActivePlugin {
    * @return action type
    */
   Action action();
-
-  /**
-   * Get the execution engine name used by this coordinator.
-   *
-   * @return execution engine name
-   */
-  String executionEngine();
 
   /**
    * Calculate the next executing time for the given table runtime.
@@ -94,7 +81,7 @@ public interface ActionCoordinator extends ActivePlugin {
    * @param tableRuntime table runtime
    * @return a new table process
    */
-  TableProcess createTableProcess(TableRuntime tableRuntime);
+  Optional<TableProcess> trigger(TableRuntime tableRuntime);
 
   /**
    * Recover a {@link TableProcess} from persisted store.
@@ -104,21 +91,4 @@ public interface ActionCoordinator extends ActivePlugin {
    * @return recovered table process
    */
   TableProcess recoverTableProcess(TableRuntime tableRuntime, TableProcessStore processStore);
-
-  /**
-   * Prepare a {@link TableProcess} for cancellation.
-   *
-   * @param tableRuntime table runtime
-   * @param process table process to cancel
-   * @return the process instance to be canceled
-   */
-  TableProcess cancelTableProcess(TableRuntime tableRuntime, TableProcess process);
-
-  /**
-   * Prepare a {@link TableProcess} for retrying.
-   *
-   * @param process table process to retry
-   * @return the process instance to be retried
-   */
-  TableProcess retryTableProcess(TableProcess process);
 }
--- amoro-common/src/main/java/org/apache/amoro/process/ProcessFactory.java
+++ amoro-common/src/main/java/org/apache/amoro/process/ProcessFactory.java
@@ -19,23 +19,42 @@
 package org.apache.amoro.process;
 
 import org.apache.amoro.Action;
+import org.apache.amoro.ActivePlugin;
+import org.apache.amoro.TableFormat;
 import org.apache.amoro.TableRuntime;
+import org.apache.amoro.shade.guava32.com.google.common.collect.Lists;
+import org.apache.amoro.table.StateKey;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * A factory to create a process. Normally, There will be default ProcessFactories for each action
  * and used by default scheduler. Meanwhile, user could extend external ProcessFactory to run jobs
  * on external resources like Yarn.
  */
-public interface ProcessFactory {
+public interface ProcessFactory extends ActivePlugin {
+
+  default List<StateKey<?>> requiredStates() {
+    return Lists.newArrayList();
+  }
+
+  /** Get supported actions for each table format. */
+  Map<TableFormat, Set<Action>> supportedActions();
+
+  /** How to trigger a process for the action. */
+  ProcessTriggerStrategy triggerStrategy(TableFormat format, Action action);
 
   /**
-   * Create a process for the action.
+   * Try trigger a process for the action.
    *
    * @param tableRuntime table runtime
    * @param action action type
    * @return target process which has not been submitted yet.
    */
-  AmoroProcess create(TableRuntime tableRuntime, Action action);
+  Optional<TableProcess> trigger(TableRuntime tableRuntime, Action action);
 
   /**
    * Recover a process for the action from a state.
@@ -44,5 +63,5 @@ public interface ProcessFactory {
    * @param state state of the process
    * @return target process which has not been submitted yet.
    */
-  AmoroProcess recover(TableRuntime tableRuntime, TableProcessState state);
+  TableProcess recover(TableRuntime tableRuntime, TableProcessStore store);
 }
