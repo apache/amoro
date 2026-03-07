bd2aa806c3370d6bd47c9e59ced23ed935edf1aa
zhangyongxiang.alpha <zhangyongxiang.alpha@bytedance.com>
2026-03-07 11:54:17 +0800

    [Feature] Introduce a new plugin RestExtension to extend rest service
    Cherry-picked from 2010d5998a0770fa7b58456e51d63538af9e5e78.
    
    Co-Authored-By: Aime <aime@bytedance.com>
    Change-Id: Iedfc5a383dabe42c96ce4030411cb93adc468a77

---
+++ amoro-ams/src/main/java/org/apache/amoro/server/RestExtensionManager.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.amoro.server;
+
+import org.apache.amoro.config.ConfigOption;
+import org.apache.amoro.config.ConfigOptions;
+import org.apache.amoro.config.Configurations;
+import org.apache.amoro.server.catalog.CatalogManager;
+import org.apache.amoro.server.manager.AbstractPluginManager;
+import org.apache.amoro.server.manager.PluginConfiguration;
+import org.apache.amoro.server.table.TableManager;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class RestExtensionManager extends AbstractPluginManager<RestExtensionFactory> {
+
+  private static final String REST_EXTENSION_PLUGIN_CATEGORY = "rest-extension";
+  private final Configurations configurations;
+
+  public RestExtensionManager(Configurations configurations) {
+    super(REST_EXTENSION_PLUGIN_CATEGORY);
+    this.configurations = configurations;
+  }
+
+  @Override
+  protected List<PluginConfiguration> loadPluginConfigurations() {
+    Set<String> restExtensions = getFoundedPlugins().keySet();
+    return restExtensions.stream()
+        .filter(this::enabled)
+        .map(PluginConfiguration::emptyConfig)
+        .collect(Collectors.toList());
+  }
+
+  private boolean enabled(String name) {
+    ConfigOption<Boolean> key =
+        ConfigOptions.key("http-server." + name + ".enabled").booleanType().defaultValue(false);
+    return configurations.getBoolean(key);
+  }
+
+  public List<RestExtension> loadExtensions(
+      Configurations serviceConfig, CatalogManager catalogManager, TableManager tableManager) {
+    List<RestExtensionFactory> factories = installedPlugins();
+    return factories.stream()
+        .map(
+            factory ->
+                factory
+                    .withServiceConfig(serviceConfig)
+                    .withCatalogManager(catalogManager)
+                    .withTableManager(tableManager)
+                    .build())
+        .collect(Collectors.toList());
+  }
+}
