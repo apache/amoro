63452547943a275e30a7ef39caefb1fc103c96db
Lin Tingbin <lintingbin31@gmail.com>
2026-05-01 22:26:11 +0800

    [AMORO-3974][common] Migrate self-contained tests from JUnit 4 to JUnit 5
    Migrate the JUnit 4 test classes in amoro-common that have no
    external JUnit 4 consumers to JUnit 5 (org.junit.jupiter):
    
    - TestLocalExecutionEngine
    - TestSimpleFuture
    - JacksonUtilTest
    - MemorySizeTest
    - TestPoolConfig
    - MockZookeeperServer
    
    Shared test base classes and rule helpers (TestAms, TestHMS,
    AmoroCatalogTestBase, TestAmoroCatalogBase, TestServerTableDescriptor,
    AmoroRunListener) are kept on JUnit 4 because they are still extended
    or referenced as @Rule/@ClassRule by JUnit 4 tests in other modules
    (amoro-ams, amoro-format-iceberg, amoro-format-paimon,
    amoro-format-mixed-flink, etc.). They will be migrated together with
    those consumers in a follow-up so the JUnit 4 dependency can be dropped.

--- amoro-common/src/test/java/org/apache/amoro/utils/JacksonUtilTest.java
+++ amoro-common/src/test/java/org/apache/amoro/utils/JacksonUtilTest.java
@@ -18,13 +18,13 @@
 
 package org.apache.amoro.utils;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.apache.amoro.shade.jackson2.com.fasterxml.jackson.databind.JsonNode;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Collections;
