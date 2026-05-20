5bb36b21ad3bac6b956855479a270ec03c4ad928
WenLingzhang <1298877813@qq.com>
2026-05-20 09:04:18 +0800

    Add assertions for containers in JacksonUtilTest

--- amoro-common/src/test/java/org/apache/amoro/utils/JacksonUtilTest.java
+++ amoro-common/src/test/java/org/apache/amoro/utils/JacksonUtilTest.java
@@ -115,6 +115,7 @@ public class JacksonUtilTest {
     JsonNode expiredSnapshots = yamlNode.get("expire-snapshots");
     assertTrue(JacksonUtil.getBoolean(expiredSnapshots, "enabled"));
     assertEquals("1h", JacksonUtil.getString(expiredSnapshots, "interval"));
+    
     JsonNode containersNode = yamlNode.get("containers");
     assertEquals(2, containersNode.size());
     JsonNode secondNode = containersNode.get(1);
